8664e446516d02ceeac4e3f5c32ecfa542476ba0
Nathan Ansel <nathan@livefront.com>
2023-09-05 14:18:18 -0500

    BIT-174 Fixes a linting warning

--- Bitwarden/Application/TestHelpers/Support/TestingAppDelegate.swift
+++ Bitwarden/Application/TestHelpers/Support/TestingAppDelegate.swift
@@ -4,5 +4,5 @@ import UIKit
 
 /// A replacement for `AppDelegate` that allows for checking that certain app delegate methods get called at the
 /// appropriate times during unit tests.
-/// 
+///
 class TestingAppDelegate: NSObject, UIApplicationDelegate {}
