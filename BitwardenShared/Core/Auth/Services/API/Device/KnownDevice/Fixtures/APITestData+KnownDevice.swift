df6c2de47ec21a17a1709e7c694d8a0b1127f74c
Nathan Ansel <nathan@livefront.com>
2023-09-07 11:15:11 -0500

    BIT-100 Hardcodes KnownDevice fixtures

--- BitwardenShared/Core/Auth/Services/API/Device/KnownDevice/Fixtures/APITestData+KnownDevice.swift
+++ BitwardenShared/Core/Auth/Services/API/Device/KnownDevice/Fixtures/APITestData+KnownDevice.swift
@@ -1,4 +1,6 @@
+import Foundation
+
 extension APITestData {
-    static let knownDeviceTrue = loadFromBundle(resource: "KnownDeviceTrue", extension: "json")
-    static let knownDeviceFalse = loadFromBundle(resource: "KnownDeviceFalse", extension: "json")
+    static let knownDeviceTrue = APITestData(data: Data("true".utf8))
+    static let knownDeviceFalse = APITestData(data: Data("false".utf8))
 }
