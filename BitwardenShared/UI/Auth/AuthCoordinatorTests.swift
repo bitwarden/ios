416a672bf2275d3512cfbaca8d9ce5723b8a1117
Nathan Ansel <nathan@livefront.com>
2023-09-11 20:35:59 -0500

    BIT-155: Adds the landing screen and Auth module (#20)

---
+++ BitwardenShared/UI/Auth/AuthCoordinatorTests.swift
@@ -0,0 +1,84 @@
+import SwiftUI
+import XCTest
+
+@testable import BitwardenShared
+
+// MARK: - AuthCoordinatorTests
+
+class AuthCoordinatorTests: BitwardenTestCase {
+    // MARK: Properties
+
+    var rootNavigator: MockRootNavigator!
+    var stackNavigator: MockStackNavigator!
+    var subject: AuthCoordinator!
+
+    // MARK: Setup & Teardown
+
+    override func setUp() {
+        super.setUp()
+        rootNavigator = MockRootNavigator()
+        stackNavigator = MockStackNavigator()
+        subject = AuthCoordinator(
+            rootNavigator: rootNavigator,
+            stackNavigator: stackNavigator
+        )
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        rootNavigator = nil
+        stackNavigator = nil
+        subject = nil
+    }
+
+    // MARK: Tests
+
+    /// `navigate(to:)` with `.createAccount` pushes the create account view onto the stack navigator.
+    func test_navigate_createAccount() {
+        subject.navigate(to: .createAccount)
+
+        // Placeholder assertion until the create account screen is added: BIT-157
+        XCTAssertTrue(stackNavigator.actions.last?.view is Text)
+    }
+
+    /// `navigate(to:)` with `.landing` pushes the landing view onto the stack navigator.
+    func test_navigate_landing() {
+        subject.navigate(to: .landing)
+        XCTAssertTrue(stackNavigator.actions.last?.view is LandingView)
+    }
+
+    /// `navigate(to:)` with `.login` pushes the login view onto the stack navigator.
+    func test_navigate_login() {
+        subject.navigate(to: .login)
+
+        // Placeholder assertion until the login screen is added: BIT-83
+        XCTAssertTrue(stackNavigator.actions.last?.view is Text)
+    }
+
+    /// `navigate(to:)` with `.regionSelection` pushes the region selection view onto the stack navigator.
+    func test_navigate_regionSelection() {
+        subject.navigate(to: .regionSelection)
+
+        // Placeholder assertion until the region selection screen is added: BIT-268
+        XCTAssertTrue(stackNavigator.actions.last?.view is Text)
+    }
+
+    /// `rootNavigator` uses a weak reference and does not retain a value once the root navigator has been erased.
+    func test_rootNavigator_resetWeakReference() {
+        var rootNavigator: MockRootNavigator? = MockRootNavigator()
+        subject = AuthCoordinator(
+            rootNavigator: rootNavigator!,
+            stackNavigator: stackNavigator
+        )
+        XCTAssertNotNil(subject.rootNavigator)
+
+        rootNavigator = nil
+        XCTAssertNil(subject.rootNavigator)
+    }
+
+    /// `start()` presents the stack navigator within the root navigator.
+    func test_start() {
+        subject.start()
+        XCTAssertIdentical(rootNavigator.navigatorShown, stackNavigator)
+    }
+}
---
+++ BitwardenShared/UI/Auth/Landing/LandingProcessorTests.swift
@@ -0,0 +1,67 @@
+import XCTest
+
+@testable import BitwardenShared
+
+// MARK: - LandingProcessorTests
+
+class LandingProcessorTests: BitwardenTestCase {
+    // MARK: Properties
+
+    var coordinator: MockCoordinator<AuthRoute>!
+    var subject: LandingProcessor!
+
+    // MARK: Setup & Teardown
+
+    override func setUp() {
+        super.setUp()
+        coordinator = MockCoordinator<AuthRoute>()
+
+        let state = LandingState()
+        subject = LandingProcessor(
+            coordinator: coordinator.asAnyCoordinator(),
+            state: state
+        )
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        coordinator = nil
+        subject = nil
+    }
+
+    // MARK: Tests
+
+    /// `receive(_:)` with `.continuePressed` navigates to the login screen.
+    func test_receive_continuePressed() {
+        subject.receive(.continuePressed)
+        XCTAssertEqual(coordinator.routes.last, .login)
+    }
+
+    /// `receive(_:)` with `.createAccountPressed` navigates to the create account screen.
+    func test_receive_createAccountPressed() {
+        subject.receive(.createAccountPressed)
+        XCTAssertEqual(coordinator.routes.last, .createAccount)
+    }
+
+    /// `receive(_:)` with `.emailChanged` updates the state to reflect the changes.
+    func test_receive_emailChanged() {
+        XCTAssertEqual(subject.state.email, "")
+
+        subject.receive(.emailChanged("email@example.com"))
+        XCTAssertEqual(subject.state.email, "email@example.com")
+    }
+
+    /// `receive(_:)` with `.regionPressed` navigates to the region selection screen.
+    func test_receive_regionPressed() {
+        subject.receive(.regionPressed)
+        XCTAssertEqual(coordinator.routes.last, .regionSelection)
+    }
+
+    /// `receive(_:)` with `.emailChanged` updates the state to reflect the changes.
+    func test_receive_rememberMeChanged() {
+        XCTAssertFalse(subject.state.isRememberMeOn)
+
+        subject.receive(.rememberMeChanged(true))
+        XCTAssertTrue(subject.state.isRememberMeOn)
+    }
+}
