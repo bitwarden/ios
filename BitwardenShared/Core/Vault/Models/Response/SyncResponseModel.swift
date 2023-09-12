a7f11ec8ff2ebdb3e11c0f2dc68a2a7212bb338b
Matt Czech <matt@livefront.com>
2023-09-12 11:04:45 -0500

    BIT-162: Update SyncResponseModel list optionals

--- BitwardenShared/Core/Vault/Models/Response/SyncResponseModel.swift
+++ BitwardenShared/Core/Vault/Models/Response/SyncResponseModel.swift
@@ -6,23 +6,23 @@ struct SyncResponseModel: JSONResponse, Equatable {
     // MARK: Properties
 
     /// The user's list of ciphers.
-    let ciphers: [CipherDetailsResponseModel]?
+    let ciphers: [CipherDetailsResponseModel]
 
     /// The user's list of collections.
-    let collections: [CollectionDetailsResponseModel]?
+    let collections: [CollectionDetailsResponseModel]
 
     /// Domain details.
     let domains: DomainsResponseModel?
 
     /// The user's list of folders.
-    let folders: [FolderResponseModel]?
+    let folders: [FolderResponseModel]
 
     /// Policies that apply to the user.
-    let policies: [PolicyResponseModel]?
+    let policies: [PolicyResponseModel]
 
     /// The user's profile.
     let profile: ProfileResponseModel?
 
     /// The user's list of sends.
-    let sends: [SendResponseModel]?
+    let sends: [SendResponseModel]
 }
