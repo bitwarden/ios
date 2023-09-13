a1c85ffeed476bdaffeac6d9453afa7a5df65c28
Jubie Alade <125899965+jubie-livefront@users.noreply.github.com>
2023-09-13 14:54:41 -0500

    BIT-104: Accounts Register Request (#23)

---
+++ BitwardenShared/Core/Auth/Models/API/CreateAccount/CreateAccountResponseModelTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+
+@testable import BitwardenShared
+
+// MARK: - CreateAccountResponseModelTests
+
+class CreateAccountResponseModelTests: BitwardenTestCase {
+    /// Tests that a response is initialized correctly.
+    func test_init() {
+        let subject = CreateAccountResponseModel(captchaBypassToken: "captchaBypassToken")
+        XCTAssertEqual(subject.captchaBypassToken, "captchaBypassToken")
+    }
+
+    /// Tests the successful decoding of a JSON response.
+    func test_decode_success() throws {
+        let json = APITestData.createAccountResponse.data
+        let decoder = JSONDecoder()
+        let subject = try decoder.decode(CreateAccountResponseModel.self, from: json)
+        XCTAssertEqual(subject.captchaBypassToken, "captchaBypassToken")
+    }
+}
