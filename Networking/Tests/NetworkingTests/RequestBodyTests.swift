5c2e8764cd8c10dfd5136a6933073ff699517375
Matt Czech <matt@livefront.com>
2023-09-20 10:35:59 -0500

    BIT-316: Add form URL encoded body to networking (#33)

--- Networking/Tests/NetworkingTests/RequestBodyTests.swift
+++ Networking/Tests/NetworkingTests/RequestBodyTests.swift
@@ -9,8 +9,12 @@ class RequestBodyTests: XCTestCase {
         let name = "john"
     }
 
+    struct TestRequestBodyFormURLEncoded: FormURLEncodedRequestBody {
+        let values: [URLQueryItem]
+    }
+
     /// `JSONRequestBody` can encode the JSON request body and provide additional headers.
-    func testRequestBodyJSON() throws {
+    func test_requestBody_json() throws {
         let subject = TestRequestBodyJSON()
 
         XCTAssertEqual(subject.additionalHeaders, ["Content-Type": "application/json"])
@@ -20,12 +24,43 @@ class RequestBodyTests: XCTestCase {
     }
 
     /// Test that `Data` conforms to `RequestBody` and can be used directly.
-    func testRequestBodyData() throws {
+    func test_requestBody_data() throws {
         let data = try XCTUnwrap("💾".data(using: .utf8))
 
         let subject: RequestBody = data
 
         XCTAssertEqual(subject.additionalHeaders, [:])
         XCTAssertEqual(try subject.encode(), data)
+    }
+
+    /// `FormURLEncodedBody` can encode a list of key-value pairs for a form URL encoded body.
+    func test_requestBody_formURLEncoded() throws {
+        let subject = TestRequestBodyFormURLEncoded(values: [
+            URLQueryItem(name: "foo", value: "bar"),
+            URLQueryItem(name: "abc", value: "xyz"),
+        ])
+
+        XCTAssertEqual(subject.additionalHeaders, ["Content-Type": "application/x-www-form-urlencoded"])
+
+        XCTAssertEqual(
+            try String(data: subject.encode(), encoding: .utf8),
+            "foo=bar&abc=xyz"
+        )
+    }
+
+    /// `FormURLEncodedRequestBody` uses percent encoding to handle special characters.
+    func test_requestBody_formURLEncoded_usesPercentEncoding() throws {
+        let subject = TestRequestBodyFormURLEncoded(values: [
+            URLQueryItem(name: "foo", value: "a b c"),
+            URLQueryItem(name: "bar", value: "+=&"),
+            URLQueryItem(name: "email", value: "test+1@example.com"),
+        ])
+
+        XCTAssertEqual(subject.additionalHeaders, ["Content-Type": "application/x-www-form-urlencoded"])
+
+        XCTAssertEqual(
+            try String(data: subject.encode(), encoding: .utf8),
+            "foo=a%20b%20c&bar=%2B%3D%26&email=test%2B1%40example%2Ecom"
+        )
     }
 }
