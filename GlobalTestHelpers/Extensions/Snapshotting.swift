530b780c3186bf8e4175cb7d7807ace9ea647568
Nathan Ansel <nathan@livefront.com>
2023-09-28 09:58:14 -0500

    BIT-548 Adds tests for LandingView

---
+++ GlobalTestHelpers/Extensions/Snapshotting.swift
@@ -0,0 +1,14 @@
+import SnapshotTesting
+import SwiftUI
+
+extension Snapshotting where Value: SwiftUI.View, Format == UIImage {
+    /// A default snapshot in portrait on iPhone 13, with precision 1 and perceptual precision of 0.99.
+    static var defaultPortrait: Snapshotting {
+        return .image(
+            precision: 1,
+            perceptualPrecision: 0.99,
+            layout: .device(config: .iPhone13),
+            traits: .init(userInterfaceStyle: .light)
+        )
+    }
+}
