872d76623aef4c894a2b424d5031e91beebea18a
Nathan Ansel <nathan@livefront.com>
2023-10-18 14:04:05 -0500

    BIT-676 Adds an AsyncButton for easily kicking of async actions when tapped

--- GlobalTestHelpers/Extensions/InspectableView.swift
+++ GlobalTestHelpers/Extensions/InspectableView.swift
@@ -1,6 +1,32 @@
+import Foundation
 import ViewInspector
 
+/// A generic type wrapper around `AsyncButton` to allow `ViewInspector` to find instances of `AsyncButton` without
+/// needing to know the type of its `Label`.
+///
+struct AsyncButtonType: BaseViewType {
+    static var typePrefix: String = "AsyncButton"
+
+    static var namespacedPrefixes: [String] = [
+        "BitwardenShared.AsyncButton",
+    ]
+}
+
+// MARK: InspectableView
+
 extension InspectableView {
+    // MARK: Methods
+
+    /// Attempts to locate an async button with the provided title.
+    ///
+    /// - Parameter title: The title to use while searching for a button.
+    /// - Returns: An async button, if one can be located.
+    /// - Throws: Throws an error if a view was unable to be located.
+    ///
+    func find(asyncButton title: String) throws -> InspectableView<AsyncButtonType> {
+        try find(AsyncButtonType.self, containing: title)
+    }
+
     /// Attempts to locate a button with the provided id.
     ///
     /// - Parameter id: The id to use while searching for a button.
@@ -10,6 +36,15 @@ extension InspectableView {
     func find(buttonWithId id: AnyHashable) throws -> InspectableView<ViewType.Button> {
         try find(ViewType.Button.self) { view in
             try view.id() == id
+        }
+    }
+
+    func find(
+        buttonWithAccessibilityLabel accessibilityLabel: String,
+        locale: Locale = .testsDefault
+    ) throws -> InspectableView<ViewType.Button> {
+        try find(ViewType.Button.self) { view in
+            try view.accessibilityLabel().string(locale: locale) == accessibilityLabel
         }
     }
 
@@ -31,5 +66,23 @@ extension InspectableView {
     ///
     func find(secureField label: String) throws -> InspectableView<ViewType.SecureField> {
         try find(ViewType.SecureField.self, containing: label)
+    }
+}
+
+extension InspectableView where View == AsyncButtonType {
+    /// Simulates a tap on an `AsyncButton`. This method is asynchronous and allows the entire `async` `action` on the
+    /// button to run before returning.
+    ///
+    func tap() async throws {
+        typealias Callback = () async -> Void
+        let mirror = Mirror(reflecting: self)
+        if let action = mirror.descendant("content", "view", "action") as? Callback {
+            await action()
+        } else {
+            throw InspectionError.attributeNotFound(
+                label: "action",
+                type: String(describing: AsyncButtonType.self)
+            )
+        }
     }
 }
