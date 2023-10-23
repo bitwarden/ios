9ab3da53e8380f7a2f739c94920756826403eb8d
jubie-livefront <jubie@livefront.com>
2023-10-23 15:15:07 -0500

    BIT-861: Password/Hint field errors

--- BitwardenShared/UI/Platform/Application/Extensions/Alert+Extensions.swift
+++ BitwardenShared/UI/Platform/Application/Extensions/Alert+Extensions.swift
@@ -13,16 +13,24 @@ extension Alert {
         )
     }
 
-    /// An alert to show when a required field was left empty.
+    /// An alert to show when password confirmation is incorrect.
     ///
-    /// - Parameter fieldName: The name of the field that was left empty.
-    ///
-    /// - Returns: an alert shown when a required field was left empty.
-    ///
-    static func validationFieldRequired(fieldName: String) -> Alert {
+    static var passwordsDontMatch: Alert {
         Alert(
             title: Localizations.anErrorHasOccurred,
-            message: Localizations.validationFieldRequired(fieldName),
+            message: Localizations.masterPasswordConfirmationValMessage,
+            alertActions: [
+                AlertAction(title: Localizations.ok, style: .default),
+            ]
+        )
+    }
+
+    /// An alert to show when the password does not meet the minimum length requirement.
+    ///
+    static var passwordIsTooShort: Alert {
+        Alert(
+            title: Localizations.anErrorHasOccurred,
+            message: Localizations.masterPasswordLengthValMessageX(Constants.minimumPasswordCharacters),
             alertActions: [
                 AlertAction(title: Localizations.ok, style: .default),
             ]
@@ -39,6 +47,22 @@ extension Alert {
         Alert(
             title: Localizations.anErrorHasOccurred,
             message: errorMessage,
+            alertActions: [
+                AlertAction(title: Localizations.ok, style: .default),
+            ]
+        )
+    }
+
+    /// An alert to show when a required field was left empty.
+    ///
+    /// - Parameter fieldName: The name of the field that was left empty.
+    ///
+    /// - Returns: an alert shown when a required field was left empty.
+    ///
+    static func validationFieldRequired(fieldName: String) -> Alert {
+        Alert(
+            title: Localizations.anErrorHasOccurred,
+            message: Localizations.validationFieldRequired(fieldName),
             alertActions: [
                 AlertAction(title: Localizations.ok, style: .default),
             ]
