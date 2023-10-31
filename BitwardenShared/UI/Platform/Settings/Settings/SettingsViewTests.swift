41637b2bfa2a2bc34c4c7492a5f73e4ce088265c
Jubie Alade <125899965+jubie-livefront@users.noreply.github.com>
2023-10-31 11:28:58 -0500

    BIT-86: Settings UI (#112)

---
+++ BitwardenShared/UI/Platform/Settings/Settings/SettingsViewTests.swift
@@ -0,0 +1,15 @@
+import SnapshotTesting
+import XCTest
+
+// MARK: - SettingsViewTests
+
+@testable import BitwardenShared
+
+class SettingsViewTests: BitwardenTestCase {
+    let subject = SettingsView(store: .mock(state: SettingsState()))
+
+    /// Tests the view renders correctly.
+    func test_viewRender() {
+        assertSnapshot(of: subject, as: .defaultPortrait)
+    }
+}
