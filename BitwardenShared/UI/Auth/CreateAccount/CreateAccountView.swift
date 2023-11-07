e63a8b277d5db8a4bb08e932913cbb37e575acb7
Matt Czech <matt@livefront.com>
2023-11-07 14:20:05 -0600

    BIT-1109: Add accessibility identifiers to Create Account (#133)

--- BitwardenShared/UI/Auth/CreateAccount/CreateAccountView.swift
+++ BitwardenShared/UI/Auth/CreateAccount/CreateAccountView.swift
@@ -69,6 +69,7 @@ struct CreateAccountView: View {
                 .foregroundColor(Color(asset: Asset.Colors.textPrimary))
                 .font(.styleGuide(.footnote))
         }
+        .accessibilityIdentifier("CheckExposedMasterPasswordToggle")
         .toggleStyle(.bitwarden)
         .id(ViewIdentifier.CreateAccount.checkBreaches)
     }
@@ -77,6 +78,7 @@ struct CreateAccountView: View {
     private var emailAndPassword: some View {
         VStack(spacing: 16) {
             BitwardenTextField(
+                accessibilityIdentifier: "EmailAddressEntry",
                 title: Localizations.emailAddress,
                 text: store.binding(
                     get: \.emailText,
@@ -89,11 +91,13 @@ struct CreateAccountView: View {
             .autocorrectionDisabled()
 
             BitwardenTextField(
+                accessibilityIdentifier: "MasterPasswordEntry",
                 title: Localizations.masterPassword,
                 isPasswordVisible: store.binding(
                     get: \.arePasswordsVisible,
                     send: CreateAccountAction.togglePasswordVisibility
                 ),
+                passwordVisibilityAccessibilityId: "PasswordVisibilityToggle",
                 text: store.binding(
                     get: \.passwordText,
                     send: CreateAccountAction.passwordTextChanged
@@ -109,6 +113,7 @@ struct CreateAccountView: View {
     private var passwordHint: some View {
         VStack(alignment: .leading) {
             BitwardenTextField(
+                accessibilityIdentifier: "MasterPasswordHintLabel",
                 title: Localizations.masterPasswordHint,
                 text: store.binding(
                     get: \.passwordHintText,
@@ -125,11 +130,13 @@ struct CreateAccountView: View {
     /// The text field for re-typing the master password.
     private var retypePassword: some View {
         BitwardenTextField(
+            accessibilityIdentifier: "ConfirmMasterPasswordEntry",
             title: Localizations.retypeMasterPassword,
             isPasswordVisible: store.binding(
                 get: \.arePasswordsVisible,
                 send: CreateAccountAction.togglePasswordVisibility
             ),
+            passwordVisibilityAccessibilityId: "ConfirmPasswordVisibilityToggle",
             text: store.binding(
                 get: \.retypePasswordText,
                 send: CreateAccountAction.retypePasswordTextChanged
@@ -174,6 +181,7 @@ struct CreateAccountView: View {
         .accessibilityAction(named: Localizations.privacyPolicy) {
             openURL(ExternalLinksConstants.privacyPolicy)
         }
+        .accessibilityIdentifier("AcceptPoliciesToggle")
         .foregroundColor(Color(asset: Asset.Colors.textPrimary))
         .font(.styleGuide(.footnote))
         .toggleStyle(.bitwarden)
