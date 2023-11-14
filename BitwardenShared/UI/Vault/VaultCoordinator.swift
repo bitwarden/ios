2c4bbe025c847899bf7a6c121a12e00f893d6946
Nathan Ansel <nathan@livefront.com>
2023-11-14 10:49:39 -0600

    BIT-374 Adds comment about navigation item presetting

--- BitwardenShared/UI/Vault/VaultCoordinator.swift
+++ BitwardenShared/UI/Vault/VaultCoordinator.swift
@@ -104,7 +104,14 @@ final class VaultCoordinator: Coordinator, HasStackNavigator {
         let viewController = UIHostingController(rootView: view)
 
         // Preset some navigation item values so that the navigation bar does not flash oddly once
-        // the view's push animation has completed.
+        // the view's push animation has completed. This happens because `UIHostingController` does
+        // not resolve its `navigationItem` properties until the view has been displayed on screen.
+        // In this case, that doesn't happen until the push animation has completed, which results
+        // in both the title and the search bar flashing into view after the push animation
+        // completes. This occurs on all iOS versions (tested on iOS 17).
+        //
+        // The values set here are temporary, and are overwritten once the hosting controller has
+        // resolved its root view's navigation bar modifiers.
         viewController.navigationItem.largeTitleDisplayMode = .never
         viewController.navigationItem.title = group.navigationTitle
         let searchController = UISearchController()
