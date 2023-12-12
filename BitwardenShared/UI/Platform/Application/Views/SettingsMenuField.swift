a227748ecba8e2a637c0998b406e04c60badf8c5
jubie-livefront <jubie@livefront.com>
2023-12-12 09:41:48 -0600

    Updates how font is applied to SettingsMenuField

--- BitwardenShared/UI/Platform/Application/Views/SettingsMenuField.swift
+++ BitwardenShared/UI/Platform/Application/Views/SettingsMenuField.swift
@@ -46,7 +46,7 @@ struct SettingsMenuField<T>: View where T: Menuable {
                         .foregroundColor(Asset.Colors.textSecondary.swiftUIColor)
                 }
             }
-            .font(.styleGuide(.body))
+            .styleGuide(.body)
             .id(title)
             .padding(.horizontal, 16)
 
