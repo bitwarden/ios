d9dd9b37feae9107dfd07a81746268fbd2d4f013
Matt Czech <matt@livefront.com>
2023-12-12 12:19:55 -0600

    BIT-1233: Update password strength indicator colors (#195)

--- BitwardenShared/UI/Platform/Settings/Extensions/AlertSettingsTests.swift
+++ BitwardenShared/UI/Platform/Settings/Extensions/AlertSettingsTests.swift
@@ -2,7 +2,7 @@ import XCTest
 
 @testable import BitwardenShared
 
-class AlertAccountTests: BitwardenTestCase {
+class AlertSettingsTests: BitwardenTestCase {
     /// `logoutOnTimeoutAlert(action:)` constructs an `Alert` with the title, message, and Yes and Cancel buttons.
     func test_logoutOnTimeoutAlert() {
         let subject = Alert.logoutOnTimeoutAlert {}
