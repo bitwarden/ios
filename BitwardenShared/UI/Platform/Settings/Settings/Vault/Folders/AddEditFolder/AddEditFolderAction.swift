4b8efed3f61bc15ce6ffc814de92103d187d4e42
Shannon Draeker <125921730+shannon-livefront@users.noreply.github.com>
2023-12-20 11:12:43 -0700

    BIT-452: Add add folder view (#221)

---
+++ BitwardenShared/UI/Platform/Settings/Settings/Vault/Folders/AddEditFolder/AddEditFolderAction.swift
@@ -0,0 +1,14 @@
+// MARK: - AddEditFolderAction
+
+/// Actions handled by the `AddEditFolderView`.
+///
+enum AddEditFolderAction: Equatable {
+    /// Dismiss the sheet.
+    case dismiss
+
+    /// The user edited the folder name text field.
+    case folderNameTextChanged(String)
+
+    /// The more toolbar button was tapped.
+    case moreTapped
+}
