d2b44a64714baf71b5d00bd44980ec96948780b2
Matt Czech <matt@livefront.com>
2023-12-20 10:25:39 -0600

    BIT-1141: Update create and delete account to use response validation

--- BitwardenShared/Core/Auth/Services/API/Account/Requests/DeleteAccountRequest.swift
+++ BitwardenShared/Core/Auth/Services/API/Account/Requests/DeleteAccountRequest.swift
@@ -1,16 +1,6 @@
 import Foundation
 import Networking
 
-// MARK: - DeleteAccountRequestError
-
-/// Errors that can occur when sending a `DeleteAccountRequest`.
-enum DeleteAccountRequestError: Error, Equatable {
-    /// A validation error occurred when deleting an account.
-    ///
-    /// - Parameter errorResponse: The error response returned from the server.
-    case serverError(_ errorResponse: ErrorResponseModel)
-}
-
 // MARK: - DeleteAccountRequest
 
 /// The API request sent when deleting an account.
@@ -26,16 +16,4 @@ struct DeleteAccountRequest: Request {
 
     /// The URL path for this request.
     var path: String = "/accounts"
-
-    // MARK: Methods
-
-    func validate(_ response: HTTPResponse) throws {
-        switch response.statusCode {
-        case 400 ..< 500:
-            guard let errorResponse = try? ErrorResponseModel(response: response) else { return }
-            throw DeleteAccountRequestError.serverError(errorResponse)
-        default:
-            return
-        }
-    }
 }
