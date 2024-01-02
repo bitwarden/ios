8d4aaf4d2ccf9fb31151f23e3bb9584db00afebd
Ezimet <148797477+ezimet-livefront@users.noreply.github.com>
2024-01-02 13:44:01 -0500

    BIT-937: Appearance menu UI (#249)
    Co-authored-by: Matt Czech <matt@livefront.com>

---
+++ BitwardenShared/UI/Platform/Settings/Settings/Appearance/AppearanceState.swift
@@ -0,0 +1,8 @@
+// MARK: - AppearanceState
+
+/// An object that defines the current state of the `AppearanceView`.
+///
+struct AppearanceState {
+    /// Whether or not the show website icons toggle is on.
+    var isShowWebsiteIconsToggleOn: Bool = false
+}
--- BitwardenShared/UI/Platform/Settings/Settings/SettingsViewTests.swift
+++ BitwardenShared/UI/Platform/Settings/Settings/SettingsViewTests.swift
@@ -8,7 +8,70 @@ import XCTest
 class SettingsViewTests: BitwardenTestCase {
     // MARK: Properties
 
-    let subject = SettingsView(store: .mock(state: SettingsState()))
+    var processor: MockProcessor<SettingsState, SettingsAction, Void>!
+    var subject: SettingsView!
+
+    // MARK: Setup & Teardown
+
+    override func setUp() {
+        super.setUp()
+
+        processor = MockProcessor(state: SettingsState())
+        let store = Store(processor: processor)
+
+        subject = SettingsView(store: store)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+
+        processor = nil
+        subject = nil
+    }
+
+    // MARK: Tests
+
+    /// Tapping the about button dispatches the `.aboutPressed` action.
+    func test_aboutButton_tap() throws {
+        let button = try subject.inspect().find(button: Localizations.about)
+        try button.tap()
+        XCTAssertEqual(processor.dispatchedActions.last, .aboutPressed)
+    }
+
+    /// Tapping the accountSecurity button dispatches the `.accountSecurityPressed` action.
+    func test_accountSecurityButton_tap() throws {
+        let button = try subject.inspect().find(button: Localizations.accountSecurity)
+        try button.tap()
+        XCTAssertEqual(processor.dispatchedActions.last, .accountSecurityPressed)
+    }
+
+    /// Tapping the appearance button dispatches the `.appearancePressed` action.
+    func test_appearanceButton_tap() throws {
+        let button = try subject.inspect().find(button: Localizations.appearance)
+        try button.tap()
+        XCTAssertEqual(processor.dispatchedActions.last, .appearancePressed)
+    }
+
+    /// Tapping the autofill button dispatches the `.autoFillPressed` action.
+    func test_autofillButton_tap() throws {
+        let button = try subject.inspect().find(button: Localizations.autofill)
+        try button.tap()
+        XCTAssertEqual(processor.dispatchedActions.last, .autoFillPressed)
+    }
+
+    /// Tapping the other button dispatches the `.otherPressed` action.
+    func test_otherButton_tap() throws {
+        let button = try subject.inspect().find(button: Localizations.other)
+        try button.tap()
+        XCTAssertEqual(processor.dispatchedActions.last, .otherPressed)
+    }
+
+    /// Tapping the vault button dispatches the `.vaultPressed` action.
+    func test_vaultButton_tap() throws {
+        let button = try subject.inspect().find(button: Localizations.vault)
+        try button.tap()
+        XCTAssertEqual(processor.dispatchedActions.last, .vaultPressed)
+    }
 
     /// Tests the view renders correctly.
     func test_viewRender() {
