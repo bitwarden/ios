158d1f2c00b06a17908d889aa2e3a488e5048a73
Eliot Williams <148839008+eliot-livefront@users.noreply.github.com>
2024-01-02 12:06:30 -0800

    BIT-348, BIT-351, & BIT-1121: Add, Save, View, & Edit Card Type Cipher (#229)

--- BitwardenShared/Core/Vault/Extensions/TestHelpers/BitwardenSdk+VaultFixtures.swift
+++ BitwardenShared/Core/Vault/Extensions/TestHelpers/BitwardenSdk+VaultFixtures.swift
@@ -116,9 +116,9 @@ extension CipherView {
         )
     }
 
-    static func loginFixture(
+    static func cardFixture(
         attachments: [AttachmentView]? = nil,
-        card: CardView? = nil,
+        card: CardView = CardView.fixture(),
         collectionIds: [String] = [],
         creationDate: DateTime = Date(year: 2023, month: 11, day: 5, hour: 9, minute: 41),
         deletedDate: Date? = nil,
@@ -127,10 +127,8 @@ extension CipherView {
         fields: [FieldView]? = nil,
         folderId: String? = nil,
         id: String = "8675",
-        identity: IdentityView? = nil,
         key: String? = nil,
         localData: LocalDataView? = nil,
-        login: BitwardenSdk.LoginView = .fixture(),
         name: String = "Bitwarden",
         notes: String? = nil,
         organizationId: String? = nil,
@@ -138,8 +136,6 @@ extension CipherView {
         passwordHistory: [PasswordHistoryView]? = nil,
         reprompt: BitwardenSdk.CipherRepromptType = .none,
         revisionDate: Date = Date(year: 2023, month: 11, day: 5, hour: 9, minute: 41),
-        secureNote: SecureNoteView? = nil,
-        type: BitwardenSdk.CipherType = .login,
         viewPassword: Bool = true
     ) -> CipherView {
         CipherView(
@@ -150,11 +146,61 @@ extension CipherView {
             key: key,
             name: name,
             notes: notes,
-            type: type,
-            login: login,
-            identity: identity,
+            type: .card,
+            login: nil,
+            identity: nil,
             card: card,
-            secureNote: secureNote,
+            secureNote: nil,
+            favorite: favorite,
+            reprompt: reprompt,
+            organizationUseTotp: organizationUseTotp,
+            edit: edit,
+            viewPassword: viewPassword,
+            localData: localData,
+            attachments: attachments,
+            fields: fields,
+            passwordHistory: passwordHistory,
+            creationDate: creationDate,
+            deletedDate: deletedDate,
+            revisionDate: revisionDate
+        )
+    }
+
+    static func loginFixture(
+        attachments: [AttachmentView]? = nil,
+        collectionIds: [String] = [],
+        creationDate: DateTime = Date(year: 2023, month: 11, day: 5, hour: 9, minute: 41),
+        deletedDate: Date? = nil,
+        edit: Bool = true,
+        favorite: Bool = false,
+        fields: [FieldView]? = nil,
+        folderId: String? = nil,
+        id: String = "8675",
+        key: String? = nil,
+        localData: LocalDataView? = nil,
+        login: BitwardenSdk.LoginView = .fixture(),
+        name: String = "Bitwarden",
+        notes: String? = nil,
+        organizationId: String? = nil,
+        organizationUseTotp: Bool = false,
+        passwordHistory: [PasswordHistoryView]? = nil,
+        reprompt: BitwardenSdk.CipherRepromptType = .none,
+        revisionDate: Date = Date(year: 2023, month: 11, day: 5, hour: 9, minute: 41),
+        viewPassword: Bool = true
+    ) -> CipherView {
+        CipherView(
+            id: id,
+            organizationId: organizationId,
+            folderId: folderId,
+            collectionIds: collectionIds,
+            key: key,
+            name: name,
+            notes: notes,
+            type: .login,
+            login: login,
+            identity: nil,
+            card: nil,
+            secureNote: nil,
             favorite: favorite,
             reprompt: reprompt,
             organizationUseTotp: organizationUseTotp,
@@ -187,6 +233,26 @@ extension Collection {
             externalId: externalId,
             hidePasswords: hidePasswords,
             readOnly: readOnly
+        )
+    }
+}
+
+extension BitwardenSdk.CardView {
+    static func fixture(
+        brand: String? = nil,
+        cardholderName: String? = nil,
+        code: String? = nil,
+        expMonth: String? = nil,
+        expYear: String? = nil,
+        number: String? = nil
+    ) -> BitwardenSdk.CardView {
+        BitwardenSdk.CardView(
+            cardholderName: cardholderName,
+            expMonth: expMonth,
+            expYear: expYear,
+            code: code,
+            brand: brand,
+            number: number
         )
     }
 }
---
+++ BitwardenShared/UI/Vault/VaultItem/AddEditItem/AddEditCardItem/CardComponent.swift
@@ -0,0 +1,147 @@
+// MARK: - CardComponent
+
+import Foundation
+
+/// An enumeration defining various components associated with credit and debit cards.
+///
+enum CardComponent {
+    /// Represents months of the year, commonly used in card expiration dates.
+    ///
+    enum Month: Int {
+        /// 01 - January
+        case jan = 1
+
+        /// 02 - February
+        case feb
+
+        /// 03 - March
+        case mar
+
+        /// 04 - April
+        case apr
+
+        /// 05 - May
+        case may
+
+        /// 06 - June
+        case jun
+
+        /// 07 - July
+        case jul
+
+        /// 08 - August
+        case aug
+
+        /// 09 - September
+        case sep
+
+        /// 10 - October
+        case oct
+
+        /// 11- November
+        case nov
+
+        /// 12 - December
+        case dec
+
+        /// A formatted number string for the month.
+        var formattedValue: String {
+            let formatter = NumberFormatter()
+            formatter.minimumIntegerDigits = 2
+            return formatter.string(from: NSNumber(value: rawValue)) ?? "00"
+        }
+
+        /// A localized string for the month name.
+        var localized: String {
+            switch self {
+            case .jan:
+                Localizations.january
+            case .feb:
+                Localizations.february
+            case .mar:
+                Localizations.march
+            case .apr:
+                Localizations.april
+            case .may:
+                Localizations.may
+            case .jun:
+                Localizations.june
+            case .jul:
+                Localizations.july
+            case .aug:
+                Localizations.august
+            case .sep:
+                Localizations.september
+            case .oct:
+                Localizations.october
+            case .nov:
+                Localizations.november
+            case .dec:
+                Localizations.december
+            }
+        }
+    }
+
+    /// Represents various credit card brands.
+    ///
+    enum Brand: String {
+        /// Visa
+        case visa = "Visa"
+
+        /// Mastercard
+        case mastercard = "Mastercard" // swiftlint:disable:this inclusive_language
+
+        /// American Express
+        case americanExpress = "American Express"
+
+        /// Discover
+        case discover = "Discover"
+
+        /// Diners Club
+        case dinersClub = "Diners Club"
+
+        /// JCB
+        case jcb = "JCB"
+
+        /// Maestro
+        case maestro = "Maestro"
+
+        /// UnionPay
+        case unionPay = "UnionPay"
+
+        /// RuPay
+        case ruPay = "RuPay"
+
+        /// Other brands not explicitly listed here
+        case other = "Other"
+    }
+}
+
+extension CardComponent.Brand: CaseIterable {}
+extension CardComponent.Brand: Menuable {
+    /// default state title for title type
+    static var defaultValueLocalizedName: String {
+        "--\(Localizations.select)--"
+    }
+
+    /// Provides a localized string representation of the card brand.
+    /// For the 'other' case, it returns a localized string for 'Other'.
+    var localizedName: String {
+        guard case .other = self else {
+            return rawValue
+        }
+        return Localizations.other
+    }
+}
+
+extension CardComponent.Month: CaseIterable {}
+extension CardComponent.Month: Menuable {
+    /// default state title for title type
+    static var defaultValueLocalizedName: String {
+        "--\(Localizations.select)--"
+    }
+
+    var localizedName: String {
+        "\(formattedValue) - \(localized)"
+    }
+}
