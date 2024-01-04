5c34e97b5c72b469c5ffa7db1bb47e135c53e0bd
Nathan Ansel <nathan@livefront.com>
2024-01-03 21:18:00 -0600

    BIT-733 Removes manual validation

--- BitwardenShared/Core/Auth/Services/API/Account/Requests/PasswordHintRequest.swift
+++ BitwardenShared/Core/Auth/Services/API/Account/Requests/PasswordHintRequest.swift
@@ -1,15 +1,5 @@
 import Networking
 
-// MARK: - PasswordHintRequestError
-
-/// Errors that can occur when sending a `DeleteAccountRequest`.
-enum PasswordHintRequestError: Error, Equatable {
-    /// A validation error occurred when requesting a user's password hint.
-    ///
-    /// - Parameter errorResponse: The error response returned from the server.
-    case serverError(_ errorResponse: ErrorResponseModel)
-}
-
 // MARK: - PasswordHintRequest
 
 /// A request for requesting a user's password hint.
@@ -21,16 +11,4 @@ struct PasswordHintRequest: Request {
     let method: HTTPMethod = .post
 
     let path = "/accounts/password-hint"
-
-    // MARK: Methods
-
-    func validate(_ response: HTTPResponse) throws {
-        switch response.statusCode {
-        case 400 ..< 500:
-            guard let errorResponse = try? ErrorResponseModel(response: response) else { return }
-            throw PasswordHintRequestError.serverError(errorResponse)
-        default:
-            return
-        }
-    }
 }
