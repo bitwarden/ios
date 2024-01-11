1fe4710da6b08b1097ca78d763159cd976200531
Nathan Ansel <nathan@livefront.com>
2024-01-11 10:02:07 -0600

    BIT-433: Removes unneeded comments

--- BitwardenShared/Core/Tools/Services/API/Requests/AddSendRequest.swift
+++ BitwardenShared/Core/Tools/Services/API/Requests/AddSendRequest.swift
@@ -47,36 +47,3 @@ struct AddSendRequest: Request {
         )
     }
 }
-//
-//
-///// A request model for adding a new cipher.
-/////
-//struct AddCipherRequest: Request {
-//    typealias Response = CipherDetailsResponseModel
-//
-//    // MARK: Properties
-//
-//    /// The body of the request.
-//    var body: CipherRequestModel? {
-//        requestModel
-//    }
-//
-//    /// The HTTP method for this request.
-//    let method = HTTPMethod.post
-//
-//    /// The URL path for this request.
-//    let path = "/ciphers"
-//
-//    /// The request details to include in the body of the request.
-//    let requestModel: CipherRequestModel
-//
-//    // MARK: Initialization
-//
-//    / Initialize an `AddCipherRequest` for a `Cipher`.
-//    /
-//    / - Parameter cipher: The `Cipher` to add to the user's vault.
-//    /
-//    init(cipher: Cipher) {
-//        requestModel = CipherRequestModel(cipher: cipher)
-//    }
-//}
