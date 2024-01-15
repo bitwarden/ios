f81bf6967c569ea7a3049c532e74e5a51bffb3a3
Shannon Draeker <125921730+shannon-livefront@users.noreply.github.com>
2024-01-15 15:24:18 -0700

    BIT-425: Validate password before exporting vault (#325)

--- BitwardenShared/Core/Platform/Services/ServiceContainer.swift
+++ BitwardenShared/Core/Platform/Services/ServiceContainer.swift
@@ -273,6 +273,7 @@ public class ServiceContainer: Services {
         )
 
         let settingsRepository = DefaultSettingsRepository(
+            clientAuth: clientService.clientAuth(),
             clientVault: clientService.clientVault(),
             folderService: folderService,
             pasteboardService: pasteboardService,
