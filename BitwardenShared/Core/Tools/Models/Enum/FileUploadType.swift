5f9cf9893ccbdbc74bc46f075033837bdbe5c117
Nathan Ansel <nathan@livefront.com>
2024-01-22 12:21:11 -0600

    Update BitwardenShared/Core/Tools/Models/Enum/FileUploadType.swift
    Co-authored-by: Shannon Draeker <125921730+shannon-livefront@users.noreply.github.com>

--- BitwardenShared/Core/Tools/Models/Enum/FileUploadType.swift
+++ BitwardenShared/Core/Tools/Models/Enum/FileUploadType.swift
@@ -8,6 +8,6 @@ enum FileUploadType: Int, Codable, Equatable {
     /// Upload directly to the server hosting the user's vault.
     case direct = 0
 
-    /// Upload to an Azure enviornment.
+    /// Upload to an Azure environment.
     case azure = 1
 }
