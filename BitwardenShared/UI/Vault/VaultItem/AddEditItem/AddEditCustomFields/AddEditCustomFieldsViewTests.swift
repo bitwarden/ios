40f92056ab488689ee12bc2955888df4b830d302
ezimet-livefront <ezimet@livefront.com>
2024-01-23 17:06:28 -0500

    updated doc

--- BitwardenShared/UI/Vault/VaultItem/AddEditItem/AddEditCustomFields/AddEditCustomFieldsViewTests.swift
+++ BitwardenShared/UI/Vault/VaultItem/AddEditItem/AddEditCustomFields/AddEditCustomFieldsViewTests.swift
@@ -71,21 +71,21 @@ class AddEditCustomFieldsViewTests: BitwardenTestCase {
         XCTAssertEqual(processor.dispatchedActions.last, .removeCustomFieldPressed(index: 0))
     }
 
-    /// The view with several all types of custom fields renders correctly in dark mode.
+    /// The view with all types of custom fields renders correctly in dark mode.
     func test_snapshot_allFields_dark() {
         for preview in AddEditCustomFieldsView_Previews._allPreviews {
             assertSnapshot(of: preview.content, as: .defaultPortraitDark)
         }
     }
 
-    /// The view with several all types of custom fields renders correctly.
+    /// The view with all types of custom fields renders correctly.
     func test_snapshot_allFields_default() {
         for preview in AddEditCustomFieldsView_Previews._allPreviews {
             assertSnapshot(of: preview.content, as: .defaultPortrait)
         }
     }
 
-    /// The view with several all types of custom fields renders correctly with large text.
+    /// The view with all types of custom fields renders correctly with large text.
     func test_snapshot_allFields_large() {
         for preview in AddEditCustomFieldsView_Previews._allPreviews {
             assertSnapshot(of: preview.content, as: .defaultPortraitAX5)
