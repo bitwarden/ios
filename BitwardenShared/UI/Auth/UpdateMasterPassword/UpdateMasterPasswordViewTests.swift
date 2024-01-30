951187a3d0f91c061775b3a143d3bd5bdda9fe07
ezimet-livefront <ezimet@livefront.com>
2024-01-30 16:47:34 -0500

    update snapshot test

--- BitwardenShared/UI/Auth/UpdateMasterPassword/UpdateMasterPasswordViewTests.swift
+++ BitwardenShared/UI/Auth/UpdateMasterPassword/UpdateMasterPasswordViewTests.swift
@@ -122,6 +122,7 @@ class UpdateMasterPasswordViewTests: BitwardenTestCase {
 
     /// A snapshot of the view with all filled values fields in a large text.
     func test_snapshot_withFilled_large() {
-        assertSnapshots(of: subject, as: [.defaultPortraitAX5])
+        isRecording = true
+        assertSnapshots(of: subject, as: [.tallPortraitAX5(heightMultiple: 6)])
     }
 }
