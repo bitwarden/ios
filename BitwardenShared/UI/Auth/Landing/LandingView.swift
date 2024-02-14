6eacda3c292771cbc4aab1f2e748e45471a501d7
Jubie Alade <125899965+jubie-livefront@users.noreply.github.com>
2024-02-14 12:06:48 -0600

    BIT-1889: Region Icon (#483)

--- BitwardenShared/UI/Auth/Landing/LandingView.swift
+++ BitwardenShared/UI/Auth/Landing/LandingView.swift
@@ -100,7 +100,7 @@ struct LandingView: View {
                         }
                         .styleGuide(.subheadline)
 
-                        Image(decorative: Asset.Images.downTriangle)
+                        Image(decorative: Asset.Images.downAngle)
                             .resizable()
                             .frame(width: 12, height: 12)
                             .foregroundColor(Asset.Colors.primaryBitwarden.swiftUIColor)
