b184251c5415ddeb0a286aef2a646660e27d00fb
Matt Czech <matt@livefront.com>
2024-02-27 14:07:00 -0600

    BIT-1934: Add try again button to Yubikey two-factor screen

--- BitwardenShared/UI/Auth/Login/TwoFactorAuth/TwoFactorAuthView.swift
+++ BitwardenShared/UI/Auth/Login/TwoFactorAuth/TwoFactorAuthView.swift
@@ -162,6 +162,11 @@ struct TwoFactorAuthView: View {
                 await store.perform(.resendEmailTapped)
             }
             .buttonStyle(.tertiary())
+        } else if store.state.authMethod == .yubiKey {
+            AsyncButton(Localizations.tryAgain) {
+                await store.perform(.tryAgainTapped)
+            }
+            .buttonStyle(.tertiary())
         }
     }
 
