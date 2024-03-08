6615bee09126d90c4e9b3056ccbfda9ad8edd916
Matt Czech <matt@livefront.com>
2024-03-08 16:58:32 -0600

    BIT-1163: Generate unset avatar colors based on user ID (#518)

--- BitwardenShared/UI/Platform/Application/Extensions/StringTests.swift
+++ BitwardenShared/UI/Platform/Application/Extensions/StringTests.swift
@@ -7,6 +7,13 @@ import XCTest
 class StringTests: BitwardenTestCase {
     // MARK: Tests
 
+    /// `hashColor` returns a color generated from a hash of the string's characters.
+    func test_hashColor() {
+        XCTAssertEqual("test".hashColor.description, "#924436FF")
+        XCTAssertEqual("0620ee30-91c3-40cb-8fad-b102005c35b0".hashColor.description, "#32F23FFF")
+        XCTAssertEqual("9c303aee-e636-4760-94b6-e4951d7b0abb".hashColor.description, "#C96CD2FF")
+    }
+
     /// `isValidEmail` with an invalid string returns `false`.
     func test_isValidEmail_withInvalidString() {
         let subjects = [
@@ -54,6 +61,14 @@ class StringTests: BitwardenTestCase {
         XCTAssertFalse("a b c".isValidURL)
         XCTAssertFalse("a<b>c".isValidURL)
         XCTAssertFalse("a[b]c".isValidURL)
+    }
+
+    /// `leftPadding(toLength:withPad:)` returns a string padded to the specified length.
+    func test_leftPadding() {
+        XCTAssertEqual("".leftPadding(toLength: 2, withPad: "0"), "00")
+        XCTAssertEqual("AB".leftPadding(toLength: 4, withPad: "0"), "00AB")
+        XCTAssertEqual("ABCD".leftPadding(toLength: 4, withPad: "0"), "ABCD")
+        XCTAssertEqual("ABCDEF".leftPadding(toLength: 4, withPad: "0"), "CDEF")
     }
 
     /// `urlDecoded()` with an invalid string throws an error.
