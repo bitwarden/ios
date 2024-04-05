91c19bd2ff299913982f6e7588976763c84889e6
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-04-05 09:38:05 -0500

    Migrate to base_bundle_id everywhere

--- Scripts/select_variant.sh
+++ Scripts/select_variant.sh
@@ -21,35 +21,8 @@ variant=$1
 
 echo "🧱 Setting build variant to ${bold}${variant}${normal}."
 
-entitlements_file="Bitwarden/Application/Support/Bitwarden.entitlements"
-extension_entitlements_file="BitwardenActionExtension/Application/Support/BitwardenActionExtension.entitlements"
-autofill_entitlements_file="BitwardenAutoFillExtension/Application/Support/BitwardenAutoFill.entitlements"
-share_entitlements_file="BitwardenShareExtension/Application/Support/BitwardenShareExtension.entitlements"
 local_xcconfig_file="Configs/Local.xcconfig"
 export_options_file="Configs/export_options.plist"
-
-case $variant in
-    Production)
-        plutil -replace 'com\.apple\.security\.application-groups' -json '[ "group.$(ORGANIZATION_IDENTIFIER).bitwarden" ]' "${entitlements_file}"
-        plutil -replace 'keychain-access-groups' -json '[ "$(AppIdentifierPrefix)$(ORGANIZATION_IDENTIFIER).bitwarden" ]' "${entitlements_file}"
-        plutil -replace 'com\.apple\.security\.application-groups' -json '[ "group.$(ORGANIZATION_IDENTIFIER).bitwarden" ]' "${extension_entitlements_file}"
-        plutil -replace 'keychain-access-groups' -json '[ "$(AppIdentifierPrefix)$(ORGANIZATION_IDENTIFIER).bitwarden" ]' "${extension_entitlements_file}"
-        plutil -replace 'com\.apple\.security\.application-groups' -json '[ "group.$(ORGANIZATION_IDENTIFIER).bitwarden" ]' "${autofill_entitlements_file}"
-        plutil -replace 'keychain-access-groups' -json '[ "$(AppIdentifierPrefix)$(ORGANIZATION_IDENTIFIER).bitwarden" ]' "${autofill_entitlements_file}"
-        plutil -replace 'com\.apple\.security\.application-groups' -json '[ "group.$(ORGANIZATION_IDENTIFIER).bitwarden" ]' "${share_entitlements_file}"
-        plutil -replace 'keychain-access-groups' -json '[ "$(AppIdentifierPrefix)$(ORGANIZATION_IDENTIFIER).bitwarden" ]' "${share_entitlements_file}"
-        ;;
-    Beta)
-        plutil -replace 'com\.apple\.security\.application-groups' -json '[ "group.$(ORGANIZATION_IDENTIFIER).bitwarden.beta" ]' "${entitlements_file}"
-        plutil -replace 'keychain-access-groups' -json '[ "$(AppIdentifierPrefix)$(ORGANIZATION_IDENTIFIER).bitwarden.beta" ]' "${entitlements_file}"
-        plutil -replace 'com\.apple\.security\.application-groups' -json '[ "group.$(ORGANIZATION_IDENTIFIER).bitwarden.beta" ]' "${extension_entitlements_file}"
-        plutil -replace 'keychain-access-groups' -json '[ "$(AppIdentifierPrefix)$(ORGANIZATION_IDENTIFIER).bitwarden.beta" ]' "${extension_entitlements_file}"
-        plutil -replace 'com\.apple\.security\.application-groups' -json '[ "group.$(ORGANIZATION_IDENTIFIER).bitwarden.beta" ]' "${autofill_entitlements_file}"
-        plutil -replace 'keychain-access-groups' -json '[ "$(AppIdentifierPrefix)$(ORGANIZATION_IDENTIFIER).bitwarden.beta" ]' "${autofill_entitlements_file}"
-        plutil -replace 'com\.apple\.security\.application-groups' -json '[ "group.$(ORGANIZATION_IDENTIFIER).bitwarden.beta" ]' "${share_entitlements_file}"
-        plutil -replace 'keychain-access-groups' -json '[ "$(AppIdentifierPrefix)$(ORGANIZATION_IDENTIFIER).bitwarden.beta" ]' "${share_entitlements_file}"
-        ;;
-esac
 
 case $variant in
     Production)
