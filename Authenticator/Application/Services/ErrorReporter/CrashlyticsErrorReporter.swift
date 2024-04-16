6fcc415fff99c1cc550564df17ba8ff30ace0bf9
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-04-15 19:50:11 -0500

    Include Crashlytics (#37)

---
+++ Authenticator/Application/Services/ErrorReporter/CrashlyticsErrorReporter.swift
@@ -0,0 +1,33 @@
+import AuthenticatorShared
+import FirebaseCore
+import FirebaseCrashlytics
+
+/// An `ErrorReporter` that logs non-fatal errors to Crashlytics for investigation.
+///
+final class CrashlyticsErrorReporter: ErrorReporter {
+    // MARK: ErrorReporter Properties
+
+    var isEnabled: Bool {
+        get { Crashlytics.crashlytics().isCrashlyticsCollectionEnabled() }
+        set {
+            Crashlytics.crashlytics().setCrashlyticsCollectionEnabled(newValue)
+        }
+    }
+
+    // MARK: Initialization
+
+    /// Initialize the `CrashlyticsErrorReporter`.
+    ///
+    init() {
+        FirebaseApp.configure()
+    }
+
+    // MARK: ErrorReporter
+
+    func log(error: Error) {
+        // Don't log networking related errors to Crashlytics.
+        guard !error.isNetworkingError else { return }
+
+        Crashlytics.crashlytics().record(error: error)
+    }
+}
