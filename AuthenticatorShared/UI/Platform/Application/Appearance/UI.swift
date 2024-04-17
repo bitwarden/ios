d86a05b3a11c35c3ef6f21a59d0cff4c8d2fd2b8
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-04-17 17:09:13 -0500

    Bring in strings files for other languages (#44)

--- AuthenticatorShared/UI/Platform/Application/Appearance/UI.swift
+++ AuthenticatorShared/UI/Platform/Application/Appearance/UI.swift
@@ -82,7 +82,12 @@ public enum UI {
 
     /// Override SwiftGen's lookup function in order to determine the language manually.
     public static func localizationFunction(key: String, table: String, fallbackValue: String) -> String {
-        Bundle.main.localizedString(forKey: key, value: fallbackValue, table: table)
+        if let languageCode = initialLanguageCode,
+           let path = Bundle(for: AppProcessor.self).path(forResource: languageCode, ofType: "lproj"),
+           let bundle = Bundle(path: path) {
+            return bundle.localizedString(forKey: key, value: fallbackValue, table: table)
+        }
+        return Bundle.main.localizedString(forKey: key, value: fallbackValue, table: table)
     }
 }
 
