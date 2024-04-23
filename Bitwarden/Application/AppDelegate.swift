e6ff83cb22796becb24d5c22f126f832c706caf6
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-04-23 11:14:46 -0500

    Handle background fetch push notifications

--- Bitwarden/Application/AppDelegate.swift
+++ Bitwarden/Application/AppDelegate.swift
@@ -1,4 +1,5 @@
 import BitwardenShared
+import OSLog
 import UIKit
 
 /// A protocol for an `AppDelegate` that can be used by the `SceneDelegate` to look up the
@@ -54,12 +55,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
 
     /// Successfully registered for push notifications.
     func application(_: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        Logger.application.log("Did Register with device token \(token, privacy: .public)")
         appProcessor?.didRegister(withToken: deviceToken)
     }
 
     /// Record an error if registering for push notifications failed.
     func application(_: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        Logger.application.log("Failed to register remote notifications with error \(error, privacy: .public)")
         appProcessor?.failedToRegister(error)
+    }
+
+    func application(
+        _ application: UIApplication,
+        didReceiveRemoteNotification userInfo: [AnyHashable: Any]
+    ) async -> UIBackgroundFetchResult {
+        Logger.application.log("Did recieve fetch notification \(userInfo, privacy: .public)")
+        await appProcessor?.messageReceived(userInfo)
+        return .newData
     }
 
     /// Received a response to a push notification alert.
@@ -67,6 +80,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         _: UNUserNotificationCenter,
         didReceive response: UNNotificationResponse
     ) async {
+        Logger.application.log("Did receive notification \(response, privacy: .public)")
         await appProcessor?.messageReceived(
             response.notification.request.content.userInfo,
             notificationDismissed: response.actionIdentifier == UNNotificationDismissActionIdentifier,
@@ -79,6 +93,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         _: UNUserNotificationCenter,
         willPresent notification: UNNotification
     ) async -> UNNotificationPresentationOptions {
+        Logger.application.log("Did receive message \(notification, privacy: .public))")
         await appProcessor?.messageReceived(notification.request.content.userInfo)
         return .banner
     }
