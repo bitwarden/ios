5e75b8206261b6607aaf014f2ca93c38b4398ec1
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-05-06 12:58:25 -0500

    Add additional tests for feature flag types

--- BitwardenShared/Core/Platform/Services/ConfigServiceTests.swift
+++ BitwardenShared/Core/Platform/Services/ConfigServiceTests.swift
@@ -136,4 +136,68 @@ final class ConfigServiceTests: BitwardenTestCase {
         let value = await subject.getFeatureFlag(.unassignedItemsBanner, defaultValue: true, forceRefresh: false)
         XCTAssertTrue(value)
     }
+
+    /// `getFeatureFlag(:)` can return an integer if it's in the configuration
+    func test_getFeatureFlag_int_exists() async {
+        stateService.serverConfig["1"] = ServerConfig(
+            date: Date(year: 2024, month: 2, day: 14, hour: 7, minute: 50, second: 0),
+            responseModel: ConfigResponseModel(
+                environment: nil,
+                featureStates: ["unassigned-items-banner": .int(42)],
+                gitHash: "75238191",
+                server: nil,
+                version: "2024.4.0"
+            )
+        )
+        let value = await subject.getFeatureFlag(.unassignedItemsBanner, defaultValue: 30, forceRefresh: false)
+        XCTAssertEqual(value, 42)
+    }
+
+    /// `getFeatureFlag(:)` returns the default value for integers
+    func test_getFeatureFlag_int_fallback() async {
+        stateService.serverConfig["1"] = ServerConfig(
+            date: Date(year: 2024, month: 2, day: 14, hour: 7, minute: 50, second: 0),
+            responseModel: ConfigResponseModel(
+                environment: nil,
+                featureStates: [:],
+                gitHash: "75238191",
+                server: nil,
+                version: "2024.4.0"
+            )
+        )
+        let value = await subject.getFeatureFlag(.unassignedItemsBanner, defaultValue: 30, forceRefresh: false)
+        XCTAssertEqual(value, 30)
+    }
+
+    /// `getFeatureFlag(:)` can return a string if it's in the configuration
+    func test_getFeatureFlag_string_exists() async {
+        stateService.serverConfig["1"] = ServerConfig(
+            date: Date(year: 2024, month: 2, day: 14, hour: 7, minute: 50, second: 0),
+            responseModel: ConfigResponseModel(
+                environment: nil,
+                featureStates: ["unassigned-items-banner": .string("exists")],
+                gitHash: "75238191",
+                server: nil,
+                version: "2024.4.0"
+            )
+        )
+        let value = await subject.getFeatureFlag(.unassignedItemsBanner, defaultValue: "fallback", forceRefresh: false)
+        XCTAssertEqual(value, "exists")
+    }
+
+    /// `getFeatureFlag(:)` returns the default value for booleans
+    func test_getFeatureFlag_string_fallback() async {
+        stateService.serverConfig["1"] = ServerConfig(
+            date: Date(year: 2024, month: 2, day: 14, hour: 7, minute: 50, second: 0),
+            responseModel: ConfigResponseModel(
+                environment: nil,
+                featureStates: [:],
+                gitHash: "75238191",
+                server: nil,
+                version: "2024.4.0"
+            )
+        )
+        let value = await subject.getFeatureFlag(.unassignedItemsBanner, defaultValue: "fallback", forceRefresh: false)
+        XCTAssertEqual(value, "fallback")
+    }
 }
