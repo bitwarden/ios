6e617b0a7bff418eb7c46df589197242bc8422e9
Matt Czech <matt@livefront.com>
2024-05-14 12:14:40 -0500

    BIT-2352: Fix TDE device key name for migration

--- BitwardenShared/Core/Auth/Services/KeychainRepository.swift
+++ BitwardenShared/Core/Auth/Services/KeychainRepository.swift
@@ -46,7 +46,7 @@ enum KeychainItem: Equatable {
         case let .biometrics(userId: id):
             "biometric_key_" + id
         case let .deviceKey(userId: id):
-            "device_key_" + id
+            "deviceKey_" + id
         case let .neverLock(userId: id):
             "userKeyAutoUnlock_" + id
         case let .pendingAdminLoginRequest(userId):
