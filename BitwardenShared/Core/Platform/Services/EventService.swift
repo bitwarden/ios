70e96020df21dc5206bff006c9308508565d5e8e
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-06-18 11:34:26 -0500

    Create collect method

--- BitwardenShared/Core/Platform/Services/EventService.swift
+++ BitwardenShared/Core/Platform/Services/EventService.swift
@@ -6,7 +6,23 @@ import Foundation
 /// and uploading those events.
 ///
 protocol EventService {
+    /// Save an event to disk for future upload.
+    ///
+    /// - Parameters:
+    ///   - eventType: The event to track.
+    ///   - cipherId: The ID of the relevant cipher for some events.
+    ///   - uploadImmediately: If `true` then immediately attempts an upload after saving.
+    func collect(eventType: EventType, cipherId: String?, uploadImmediately: Bool) async
+}
 
+extension EventService {
+    func collect(
+        eventType: EventType,
+        cipherId: String? = nil,
+        uploadImmediately: Bool = false
+    ) async {
+        await collect(eventType: eventType, cipherId: cipherId, uploadImmediately: uploadImmediately)
+    }
 }
 
 // MARK: - DefaultEventService
@@ -19,4 +35,6 @@ class DefaultEventService: EventService {
     // MARK: Initialization
 
     // MARK: Methods
+
+    func collect(eventType: EventType, cipherId: String?, uploadImmediately: Bool) async {}
 }
--- BitwardenShared/Core/Platform/Services/TestHelpers/MockEventService.swift
+++ BitwardenShared/Core/Platform/Services/TestHelpers/MockEventService.swift
@@ -3,5 +3,13 @@ import Foundation
 @testable import BitwardenShared
 
 class MockEventService: EventService {
-    
+    var collectEventType: EventType?
+    var collectCipherId: String?
+    var collectUploadImmediately: Bool?
+
+    func collect(eventType: EventType, cipherId: String?, uploadImmediately: Bool) async {
+        collectEventType = eventType
+        collectCipherId = cipherId
+        collectUploadImmediately = uploadImmediately
+    }
 }
