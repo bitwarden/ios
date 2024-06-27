792cad457aff83d4b49a8a3d582c726aaac4d55e
Matt Czech <matt@livefront.com>
2024-06-27 11:51:53 -0500

    Fix swiftlint warnings and disable non_optional_string_data_conversion

--- BitwardenShared/UI/Vault/Views/VaultListItemRow/VaultListItemRowView.swift
+++ BitwardenShared/UI/Vault/Views/VaultListItemRow/VaultListItemRowView.swift
@@ -85,6 +85,7 @@ struct VaultListItemRowView: View {
                         Text("\(count)")
                             .styleGuide(.body)
                             .foregroundColor(Asset.Colors.textSecondary.swiftUIColor)
+
                     case let .totp(_, model):
                         totpCodeRow(model)
                     }
