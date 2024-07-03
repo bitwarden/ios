37c032823c188604323873dd12fd3963a731c4c2
André Bispo <abispo@bitwarden.com>
2024-07-03 11:46:08 +0100

    [PM-1528] Remove unused code

--- Bitwarden/Application/AppDelegate.swift
+++ Bitwarden/Application/AppDelegate.swift
@@ -70,36 +70,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         return .newData
     }
 
-    /// Handle universal links.
-    func application(_ application: UIApplication,
-                     continue userActivity: NSUserActivity,
-                     restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        // Get URL components from the incoming user activity.
-        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb,
-              let incomingURL = userActivity.webpageURL,
-              let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true) else {
-            return false
-        }
-
-        // Check for specific URL components that you need.
-        guard let path = components.path,
-              let params = components.queryItems else {
-            return false
-        }
-        print("path = \(path)")
-
-        if let albumName = params.first(where: { $0.name == "albumname" })?.value,
-           let photoIndex = params.first(where: { $0.name == "index" })?.value {
-            print("album = \(albumName)")
-            print("photoIndex = \(photoIndex)")
-            return true
-
-        } else {
-            print("Either album name or photo index missing")
-            return false
-        }
-    }
-
     /// Received a response to a push notification alert.
     func userNotificationCenter(
         _: UNUserNotificationCenter,
