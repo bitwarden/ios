879c74739fd1c39801eb8884c40be80489b81240
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-07-09 12:54:24 -0500

    Expand event request

--- BitwardenShared/Core/Platform/Services/API/Requests/EventRequest.swift
+++ BitwardenShared/Core/Platform/Services/API/Requests/EventRequest.swift
@@ -1,4 +1,20 @@
+import Foundation
 import Networking
+
+// MARK: - EventRequestModel
+
+/// Data model for the body of an event upload.
+///
+struct EventRequestModel: JSONRequestBody {
+    /// The type of event.
+    let type: EventType
+
+    /// The ID of the cipher related to the event, if there is one.
+    let cipherId: String?
+
+    /// The time when the event occurred.
+    let date: Date
+}
 
 // MARK: - EventRequest
 
@@ -6,9 +22,17 @@ import Networking
 struct EventRequest: Request {
     typealias Response = EmptyResponse
 
-    let body: EventData
+    // MARK: Properties
 
+    /// The body of the request.
+    var body: EventRequestModel? { requestBody }
+
+    /// The HTTP method of the request.
     let method: HTTPMethod = .post
 
+    /// The URL path of this request.
     let path = "/collect"
+
+    /// The actual body of the request.
+    let requestBody: EventRequestModel
 }
