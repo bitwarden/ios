6ef0a2627329cdcd8946e1d9266f810f28d4f881
Philip Cappelli <phil@livefront.com>
2024-07-19 14:10:37 -0400

    PR comments

--- BitwardenShared/Core/Vault/Services/TOTP/TOTPKeyModel.swift
+++ BitwardenShared/Core/Vault/Services/TOTP/TOTPKeyModel.swift
@@ -3,20 +3,9 @@
 public struct TOTPKeyModel: Equatable, Sendable {
     // MARK: Properties
 
-    /// The authenticatorKey used to generate the `TOTPCodeConfig`.
-    let rawAuthenticatorKey: String
-
-    /// The key type used for generating the TOTP code.
-    let totpKey: TOTPKey
-
     /// The hash algorithm used for the TOTP code.
     var algorithm: TOTPCryptoHashAlgorithm {
         totpKey.algorithm
-    }
-
-    /// The key used to generate the TOTP code.
-    var key: String {
-        totpKey.key
     }
 
     /// The number of digits in the TOTP code.
@@ -24,10 +13,21 @@ public struct TOTPKeyModel: Equatable, Sendable {
         totpKey.digits
     }
 
+    /// The key used to generate the TOTP code.
+    var key: String {
+        totpKey.key
+    }
+
     /// The time period (in seconds) for which the TOTP code is valid.
     var period: Int {
         totpKey.period
     }
+
+    /// The authenticatorKey used to generate the `TOTPCodeConfig`.
+    let rawAuthenticatorKey: String
+
+    /// The key type used for generating the TOTP code.
+    let totpKey: TOTPKey
 
     // MARK: Initializers
 
