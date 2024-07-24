f6aeb5f5f3465354732ef65be53d4da1c5a265f8
André Bispo <abispo@bitwarden.com>
2024-07-24 16:32:58 +0100

    [PM-9842] Add expired link view

---
+++ BitwardenShared/UI/Auth/StartRegistration/ExpiredLink/ExpiredLinkState.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+// MARK: - ExpiredLinkState
+
+/// An object that defines the current state of a `ExpiredLinkView`.
+///
+struct ExpiredLinkState: Equatable {
+    // MARK: Properties
+
+    /// User's email address.
+    var email: String = ""
+    
+}
