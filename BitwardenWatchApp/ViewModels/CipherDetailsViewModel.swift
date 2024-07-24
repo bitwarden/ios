a8af508a80594cbedd1f8bea066ae0773cb9b91b
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-07-24 13:08:36 -0500

    Introduce crash

--- BitwardenWatchApp/ViewModels/CipherDetailsViewModel.swift
+++ BitwardenWatchApp/ViewModels/CipherDetailsViewModel.swift
@@ -22,6 +22,7 @@ class CipherDetailsViewModel: ObservableObject {
     }
 
     func startGeneration() {
+        fatalError("This is a test")
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] t in
             guard let self else {
                 t.invalidate()
