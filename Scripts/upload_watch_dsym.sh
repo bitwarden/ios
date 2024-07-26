dfed49a941a014ee73245e857bf5881e1cf32f01
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-07-26 10:54:22 -0500

    Use iOS platform

--- Scripts/upload_watch_dsym.sh
+++ Scripts/upload_watch_dsym.sh
@@ -6,5 +6,5 @@ if [ "$CONFIGURATION" != "Debug" ]; then
     find "${DWARF_DSYM_FOLDER_PATH}" -name "*.dSYM" \
         -exec "${BUILD_DIR%Build/*}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" \
         -gsp "${PROJECT_DIR}/BitwardenWatchApp/GoogleService-Info.plist" \
-        -p watchos -- {} +
+        -p ios -- {} +
 fi
