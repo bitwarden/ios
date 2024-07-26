bf62eb68d7c703c985985d6a0ab0e6804f2cbb83
André Bispo <andrefsbispo@hotmail.com>
2024-07-26 16:08:23 +0100

    [PM-9932] Update copy for marketing emails consent (#756)

--- BitwardenShared/UI/Auth/StartRegistration/StartRegistrationState.swift
+++ BitwardenShared/UI/Auth/StartRegistration/StartRegistrationState.swift
@@ -18,7 +18,7 @@ struct StartRegistrationState: Equatable {
 
     /// The text in the receive marketing emails toggle
     var receiveMarketingEmailsText: String {
-        Localizations.getEmailsFromBitwardenForAnnouncementsAdviceAndResearchOpportunitiesUnsubscribeAtAnyTime(
+        Localizations.getAdviceAnnouncementsAndResearchOpportunitiesFromBitwardenInYourInboxUnsubscribeAtAnyTime(
             ExternalLinksConstants.unsubscribeFromMarketingEmails
         )
     }
