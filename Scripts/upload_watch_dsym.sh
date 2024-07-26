67da27dfa6c8578f3a883cbb1dc0aaf53d91ccc9
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-07-26 10:42:06 -0500

    Upload dsym on build

---
+++ Scripts/upload_watch_dsym.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ "$CONFIGURATION" != "Debug" ]; then
+    find "${DWARF_DSYM_FOLDER_PATH}" -name "*.dSYM" \
+        -exec "${BUILD_DIR%Build/*}/SourcePackages/checkouts/firebase-ios-sdk/Crashlytics/upload-symbols" \
+        -gsp "${PROJECT_DIR}/BitwardenWatchApp/GoogleService-Info.plist" \
+        -p watchos -- {} +
+fi
