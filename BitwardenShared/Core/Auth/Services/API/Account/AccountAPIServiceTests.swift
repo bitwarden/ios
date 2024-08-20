ce12d71c1948adfbf3e4c29d7f4519810ca659d8
André Bispo <abispo@bitwarden.com>
2024-08-20 21:13:40 +0100

    [PM-9842] Added test for http failure

--- BitwardenShared/Core/Auth/Services/API/Account/AccountAPIServiceTests.swift
+++ BitwardenShared/Core/Auth/Services/API/Account/AccountAPIServiceTests.swift
@@ -4,6 +4,7 @@ import XCTest
 
 // MARK: - AccountAPIServiceTests
 
+// swiftlint:disable file_length
 class AccountAPIServiceTests: BitwardenTestCase { // swiftlint:disable:this type_body_length
     // MARK: Properties
 
@@ -387,7 +388,7 @@ class AccountAPIServiceTests: BitwardenTestCase { // swiftlint:disable:this type
     }
 
     /// `verifyEmailToken()` performs a request to verify if the verification token received by email is still valid.
-    func verifyEmailToken() async throws {
+    func test_verifyEmailToken() async throws {
         client.result = .httpSuccess(testData: .emptyResponse)
 
         try await subject.verifyEmailToken(
@@ -402,6 +403,18 @@ class AccountAPIServiceTests: BitwardenTestCase { // swiftlint:disable:this type
             client.requests[0].url.absoluteString,
             "https://example.com/identity/accounts/register/verification-email-clicked"
         )
+    }
+
+    /// `verifyEmailToken()` throws error when call fails.
+    func test_verifyEmailToken_httpFailure() async throws {
+        client.result = .httpFailure()
+
+        await assertAsyncThrows {
+            try await subject.verifyEmailToken(
+                email: "example@email.com",
+                emailVerificationToken: "verification-token"
+            )
+        }
     }
 
     /// `verifyOtp()` performs a request to verify a one-time password for the user.
