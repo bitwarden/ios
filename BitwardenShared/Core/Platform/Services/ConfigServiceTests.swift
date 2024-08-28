be051b1e7b705cf47282b93c0c2c5f9a491f4bfe
Federico Maccaroni <fedemkr@gmail.com>
2024-08-28 16:37:47 -0300

    PM-11420 Moved getting config from server to a background task when is not being forced to be refreshed. This was causing the app to appear as frozen on loading because of the call to the getConfig from the AppProcessor and also other places to take longer to load when getting feature flags.

--- BitwardenShared/Core/Platform/Services/ConfigServiceTests.swift
+++ BitwardenShared/Core/Platform/Services/ConfigServiceTests.swift
@@ -62,7 +62,8 @@ final class ConfigServiceTests: BitwardenTestCase {
     }
 
     /// `getConfig(:)` uses the local configuration if it is expired
-    func test_getConfig_local_expired() async {
+    /// but updates the local config when the http request finishes.
+    func test_getConfig_local_expired() async throws {
         stateService.serverConfig["1"] = ServerConfig(
             date: Date(year: 2024, month: 2, day: 10, hour: 8, minute: 0, second: 0),
             responseModel: ConfigResponseModel(
@@ -75,8 +76,13 @@ final class ConfigServiceTests: BitwardenTestCase {
         )
         client.result = .httpSuccess(testData: .validServerConfig)
         let response = await subject.getConfig(forceRefresh: false)
-        XCTAssertEqual(client.requests.count, 1)
-        XCTAssertEqual(response?.gitHash, "75238191")
+        XCTAssertEqual(response?.gitHash, "75238192")
+
+        try await waitForAsync {
+            self.client.requests.count == 1
+        }
+
+        XCTAssertEqual(stateService.serverConfig["1"]?.gitHash, "75238191")
     }
 
     /// `getConfig(:)` uses the local configuration if it's not expired
@@ -97,12 +103,18 @@ final class ConfigServiceTests: BitwardenTestCase {
     }
 
     /// `getConfig(:)` gets the configuration from the server if there is no local configuration
-    func test_getConfig_noLocal() async {
+    ///  but in background returning nil to the caller as is the current available local config.
+    func test_getConfig_noLocal() async throws {
         client.result = .httpSuccess(testData: .validServerConfig)
         let response = await subject.getConfig(forceRefresh: false)
-        XCTAssertEqual(client.requests.count, 1)
-        XCTAssertEqual(response?.gitHash, "75238191")
-        XCTAssertEqual(response?.featureStates[.testRemoteFeatureFlag], .bool(true))
+        XCTAssertNil(response)
+
+        try await waitForAsync {
+            self.client.requests.count == 1
+        }
+
+        XCTAssertEqual(stateService.serverConfig["1"]?.gitHash, "75238191")
+        XCTAssertEqual(stateService.serverConfig["1"]?.featureStates[.testRemoteFeatureFlag], .bool(true))
     }
 
     /// `getFeatureFlag(:)` can return a boolean if it's in the configuration
