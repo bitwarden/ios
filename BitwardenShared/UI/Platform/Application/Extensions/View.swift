864ae8ed843368cf5b2a105f801d3256499b7b18
Federico Maccaroni <fedemkr@gmail.com>
2024-09-10 14:01:59 -0300

    [PM-11672] Fix allow copy/share on notes field in cipher item view (#912)

--- BitwardenShared/UI/Platform/Application/Extensions/View.swift
+++ BitwardenShared/UI/Platform/Application/Extensions/View.swift
@@ -76,6 +76,20 @@ extension View {
         }
     }
 
+    /// Conditionally applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder
+    func `if`<Content: View>(_ condition: @autoclosure () -> Bool, transform: (Self) -> Content) -> some View {
+        if condition() {
+            transform(self)
+        } else {
+            self
+        }
+    }
+
     /// Applies a custom navigation bar title and title display mode to a view.
     ///
     /// - Parameters:
