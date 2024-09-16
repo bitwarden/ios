846669a96cfdaa1086e8dcc259d1fa2bf24a08b2
Matt Czech <matt@livefront.com>
2024-09-16 14:36:13 -0500

    PM-12243: Fix organization permissions JSON decoding errors for removed properties (#934)

--- BitwardenShared/Core/Vault/Models/API/Permissions.swift
+++ BitwardenShared/Core/Vault/Models/API/Permissions.swift
@@ -3,47 +3,11 @@
 struct Permissions: Codable, Equatable, Hashable {
     // MARK: Properties
 
-    /// Whether the user can access event logs.
-    let accessEventLogs: Bool
-
-    /// Whether the user can access import and export.
-    let accessImportExport: Bool
-
-    /// Whether the user can access reports.
-    let accessReports: Bool
-
-    /// Whether the user can create new collections.
-    let createNewCollections: Bool
-
-    /// Whether the user can delete any collection.
-    let deleteAnyCollection: Bool
-
-    /// Whether the user can delete assigned collections.
-    let deleteAssignedCollections: Bool
-
-    /// Whether the user can edit any collection.
-    let editAnyCollection: Bool
-
-    /// Whether the user can edit assigned collections.
-    let editAssignedCollections: Bool
-
-    /// Whether the user can manage groups.
-    let manageGroups: Bool
-
     /// Whether the user can manage policies.
     let managePolicies: Bool
 
     /// Whether the user can manage reset password.
     let manageResetPassword: Bool
-
-    /// Whether the user can manage SCIM.
-    let manageScim: Bool
-
-    /// Whether the user can manage SSO.
-    let manageSso: Bool
-
-    /// Whether the user can manager users.
-    let manageUsers: Bool
 }
 
 extension Permissions {
@@ -51,20 +15,8 @@ extension Permissions {
     ///
     init() {
         self.init(
-            accessEventLogs: false,
-            accessImportExport: false,
-            accessReports: false,
-            createNewCollections: false,
-            deleteAnyCollection: false,
-            deleteAssignedCollections: false,
-            editAnyCollection: false,
-            editAssignedCollections: false,
-            manageGroups: false,
             managePolicies: false,
-            manageResetPassword: false,
-            manageScim: false,
-            manageSso: false,
-            manageUsers: false
+            manageResetPassword: false
         )
     }
 }
