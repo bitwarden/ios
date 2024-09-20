8474fd80fe5dc1ba7ee97c599a70b17c95c0223d
Philip Cappelli <phil@livefront.com>
2024-09-20 15:48:46 -0400

    remove unused code from DebugMenuState

--- BitwardenShared/UI/Platform/DebugMenu/DebugMenuState.swift
+++ BitwardenShared/UI/Platform/DebugMenu/DebugMenuState.swift
@@ -7,9 +7,4 @@ import Foundation
 struct DebugMenuState: Equatable, Sendable {
     /// The current feature flags supported.
     var featureFlags: [DebugMenuFeatureFlag] = []
-
-    /// Computed property to get the isEnabled value for a given feature flag
-    func isEnabled(for feature: FeatureFlag) -> Bool {
-        featureFlags.first { $0.feature == feature }?.isEnabled ?? false
-    }
 }
