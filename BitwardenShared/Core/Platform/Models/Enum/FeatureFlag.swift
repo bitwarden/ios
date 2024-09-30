60c6c57843d063659aa32c1b607fd135e86abe26
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-09-30 14:48:32 -0500

    [PM-10599] Merge FeatureFlagConstants into FeatureFlags (#976)

--- BitwardenShared/Core/Platform/Models/Enum/FeatureFlag.swift
+++ BitwardenShared/Core/Platform/Models/Enum/FeatureFlag.swift
@@ -4,13 +4,16 @@ import Foundation
 
 /// An enum to represent a feature flag sent by the server
 ///
-enum FeatureFlag: String, Codable {
+enum FeatureFlag: String, CaseIterable, Codable {
     /// Flag to enable/disable email verification during registration
     /// This flag introduces a new flow for account creation
     case emailVerification = "email-verification"
 
     /// Flag to enable/disable the ability to sync TOTP codes with the Authenticator app.
     case enableAuthenticatorSync = "enable-authenticator-sync-ios"
+
+    /// A flag that enables individual cipher encryption.
+    case enableCipherKeyEncryption
 
     /// A feature flag for the intro carousel flow.
     case nativeCarouselFlow = "native-carousel-flow"
@@ -20,23 +23,50 @@ enum FeatureFlag: String, Codable {
 
     // MARK: Test Flags
 
-    /// A test feature flag that isn't remotely configured.
+    /// A test feature flag that isn't remotely configured and has no initial value.
     case testLocalFeatureFlag = "test-local-feature-flag"
+
+    /// A test feature flag that has an initial boolean value and is not remotely configured.
+    case testLocalInitialBoolFlag = "test-local-initial-bool-flag"
+
+    /// A test feature flag that has an initial integer value and is not remotely configured.
+    case testLocalInitialIntFlag = "test-local-initial-int-flag"
+
+    /// A test feature flag that has an initial string value and is not remotely configured.
+    case testLocalInitialStringFlag = "test-local-initial-string-flag"
 
     /// A test feature flag that can be remotely configured.
     case testRemoteFeatureFlag = "test-remote-feature-flag"
+
+    /// A test feature flag that has an initial boolean value and is not remotely configured.
+    case testRemoteInitialBoolFlag = "test-remote-initial-bool-flag"
+
+    /// A test feature flag that has an initial integer value and is not remotely configured.
+    case testRemoteInitialIntFlag = "test-remote-initial-int-flag"
+
+    /// A test feature flag that has an initial string value and is not remotely configured.
+    case testRemoteInitialStringFlag = "test-remote-initial-string-flag"
 
     // MARK: Type Properties
 
     /// An array of feature flags available in the debug menu.
     static var debugMenuFeatureFlags: [FeatureFlag] {
-        [
-            .emailVerification,
-            .enableAuthenticatorSync,
-            .nativeCarouselFlow,
-            .nativeCreateAccountFlow,
-        ]
+        allCases.filter { !$0.rawValue.hasPrefix("test-") }
+            .filter { $0 != .enableCipherKeyEncryption }
     }
+
+    /// The initial values for feature flags.
+    /// If `isRemotelyConfigured` is true for the flag, then this will get overridden by the server;
+    /// but if `isRemotelyConfigured` is false for the flag, then the value here will be used.
+    /// This is a helpful way to manage local feature flags.
+    static let initialValues: [FeatureFlag: AnyCodable] = [
+        .testLocalInitialBoolFlag: .bool(true),
+        .testLocalInitialIntFlag: .int(42),
+        .testLocalInitialStringFlag: .string("Test String"),
+        .testRemoteInitialBoolFlag: .bool(true),
+        .testRemoteInitialIntFlag: .int(42),
+        .testRemoteInitialStringFlag: .string("Test String"),
+    ]
 
     // MARK: Instance Properties
 
@@ -44,31 +74,25 @@ enum FeatureFlag: String, Codable {
     var isRemotelyConfigured: Bool {
         switch self {
         case .enableAuthenticatorSync,
+             .enableCipherKeyEncryption,
              .nativeCarouselFlow,
              .nativeCreateAccountFlow,
-             .testLocalFeatureFlag:
+             .testLocalFeatureFlag,
+             .testLocalInitialBoolFlag,
+             .testLocalInitialIntFlag,
+             .testLocalInitialStringFlag:
             false
         case .emailVerification,
-             .testRemoteFeatureFlag:
+             .testRemoteFeatureFlag,
+             .testRemoteInitialBoolFlag,
+             .testRemoteInitialIntFlag,
+             .testRemoteInitialStringFlag:
             true
         }
     }
 
     /// The display name of the feature flag.
     var name: String {
-        switch self {
-        case .emailVerification:
-            "Email Verification"
-        case .enableAuthenticatorSync:
-            "Enable Authenticator Sync"
-        case .nativeCarouselFlow:
-            "Native Carousel Flow"
-        case .nativeCreateAccountFlow:
-            "Native Create Account Flow"
-        case .testLocalFeatureFlag:
-            "Test Local Feature Flag"
-        case .testRemoteFeatureFlag:
-            "Test Remote Feature Flag"
-        }
+        rawValue.split(separator: "-").map(\.localizedCapitalized).joined(separator: " ")
     }
 }
