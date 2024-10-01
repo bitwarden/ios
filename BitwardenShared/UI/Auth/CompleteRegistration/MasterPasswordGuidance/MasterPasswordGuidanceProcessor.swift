7f5bfb05eb4133b1909fd1d1442736a59d8b9e31
Phil Cappelli <150719757+phil-livefront@users.noreply.github.com>
2024-10-01 12:59:00 -0400

    PM-10676 - Fill in generated passphrase (#991)

--- BitwardenShared/UI/Auth/CompleteRegistration/MasterPasswordGuidance/MasterPasswordGuidanceProcessor.swift
+++ BitwardenShared/UI/Auth/CompleteRegistration/MasterPasswordGuidance/MasterPasswordGuidanceProcessor.swift
@@ -12,14 +12,23 @@ class MasterPasswordGuidanceProcessor: StateProcessor<
     /// The coordinator that handles navigation.
     private let coordinator: AnyCoordinator<AuthRoute, AuthEvent>
 
+    /// The delegate used to communiate saving a new generated password.
+    private weak var delegate: MasterPasswordUpdateDelegate?
+
     // MARK: Initialization
 
     /// Creates a new `CompleteRegistrationProcessor`.
     ///
-    /// - Parameter coordinator: The coordinator that handles navigation.
+    /// - Parameters:
+    ///   - coordinator: The coordinator that handles navigation.
+    ///   - delegate: The delegate for the processor to notifiy saving a generated password.
     ///
-    init(coordinator: AnyCoordinator<AuthRoute, AuthEvent>) {
+    init(
+        coordinator: AnyCoordinator<AuthRoute, AuthEvent>,
+        delegate: MasterPasswordUpdateDelegate?
+    ) {
         self.coordinator = coordinator
+        self.delegate = delegate
         super.init(state: ())
     }
 
@@ -30,7 +39,10 @@ class MasterPasswordGuidanceProcessor: StateProcessor<
         case .dismiss:
             coordinator.navigate(to: .dismissPresented)
         case .generatePasswordPressed:
-            coordinator.navigate(to: .masterPasswordGenerator)
+            coordinator.navigate(
+                to: .masterPasswordGenerator,
+                context: delegate
+            )
         }
     }
 }
