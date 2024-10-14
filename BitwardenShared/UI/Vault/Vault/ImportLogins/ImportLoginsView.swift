5a479640d1a780b744bd2224ef2f53bc72602a57
Matt Czech <matt@livefront.com>
2024-10-14 14:28:56 -0500

    PM-11150: Add import logins step 1 (#1031)

--- BitwardenShared/UI/Vault/Vault/ImportLogins/ImportLoginsView.swift
+++ BitwardenShared/UI/Vault/Vault/ImportLogins/ImportLoginsView.swift
@@ -13,6 +13,27 @@ struct ImportLoginsView: View {
     // MARK: View
 
     var body: some View {
+        Group {
+            switch store.state.page {
+            case .intro: intro()
+            case .step1: step1()
+            }
+        }
+        .transition(.opacity)
+        .animation(.easeInOut, value: store.state.page)
+        .navigationBar(title: Localizations.importLogins, titleDisplayMode: .inline)
+        .toolbar {
+            cancelToolbarItem {
+                store.send(.dismiss)
+            }
+        }
+    }
+
+    // MARK: Private
+
+    /// The intro page view.
+    @ViewBuilder
+    private func intro() -> some View {
         VStack(spacing: 32) {
             PageHeaderView(
                 image: Asset.Images.import,
@@ -35,20 +56,83 @@ struct ImportLoginsView: View {
         .padding(.top, 8)
         .frame(maxWidth: .infinity)
         .scrollView()
-        .navigationBar(title: Localizations.importLogins, titleDisplayMode: .inline)
-        .toolbar {
-            cancelToolbarItem {
-                store.send(.dismiss)
+    }
+
+    /// The step 1 page view.
+    @ViewBuilder
+    private func step1() -> some View {
+        stepView(step: 1, totalSteps: 3, title: Localizations.exportYourSavedLogins) {
+            NumberedListRow(title: Localizations.exportLoginsStep1)
+            NumberedListRow(title: Localizations.exportLoginsStep2)
+            NumberedListRow(
+                title: Localizations.exportLoginsStep3,
+                subtitle: Localizations.exportLoginsStep3Subtitle
+            )
+        }
+    }
+
+    /// Returns a scroll view for displaying the instructions for a step.
+    ///
+    /// - Parameters:
+    ///   - step: The step number of this page.
+    ///   - totalSteps: The total number of steps.
+    ///   - title: The title of the step.
+    ///   - list: A closure that returns the views to display in a numbered list.
+    ///
+    @ViewBuilder
+    private func stepView(
+        step: Int,
+        totalSteps: Int,
+        title: String,
+        @ViewBuilder list: () -> some View
+    ) -> some View {
+        VStack(spacing: 32) {
+            VStack(spacing: 12) {
+                Text(Localizations.stepXOfY(step, totalSteps))
+                    .styleGuide(.subheadline, weight: .bold)
+
+                Text(title)
+                    .styleGuide(.title2, weight: .bold)
+            }
+            .multilineTextAlignment(.center)
+
+            NumberedList(content: list)
+
+            Text(LocalizedStringKey(
+                Localizations.needHelpCheckOutImportHelp(ExternalLinksConstants.importHelp)
+            ))
+            .multilineTextAlignment(.center)
+            .styleGuide(.footnote)
+            .tint(Asset.Colors.textInteraction.swiftUIColor)
+
+            VStack(spacing: 12) {
+                Button(Localizations.continue) {
+                    store.send(.advanceNextPage)
+                }
+                .buttonStyle(.primary())
+
+                Button(Localizations.back) {
+                    store.send(.advancePreviousPage)
+                }
+                .buttonStyle(.transparent)
             }
         }
+        .foregroundStyle(Asset.Colors.textPrimary.swiftUIColor)
+        .frame(maxWidth: .infinity)
+        .scrollView()
     }
 }
 
 // MARK: - Previews
 
 #if DEBUG
-#Preview {
+#Preview("Intro") {
     ImportLoginsView(store: Store(processor: StateProcessor(state: ImportLoginsState())))
+        .navStackWrapped
+}
+
+#Preview("Step 1") {
+    ImportLoginsView(store: Store(processor: StateProcessor(state: ImportLoginsState(page: .step1))))
         .navStackWrapped
 }
 #endif
