e7b73f18fea871d7fe9275f83fa78b85a9be972f
Philip Cappelli <phil@livefront.com>
2024-10-30 17:09:00 -0400

    PM-12664 - Create custom UITabBarController to update icons and allow support for light/dark modes changes

--- BitwardenShared/UI/Platform/Application/AppCoordinator.swift
+++ BitwardenShared/UI/Platform/Application/AppCoordinator.swift
@@ -207,7 +207,7 @@ class AppCoordinator: Coordinator, HasRootNavigator {
             coordinator.navigate(to: route)
         } else {
             guard let rootNavigator else { return }
-            let tabNavigator = UITabBarController()
+            let tabNavigator = BitwardenTabBarController()
             let coordinator = module.makeTabCoordinator(
                 errorReporter: services.errorReporter,
                 rootNavigator: rootNavigator,
