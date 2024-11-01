c0868b5b59aeb211711745dc527255ad09b4cfcb
Philip Cappelli <phil@livefront.com>
2024-11-01 12:08:33 -0400

    add the ability to hide the scrolling indicators on scroll views and disabled on the landing view

--- BitwardenShared/UI/Auth/Landing/LandingView.swift
+++ BitwardenShared/UI/Auth/Landing/LandingView.swift
@@ -86,7 +86,7 @@ struct LandingView: View {
             }
             .padding(.bottom, 16)
             .frame(minHeight: geometry.size.height)
-            .scrollView(addVerticalPadding: false)
+            .scrollView(addVerticalPadding: false, showsIndicators: false)
         }
     }
 
