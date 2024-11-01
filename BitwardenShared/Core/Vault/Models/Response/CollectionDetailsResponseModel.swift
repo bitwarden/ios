75f498d4d6d71ba3f1d8e00406ef131612c377e3
Federico Maccaroni <fedemkr@gmail.com>
2024-11-01 12:58:09 -0300

    PM-14372 Added DefaultFalse wrapper to manage property in the collection details response model.

--- BitwardenShared/Core/Vault/Models/Response/CollectionDetailsResponseModel.swift
+++ BitwardenShared/Core/Vault/Models/Response/CollectionDetailsResponseModel.swift
@@ -15,7 +15,7 @@ struct CollectionDetailsResponseModel: Codable, Equatable {
     let id: String
 
     /// Whether the collection can be managed by the user.
-    let manage: Bool
+    @DefaultFalse var manage: Bool
 
     /// The collection's name.
     let name: String
