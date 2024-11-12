e837a612cbbb3b2fd3462a5829aac17d1b4ec879
Matt Czech <matt@livefront.com>
2024-11-12 09:26:41 -0600

    PM-14646: Fix JSON decoding errors (backport of #1122) (#1128)

--- BitwardenShared/Core/Vault/Models/Enum/SecureNoteType.swift
+++ BitwardenShared/Core/Vault/Models/Enum/SecureNoteType.swift
@@ -4,3 +4,9 @@ enum SecureNoteType: Int, Codable {
     /// A generic note.
     case generic = 0
 }
+
+// MARK: - DefaultValueProvider
+
+extension SecureNoteType: DefaultValueProvider {
+    static var defaultValue: SecureNoteType { .generic }
+}
