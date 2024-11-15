53d708aa38710626e61c4dba5a72c36227ce2613
Carlos Gonçalves <cgoncalves@bitwarden.com>
2024-11-15 10:07:45 +0000

    PM-10900 Fix PR comment

--- BitwardenShared/Core/Auth/Services/AuthService.swift
+++ BitwardenShared/Core/Auth/Services/AuthService.swift
@@ -930,12 +930,6 @@ class DefaultAuthService: AuthService { // swiftlint:disable:this type_body_leng
                 // If this error was thrown, it also means any cached two-factor token is not valid.
                 await stateService.setTwoFactorToken(nil, email: email)
             }
-            if case .twoFactorProvidersNotConfigured = error {
-                errorReporter.log(error: BitwardenError.generalError(
-                    type: "Identity Token Validation Failed.",
-                    message: "Two Factor Providers data is nil."
-                ))
-            }
             // Re-throw the error.
             throw error
         }
