56105116091a8edf6aad911ac2eabaf51ecde2d8
Katherine Bertelsen <kbertelsen@bitwarden.com>
2024-12-09 13:23:47 -0600

    WIP

--- BitwardenShared/UI/Auth/NewDeviceNotice/NewDeviceNoticeView.swift
+++ BitwardenShared/UI/Auth/NewDeviceNotice/NewDeviceNoticeView.swift
@@ -15,85 +15,6 @@ struct NewDeviceNoticeView: View {
 
     var body: some View {
         VStack(spacing: 12) {
-            pageView(
-//                IntroCarouselState.CarouselPage(
-//                    image: Asset.Images.Illustrations.items.swiftUIImage,
-//                    message: Localizations.bitwardenWillSendACodeToYourAccountEmail,
-//                    title: Localizations.importantNotice
-//                )
-            )
-//            .border(.blue)
-
-            internalCard
-                .padding(.horizontal, 12)
-                .border(.red)
-
-            VStack(spacing: 12) {
-                AsyncButton(Localizations.createAccount) {
-                    await store.perform(.continueTapped)
-                }
-                .buttonStyle(.primary())
-            }
-            .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
-            .padding(.horizontal, 16)
-            .padding(.vertical, 12)
-
-            Spacer()
-        }
-        .task {
-            await store.perform(.appeared)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Asset.Colors.backgroundPrimary.swiftUIColor.ignoresSafeArea())
-        .foregroundStyle(Asset.Colors.textPrimary.swiftUIColor)
-        .multilineTextAlignment(.center)
-    }
-
-    /// A dynamic stack view that lays out content vertically when in a regular vertical size class
-    /// and horizontally for the compact vertical size class.
-    @ViewBuilder
-    private func dynamicStackView(
-        minHeight: CGFloat,
-        @ViewBuilder imageContent: () -> some View,
-        @ViewBuilder textContent: () -> some View
-    ) -> some View {
-        Group {
-            if verticalSizeClass == .regular {
-                VStack(spacing: 24) {
-                    imageContent()
-                    textContent()
-                }
-                .padding(.top, 32)
-                .padding(.bottom, 24)
-                .frame(maxWidth: .infinity, minHeight: 0)
-                .border(.red)
-            } else {
-                HStack(alignment: .top, spacing: 40) {
-                    VStack(spacing: 0) {
-                        Spacer(minLength: 0)
-                        imageContent()
-                            .padding(.leading, 36)
-                            .padding(.vertical, 16)
-                        Spacer(minLength: 0)
-                    }
-                    .frame(minHeight: minHeight)
-
-                    textContent()
-                        .padding(.vertical, 16)
-                        .frame(maxWidth: .infinity, minHeight: minHeight)
-                }
-            }
-        }
-//        .scrollView(
-//            addVerticalPadding: false,
-//            backgroundColor: Asset.Colors.backgroundPrimary.swiftUIColor
-//        )
-    }
-
-    /// A view that displays a carousel page.
-    @ViewBuilder
-    private func pageView() -> some View {
-//        GeometryReader { reader in
             dynamicStackView(minHeight: 0) {
                 Asset.Images.Illustrations.businessWarning.swiftUIImage
                     .resizable()
@@ -112,19 +33,38 @@ struct NewDeviceNoticeView: View {
                 }
                 .padding(.horizontal, 12)
             }
-            .border(.green)
-//        }
-//        .border(.yellow)
+
+            toggleCard
+                .padding(.horizontal, 12)
+
+            VStack(spacing: 12) {
+                AsyncButton(Localizations.continue) {
+                    await store.perform(.continueTapped)
+                }
+                .buttonStyle(.primary())
+            }
+            .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
+            .padding(.horizontal, 16)
+            .padding(.vertical, 12)
+
+            Spacer()
+        }
+        .task {
+            await store.perform(.appeared)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Asset.Colors.backgroundPrimary.swiftUIColor.ignoresSafeArea())
+        .foregroundStyle(Asset.Colors.textPrimary.swiftUIColor)
+        .multilineTextAlignment(.center)
+        .scrollView()
     }
 
-    @ViewBuilder
-    private var internalCard: some View {
+    private var toggleCard: some View {
         VStack(alignment: .leading, spacing: 8) {
-            Text(LocalizedStringKey(Localizations.doYouHaveReliableAccessToYourEmail("a\u{2060}@example.com")))
+            Text(LocalizedStringKey(Localizations.doYouHaveReliableAccessToYourEmail("person\u{2060}@example.com")))
                 .styleGuide(.body)
                 .foregroundColor(Asset.Colors.textPrimary.swiftUIColor)
-                .multilineTextAlignment(.leading)
-            .accessibilityHidden(true)
+                .accessibilityHidden(true)
 
             Divider()
 
@@ -135,11 +75,48 @@ struct NewDeviceNoticeView: View {
             .toggleStyle(.bitwarden)
             .accessibilityIdentifier("ItemFavoriteToggle")
         }
+        .multilineTextAlignment(.leading)
 
         .padding(.horizontal, 16)
         .padding(.vertical, 8)
         .background(Asset.Colors.backgroundSecondary.swiftUIColor)
         .cornerRadius(10)
+    }
+
+    /// A dynamic stack view that lays out content vertically when in a regular vertical size class
+    /// and horizontally for the compact vertical size class.
+    @ViewBuilder
+    private func dynamicStackView(
+        minHeight: CGFloat,
+        @ViewBuilder imageContent: () -> some View,
+        @ViewBuilder textContent: () -> some View
+    ) -> some View {
+        Group {
+            if verticalSizeClass == .regular {
+                VStack(spacing: 24) {
+                    imageContent()
+                    textContent()
+                }
+                .padding(.top, 32)
+                .padding(.bottom, 24)
+                .frame(maxWidth: .infinity, minHeight: minHeight)
+            } else {
+                HStack(alignment: .top, spacing: 40) {
+                    VStack(spacing: 0) {
+                        Spacer(minLength: 0)
+                        imageContent()
+                            .padding(.leading, 36)
+                            .padding(.vertical, 16)
+                        Spacer(minLength: 0)
+                    }
+                    .frame(minHeight: minHeight)
+
+                    textContent()
+                        .padding(.vertical, 16)
+                        .frame(maxWidth: .infinity, minHeight: minHeight)
+                }
+            }
+        }
     }
 }
 
