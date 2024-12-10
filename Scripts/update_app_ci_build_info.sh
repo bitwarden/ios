3aeb88c07841ecb66d02a6183d1ed906f29cc793
Álison Fernandes <vvolkgang@users.noreply.github.com>
2024-12-10 16:23:53 +0000

    Update build info script to improve readability outside of github

--- Scripts/update_app_ci_build_info.sh
+++ Scripts/update_app_ci_build_info.sh
@@ -9,9 +9,10 @@
 
 set -euo pipefail
 
-if [ $# -ne 5 ]; then
-    echo "Usage: $0 <repository> <branch> <commit_hash> <ci_run_url> <compiler_flags>"
-    echo "E.g: $0 bitwarden/ios main abc123 https://github.com/bitwarden/ios/actions/runs/123 \"DEBUG_MENU FEATURE1\""
+if [ $# -ne 6 ]; then
+    echo >&2 "Called with $# arguments but expected 6."
+    echo "Usage: $0 <repository> <branch> <commit_hash> <ci_run_number> <ci_run_attempt> <compiler_flags>"
+    echo "E.g: $0 bitwarden/ios main abc123 1234567890 1 \"DEBUG_MENU FEATURE1\""
     exit 1
 fi
 
@@ -20,26 +21,27 @@ set -euo pipefail
 repository=$1
 branch=$2
 commit_hash=$3
-ci_run_url=$4
-compiler_flags=${5:-''}
+ci_run_number=$4
+ci_run_attempt=$5
+compiler_flags=${6:-''}
 
 ci_build_info_file="BitwardenShared/Core/Platform/Utilities/CIBuildInfo.swift"
 git_source="${repository}/${branch}@${commit_hash}"
+ci_run_source="${repository}/actions/runs/${ci_run_number}/attempts/${ci_run_attempt}"
 
 echo "🧱 Updating app CI Build info..."
-echo "🧱 :seedling: ${git_source}"
-echo "🧱 :octocat: ${ci_run_url}"
-echo "🧱 :hammer_and_wrench: ${compiler_flags}"
+echo "🧱 🧱${git_source}"
+echo "🧱 💻${ci_run_source}"
+echo "🧱 🛠️ ${compiler_flags}"
 
 
 cat << EOF > ${ci_build_info_file}
 enum CIBuildInfo {
     static let info: KeyValuePairs<String, String> = [
-        ":seedling:": "${repository}/${branch} @ ${repository}/${branch}@${commit_hash}",
-        ":octocat:": "${ci_run_url}",
-        ":hammer_and_wrench:": "${compiler_flags}",
+        "🧱 commit:": "${git_source}",
+        "💻 build source:": "${ci_run_source}",
+        "🛠️ compiler flags:": "${compiler_flags}",
     ]
 }
 EOF
-
 echo "✅ CI Build info updated successfully."
