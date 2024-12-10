f77232554fd7cc41df7d75d65fa7a38bbda5c6a4
ezimet-livefront <ezimet@livefront.com>
2024-12-10 11:19:34 -0500

    added docs

--- BitwardenShared/Core/Platform/Models/Domain/ReviewPromptData.swift
+++ BitwardenShared/Core/Platform/Models/Domain/ReviewPromptData.swift
@@ -3,14 +3,19 @@ import Foundation
 // MARK: - UserActionItem
 
 /// A struct that represents a user action and its count.
+///
 struct UserActionItem: Codable, Equatable {
+    /// The user action that was performed.
     let userAction: UserAction
+
+    /// The count of the user action.
     var count: Int
 }
 
 // MARK: - ReviewPromptData
 
 /// A struct that combines the version that was last shown the prompt and the user actions.
+///
 struct ReviewPromptData: Codable, Equatable {
     /// The app version for which the review prompt has been shown.
     var reviewPromptShownForVersion: String?
@@ -22,6 +27,7 @@ struct ReviewPromptData: Codable, Equatable {
 // MARK: - UserAction
 
 /// An enumeration of user actions that can be tracked.
+///
 enum UserAction: String, Codable {
     /// The user added a new item.
     case addedNewItem
