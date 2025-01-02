21c6db3185b4909c0cf597586dcbd52c787cbac8
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-01-02 15:05:39 -0600

    Revert changes

--- BitwardenShared/UI/Platform/Application/Views/PageHeaderView.swift
+++ BitwardenShared/UI/Platform/Application/Views/PageHeaderView.swift
@@ -2,26 +2,13 @@ import SwiftUI
 
 // MARK: - PageHeaderView
 
-/// A view that renders a header for page.
-/// This support displaying an image, title, and message,
-/// with some configurability for different circumstances.
+/// A view that renders a header for page. This support displaying an image, title, and message.
 ///
 struct PageHeaderView: View {
-    enum ImageSizeMode: Equatable {
-        case constant
-        case largerInPortrait
-    }
-
     // MARK: Properties
 
     /// The image to display in the page header.
     let image: Image
-
-    /// How to handle image size between landscape and portrait.
-    let imageSizeMode: ImageSizeMode
-
-    /// The distance between the image and the title.
-//    let imageSpacing:
 
     /// The message to display in the page header.
     let message: String
@@ -29,37 +16,18 @@ struct PageHeaderView: View {
     /// The title to display in the page header.
     let title: String
 
-    /// The distance between the title and the message.
-    let textSpacing: CGFloat
-
     /// An environment variable for getting the vertical size class of the view.
     @Environment(\.verticalSizeClass) var verticalSizeClass
-
-    // MARK: Computed Properties
-
-    var imageDimension: CGFloat {
-        switch imageSizeMode {
-        case .constant:
-            return 100
-        case .largerInPortrait:
-            return verticalSizeClass == .regular ? 124 : 100
-        }
-    }
 
     // MARK: View
 
     var body: some View {
         dynamicStackView {
-            ZStack {
-                Color.teal.opacity(0.3)
-                    .frame(width: imageDimension, height: imageDimension)
+            image
+                .resizable()
+                .frame(width: 100, height: 100)
 
-                image
-                    .resizable()
-                    .frame(width: imageDimension, height: imageDimension)
-            }
-
-            VStack(spacing: textSpacing) {
+            VStack(spacing: 16) {
                 Text(title)
                     .styleGuide(.title2, weight: .bold)
 
@@ -77,22 +45,16 @@ struct PageHeaderView: View {
     ///
     /// - Parameters:
     ///   - image: The image to display.
-    ///   - imageSizeMode: How to handle image size between orientations.
-    ///   - textSpacing: The distance between the title and the message.
     ///   - title: The title to display.
     ///   - message: The message to display.
     ///
     init(
         image: Image,
-        imageSizeMode: ImageSizeMode = .constant,
-        textSpacing: CGFloat = 16,
         title: String,
         message: String
     ) {
         self.image = image
-        self.imageSizeMode = imageSizeMode
         self.message = message
-        self.textSpacing = textSpacing
         self.title = title
     }
 
@@ -100,22 +62,16 @@ struct PageHeaderView: View {
     ///
     /// - Parameters:
     ///   - image: The image asset to display.
-    ///   - imageSizeMode: How to handle image size between orientations.
-    ///   - textSpacing: The distance between the title and the message.
     ///   - title: The title to display.
     ///   - message: The message to display.
     ///
     init(
         image: ImageAsset,
-        imageSizeMode: ImageSizeMode = .constant,
-        textSpacing: CGFloat = 16,
         title: String,
         message: String
     ) {
         self.image = image.swiftUIImage
-        self.imageSizeMode = imageSizeMode
         self.message = message
-        self.textSpacing = textSpacing
         self.title = title
     }
 
@@ -137,19 +93,9 @@ struct PageHeaderView: View {
 // MARK: - Previews
 
 #if DEBUG
-#Preview("PageHeader Constant") {
+#Preview("PageHeader") {
     PageHeaderView(
         image: Asset.Images.Illustrations.biometricsPhone,
-        title: Localizations.setUpUnlock,
-        message: Localizations.setUpBiometricsOrChooseAPinCodeToQuicklyAccessYourVaultAndAutofillYourLogins
-    )
-}
-
-#Preview("PageHeader LargerInPortrait") {
-    PageHeaderView(
-        image: Asset.Images.Illustrations.biometricsPhone,
-        imageSizeMode: .largerInPortrait,
-        textSpacing: 32,
         title: Localizations.setUpUnlock,
         message: Localizations.setUpBiometricsOrChooseAPinCodeToQuicklyAccessYourVaultAndAutofillYourLogins
     )
