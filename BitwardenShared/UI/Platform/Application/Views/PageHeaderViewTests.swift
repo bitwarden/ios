dc3f499d21c3bdb0addee704c0f6235e24f72d30
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-01-06 14:19:39 -0600

    Add snapshots

--- BitwardenShared/UI/Platform/Application/Views/PageHeaderViewTests.swift
+++ BitwardenShared/UI/Platform/Application/Views/PageHeaderViewTests.swift
@@ -6,6 +6,23 @@ import XCTest
 class PageHeaderViewTests: BitwardenTestCase {
     // MARK: Tests
 
+    /// Test snapshots of the largeTextTintedIcon style.
+    func test_snapshot_largeTextTintedIcon() {
+        let subject = PageHeaderView(
+            image: Asset.Images.plus24,
+            style: .largeTextTintedIcon,
+            title: Localizations.importPasswords,
+            message: Localizations.startImportCXPDescriptionLong
+        )
+        assertSnapshots(
+            of: subject,
+            as: [
+                "portrait": .defaultPortrait,
+                "landscape": .defaultLandscape,
+            ]
+        )
+    }
+
     /// Test snapshots of the mediumImage style.
     func test_snapshot_mediumImage() {
         let subject = PageHeaderView(
@@ -13,6 +30,24 @@ class PageHeaderViewTests: BitwardenTestCase {
             style: .mediumImage,
             title: Localizations.setUpUnlock,
             message: Localizations.setUpBiometricsOrChooseAPinCodeToQuicklyAccessYourVaultAndAutofillYourLogins
+        )
+        assertSnapshots(
+            of: subject,
+            as: [
+                "portrait": .defaultPortrait,
+                "landscape": .defaultLandscape,
+            ]
+        )
+    }
+
+    /// Test snapshots of the mediumImage style with a button.
+    func test_snapshot_mediumImage_withButton() {
+        let subject = PageHeaderView(
+            image: Asset.Images.Illustrations.biometricsPhone,
+            style: .mediumImage,
+            title: Localizations.setUpUnlock,
+            message: Localizations.setUpBiometricsOrChooseAPinCodeToQuicklyAccessYourVaultAndAutofillYourLogins,
+            button: PageHeaderViewButton(text: Localizations.learnMore) {}
         )
         assertSnapshots(
             of: subject,
