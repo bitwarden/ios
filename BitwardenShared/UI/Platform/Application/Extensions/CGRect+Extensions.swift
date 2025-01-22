61271903c73fdd5f1987dee3e796b13eada0d02e
Ezimet Ozkhan <148797477+ezimet-livefront@users.noreply.github.com>
2025-01-22 15:33:39 -0500

    PM-16153: Draw new login action card (#1238)

---
+++ BitwardenShared/UI/Platform/Application/Extensions/CGRect+Extensions.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+/// A extension to `CGRect` that allows enlarging the rect by a given value.
+extension CGRect {
+    /// Returns a new `CGRect` that is enlarged by the given value, will add the value to each side of the rect.
+    ///
+    /// - Parameter value: The value to enlarge the `CGRect` by.
+    /// - Returns: A new `CGRect` that is enlarged by the given value.
+    ///
+    func enlarged(by value: CGFloat) -> CGRect {
+        CGRect(
+            x: origin.x - value,
+            y: origin.y - value,
+            width: size.width + 2 * value,
+            height: size.height + 2 * value
+        )
+    }
+}
---
+++ BitwardenShared/UI/Platform/Application/Extensions/View+GuidedTourStep.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+extension View {
+    /// A view modifier that informs the guided tour the spotlight region of the
+    /// view and assigns an identifier to the view.
+    ///
+    /// - Parameters:
+    ///   - step: The guided tour step.
+    ///   - perform: A closure called when the size or origin of the view changes.
+    /// - Returns: A copy of the view with the guided tour step modifier applied.
+    ///
+    func guidedTourStep(_ step: GuidedTourStep, perform: @escaping (CGRect) -> Void) -> some View {
+        onFrameChanged { origin, size in
+            perform(CGRect(origin: origin, size: size))
+        }
+        .id(step)
+    }
+}
