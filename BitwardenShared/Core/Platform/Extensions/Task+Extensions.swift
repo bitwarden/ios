ed6a1a4e2db1b846f24abbd51bbbe8d1ae3c5518
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-01-22 13:33:47 -0600

    Include tolerance argument

--- BitwardenShared/Core/Platform/Extensions/Task+Extensions.swift
+++ BitwardenShared/Core/Platform/Extensions/Task+Extensions.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 extension Task where Success == Never, Failure == Never {
-    static func sleep(forSeconds delay: Int) async throws {
+    static func sleep(forSeconds delay: Int, tolerance: Int = 1) async throws {
         if #available(iOS 16.0, *) {
-            try await sleep(for: .seconds(delay), tolerance: .seconds(1))
+            try await sleep(for: .seconds(delay), tolerance: .seconds(tolerance))
         } else {
             try await sleep(nanoseconds: UInt64(delay) * NSEC_PER_SEC)
         }
