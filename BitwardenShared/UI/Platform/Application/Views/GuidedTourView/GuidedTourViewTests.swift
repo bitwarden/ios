9cc5b3fe2da678f1925a9d3f19e7d56e843bebdd
ezimet-livefront <ezimet@livefront.com>
2025-01-24 23:02:16 -0500

    tweaked the tourview animation, fixed a bug in calculation, added snapshot tests for generator guided tour

--- BitwardenShared/UI/Platform/Application/Views/GuidedTourView/GuidedTourViewTests.swift
+++ BitwardenShared/UI/Platform/Application/Views/GuidedTourView/GuidedTourViewTests.swift
@@ -34,6 +34,22 @@ class GuidedTourViewTests: BitwardenTestCase {
         subject = nil
     }
 
+    @MainActor
+    func setupSubjectForGenerator() {
+        processor = MockProcessor(
+            state: GuidedTourViewState(currentIndex: 0, guidedTourStepStates: [
+                .generatorStep1,
+                .generatorStep2,
+                .generatorStep3,
+                .generatorStep4,
+                .generatorStep5,
+                .generatorStep6,
+            ])
+        )
+        let store = Store(processor: processor)
+        subject = GuidedTourView(store: store)
+    }
+
     // MARK: Tests
 
     /// Tap the `back` button should dispatch the `backTapped` action.
@@ -69,6 +85,80 @@ class GuidedTourViewTests: BitwardenTestCase {
         let button = try subject.inspect().find(button: Localizations.next)
         try button.tap()
         XCTAssertEqual(processor.dispatchedActions, [.nextTapped])
+    }
+
+    // MARK: Snapshot tests
+
+    /// Test the snapshot of the step 1 of the learn generator guided tour.
+    @MainActor
+    func test_snapshot_generatorStep1() {
+        setupSubjectForGenerator()
+        processor.state.currentIndex = 0
+        processor.state.guidedTourStepStates[0].spotlightRegion = CGRect(x: 25, y: 80, width: 340, height: 40)
+        assertSnapshots(
+            of: subject,
+            as: [.defaultPortrait]
+        )
+    }
+
+    /// Test the snapshot of the step 2 of the learn generator guided tour.
+    @MainActor
+    func test_snapshot_generatorStep2() {
+        setupSubjectForGenerator()
+        processor.state.currentIndex = 1
+        processor.state.guidedTourStepStates[1].spotlightRegion = CGRect(x: 25, y: 80, width: 340, height: 40)
+        assertSnapshots(
+            of: subject,
+            as: [.defaultPortrait]
+        )
+    }
+
+    /// Test the snapshot of the step 3 of the learn generator guided tour.
+    @MainActor
+    func test_snapshot_generatorStep3() {
+        setupSubjectForGenerator()
+        processor.state.currentIndex = 2
+        processor.state.guidedTourStepStates[2].spotlightRegion = CGRect(x: 25, y: 80, width: 340, height: 40)
+        assertSnapshots(
+            of: subject,
+            as: [.defaultPortrait]
+        )
+    }
+
+    /// Test the snapshot of the step 4 of the learn generator guided tour.
+    @MainActor
+    func test_snapshot_generatorStep4() {
+        setupSubjectForGenerator()
+        processor.state.currentIndex = 3
+        processor.state.guidedTourStepStates[3].spotlightRegion = CGRect(x: 25, y: 300, width: 340, height: 400)
+        assertSnapshots(
+            of: subject,
+            as: [.defaultPortrait]
+        )
+    }
+
+    /// Test the snapshot of the step 5 of the learn generator guided tour.
+    @MainActor
+    func test_snapshot_generatorStep5() {
+        setupSubjectForGenerator()
+        processor.state.currentIndex = 4
+        processor.state.guidedTourStepStates[4].spotlightRegion = CGRect(x: 300, y: 160, width: 40, height: 40)
+        assertSnapshots(
+            of: subject,
+            as: [.defaultPortrait]
+        )
+    }
+
+    /// Test the snapshot of the step 6 of the learn generator guided tour.
+    @MainActor
+    func test_snapshot_generatorStep6() {
+        setupSubjectForGenerator()
+        processor.state.currentIndex = 5
+        processor.state.guidedTourStepStates[5].spotlightRegion = CGRect(x: 25, y: 160, width: 340, height: 60)
+        assertSnapshots(
+            of: subject,
+            as: [.defaultPortrait]
+        )
     }
 
     /// Test the snapshot of the step 1 of the learn new login guided tour.
