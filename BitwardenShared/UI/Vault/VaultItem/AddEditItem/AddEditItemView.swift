b99af774e232c43d9fd0541696976ecbd07ce9e5
Federico Maccaroni <fedemkr@gmail.com>
2025-02-10 17:35:49 -0300

    PM-11712 Fix scroll going back to focus when scrolling and dismissing the keyboard.

--- BitwardenShared/UI/Vault/VaultItem/AddEditItem/AddEditItemView.swift
+++ BitwardenShared/UI/Vault/VaultItem/AddEditItem/AddEditItemView.swift
@@ -8,9 +8,6 @@ import SwiftUI
 struct AddEditItemView: View {
     // MARK: Private Properties
 
-    /// A responder to keyboard visibility events.
-    @ObservedObject private var keyboard = KeyboardResponder()
-
     /// An object used to open urls in this view.
     @Environment(\.openURL) private var openURL
 
@@ -98,7 +95,7 @@ struct AddEditItemView: View {
                 .ignoresSafeArea()
         )
         .navigationBarTitleDisplayMode(.inline)
-        .backport.scrollContentMargins(Edge.Set.bottom, keyboard.isShown ? 30.0 : 0.0)
+        .backport.scrollContentMargins(Edge.Set.bottom, 30.0)
     }
 
     @ViewBuilder private var cardItems: some View {
