44fc2a844291fa944506ddbcfc002fdc4681d1e8
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-02-26 11:13:19 -0600

    Move data tests as POC

---
+++ BitwardenKit/Core/Platform/Extensions/DataTests.swift
@@ -0,0 +1,17 @@
+import TestHelpers
+import XCTest
+
+@testable import BitwardenKit
+
+class DataTests: BitwardenTestCase {
+    // MARK: Tests
+
+    /// `asHexString()` converts the Data object into a hex formatted string.
+    func test_asHexString() {
+        let subject = Data(repeating: 1, count: 32)
+        XCTAssertEqual(
+            subject.asHexString(),
+            "0101010101010101010101010101010101010101010101010101010101010101"
+        )
+    }
+}
