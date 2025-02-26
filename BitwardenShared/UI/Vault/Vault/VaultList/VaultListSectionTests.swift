d0610d32b60983b70dd5c4b76673ac2e03904f76
Matt Czech <matt@livefront.com>
2025-02-26 11:17:28 -0600

    [PM-18594] [RC] Hide coach marks if user has existing login items

---
+++ BitwardenShared/UI/Vault/Vault/VaultList/VaultListSectionTests.swift
@@ -0,0 +1,56 @@
+import XCTest
+
+@testable import BitwardenShared
+
+class VaultListSectionTests: BitwardenTestCase {
+    // MARK: Tests
+
+    /// `hasLoginItems` returns `false` if there's no login items within the sections.
+    func test_vaultListSectionArray_hasLoginItems_false() {
+        let subjectEmpty = [VaultListSection]()
+        XCTAssertFalse(subjectEmpty.hasLoginItems)
+
+        let subjectWithoutLogin = [
+            VaultListSection(id: "2", items: [.fixtureGroup(group: .card, count: 2)], name: "Cards"),
+            VaultListSection(id: "3", items: [.fixtureGroup(group: .identity, count: 3)], name: "Identities"),
+            VaultListSection(id: "4", items: [.fixtureGroup(group: .secureNote, count: 0)], name: "Notes"),
+        ]
+        XCTAssertFalse(subjectWithoutLogin.hasLoginItems)
+
+        let subjectLoginsEmpty = [
+            VaultListSection(id: "1", items: [.fixtureGroup(group: .login, count: 0)], name: "Logins"),
+            VaultListSection(id: "2", items: [.fixtureGroup(group: .card, count: 2)], name: "Cards"),
+            VaultListSection(id: "3", items: [.fixtureGroup(group: .identity, count: 3)], name: "Identities"),
+            VaultListSection(id: "4", items: [.fixtureGroup(group: .secureNote, count: 0)], name: "Notes"),
+        ]
+        XCTAssertFalse(subjectLoginsEmpty.hasLoginItems)
+
+        let subjectCiphersNoLogins = [
+            VaultListSection(id: "5", items: [.fixture(cipherView: .fixture(type: .identity))], name: "Items"),
+        ]
+        XCTAssertFalse(subjectCiphersNoLogins.hasLoginItems)
+    }
+
+    /// `hasLoginItems` returns `true` if there's a login group with more than one item or a login
+    /// cipher within the sections.
+    func test_vaultListSectionArray_hasLoginItems_true() {
+        let subjectWithLogin = [
+            VaultListSection(id: "1", items: [.fixtureGroup(group: .login, count: 1)], name: "Logins"),
+        ]
+        XCTAssertTrue(subjectWithLogin.hasLoginItems)
+
+        let subjectWithMultipleSections = [
+            VaultListSection(id: "1", items: [.fixtureGroup(group: .login, count: 1)], name: "Logins"),
+            VaultListSection(id: "2", items: [.fixtureGroup(group: .card, count: 2)], name: "Cards"),
+            VaultListSection(id: "3", items: [.fixtureGroup(group: .identity, count: 3)], name: "Identities"),
+            VaultListSection(id: "4", items: [.fixtureGroup(group: .secureNote, count: 0)], name: "Notes"),
+        ]
+        XCTAssertTrue(subjectWithMultipleSections.hasLoginItems)
+
+        let subjectWithCipher = [
+            VaultListSection(id: "2", items: [.fixtureGroup(group: .card, count: 2)], name: "Cards"),
+            VaultListSection(id: "5", items: [.fixture(cipherView: .fixture(type: .login))], name: "Items"),
+        ]
+        XCTAssertTrue(subjectWithCipher.hasLoginItems)
+    }
+}
