dc26b49ff0d2254e6253e4eb8095848bcad6627e
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-03-06 15:45:26 -0600

    Docs

--- TestHelpers/Support/FakeData.swift
+++ TestHelpers/Support/FakeData.swift
@@ -1,8 +1,14 @@
 /// Fake Data used in tests
 public enum FakeData {
+    /// A fake email address equal to `email1@example.com`
     public static let email1 = "email1@example.com"
+
+    /// A fake email address equal to `email2@example.com`
     public static let email2 = "email2@example.com"
 
+    /// A fake username equal to `username1`
     public static let username1 = "username1"
+
+    /// A fake username equal to `username2`
     public static let username2 = "username2"
 }
