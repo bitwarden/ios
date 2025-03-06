c930a2164f7e2fbaefcf510433f75a68e278461b
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-03-06 14:46:46 -0600

    Move some more things

---
+++ TestHelpers/Support/FakeData.swift
@@ -0,0 +1,8 @@
+/// Fake Data used in tests
+public enum FakeData {
+    public static let email1 = "email1@example.com"
+    public static let email2 = "email2@example.com"
+
+    public static let username1 = "username1"
+    public static let username2 = "username2"
+}
