76cbf6ca1623a27643824bb994315ade91b785bf
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-03-10 13:03:09 -0500

    Create QR code view

---
+++ BitwardenShared/UI/Vault/VaultItem/GenerateQRCode/QRCodeView.swift
@@ -0,0 +1,61 @@
+import CoreImage.CIFilterBuiltins
+import SwiftUI
+
+// MARK: - QRCodeView
+
+/// A view for displaying a QR code.
+///
+struct QRCodeView: View {
+    // MARK: Properties
+
+    /// The string encoded by the QR code.
+    let encodedString: String
+
+    // MARK: Private Properties
+
+    /// The CoreImage Context for generating a QR code.
+    private let context = CIContext()
+
+    /// The CoreImage Filter for generating a QR code.
+    private let filter = CIFilter.qrCodeGenerator()
+
+    // MARK: View
+
+    var body: some View {
+        HStack {
+            Spacer()
+
+            Image(uiImage: generateQRCode(from: encodedString))
+                .interpolation(.none)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+
+            Spacer()
+        }
+    }
+
+    // MARK: Private Functions
+
+    private func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+}
+
+// MARK: Previews
+
+#if DEBUG
+
+#Preview {
+    QRCodeView(encodedString: "https://www.bitwarden.com")
+}
+
+#endif
