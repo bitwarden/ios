dd2a850890a2ade20135a39fff3f97f4fe18a040
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-03-27 11:52:05 -0500

    Make Mocks target

---
+++ BitwardenKit/Core/Platform/Utilities/Mocks/MockSystemDevice.swift
@@ -0,0 +1,8 @@
+@testable import BitwardenKit
+
+class MockSystemDevice: SystemDevice {
+    var model = "iPhone"
+    var modelIdentifier = "iPhone14,2"
+    var systemName = "iOS"
+    var systemVersion = "16.4"
+}
