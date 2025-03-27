53e0eba045472f84cc508a7a63bef9f0033e0cb9
Federico Maccaroni <fedemkr@gmail.com>
2025-03-27 12:49:28 -0300

    PM-19507 Fix Authenticator missing new properties and also fixed update_acknowledgements script for BWA which was causing "command not found" for mint.

--- Scripts-bwa/update_acknowledgements.sh
+++ Scripts-bwa/update_acknowledgements.sh
@@ -4,6 +4,10 @@
 
 set -euo pipefail
 
+if [[ ! "$PATH" =~ "/opt/homebrew/bin" ]]; then
+    PATH="/opt/homebrew/bin:$PATH"
+fi
+
 if [ "$CONFIGURATION" = "Debug" ]; then
     mint run LicensePlist license-plist \
         --config-path .license-plist-bwa.yml
