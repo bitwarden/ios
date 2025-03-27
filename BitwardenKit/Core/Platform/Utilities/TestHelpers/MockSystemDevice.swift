03fd33b30613d6ab1c8b12e73c810e3701cf870c
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-03-27 10:40:12 -0500

    Comment out for now

--- BitwardenKit/Core/Platform/Utilities/TestHelpers/MockSystemDevice.swift
+++ BitwardenKit/Core/Platform/Utilities/TestHelpers/MockSystemDevice.swift
@@ -1,8 +1,8 @@
-@testable import BitwardenShared
-
-class MockSystemDevice: SystemDevice {
-    var model = "iPhone"
-    var modelIdentifier = "iPhone14,2"
-    var systemName = "iOS"
-    var systemVersion = "16.4"
-}
+//@testable import BitwardenShared
+//
+//class MockSystemDevice: SystemDevice {
+//    var model = "iPhone"
+//    var modelIdentifier = "iPhone14,2"
+//    var systemName = "iOS"
+//    var systemVersion = "16.4"
+//}
