86279ec7cf79be0e359eda548f9d68cb5b28c069
Matt Czech <matt@livefront.com>
2025-04-09 14:57:09 -0500

    [PM-19552] Add flight recorder logs screen (#1483)

--- BitwardenShared/Core/Platform/Models/Enum/FlightRecorderLoggingDurationTests.swift
+++ BitwardenShared/Core/Platform/Models/Enum/FlightRecorderLoggingDurationTests.swift
@@ -5,6 +5,23 @@ import XCTest
 class FlightRecorderLoggingDurationTests: BitwardenTestCase {
     // MARK: Tests
 
+    /// `Calendar.date(byAdding:to:)` adds the logging duration to the specified date.
+    func test_calendarDateByAdding() {
+        let date = Date(year: 2025, month: 3, day: 4, hour: 8, minute: 30, second: 20)
+
+        let oneHour = Calendar.current.date(byAdding: .oneHour, to: date)
+        XCTAssertEqual(oneHour, Date(year: 2025, month: 3, day: 4, hour: 9, minute: 30, second: 20))
+
+        let eightHours = Calendar.current.date(byAdding: .eightHours, to: date)
+        XCTAssertEqual(eightHours, Date(year: 2025, month: 3, day: 4, hour: 16, minute: 30, second: 20))
+
+        let twentyFourHours = Calendar.current.date(byAdding: .twentyFourHours, to: date)
+        XCTAssertEqual(twentyFourHours, Date(year: 2025, month: 3, day: 5, hour: 8, minute: 30, second: 20))
+
+        let oneWeek = Calendar.current.date(byAdding: .oneWeek, to: date)
+        XCTAssertEqual(oneWeek, Date(year: 2025, month: 3, day: 11, hour: 8, minute: 30, second: 20))
+    }
+
     /// `localizedName` returns the correct values.
     func test_localizedName() {
         XCTAssertEqual(FlightRecorderLoggingDuration.oneHour.localizedName, Localizations.oneHour)
