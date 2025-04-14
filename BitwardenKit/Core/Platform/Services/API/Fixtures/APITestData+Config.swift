9412babc66a12a4e4625054d44d4cafebbdc7940
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-04-14 14:03:42 -0500

    Bundle

--- BitwardenKit/Core/Platform/Services/API/Fixtures/APITestData+Config.swift
+++ BitwardenKit/Core/Platform/Services/API/Fixtures/APITestData+Config.swift
@@ -3,5 +3,8 @@ import TestHelpers
 
 public extension APITestData {
     /// A valid server configuration to produce a `ConfigResponseModel`.
-    static let validServerConfig = loadFromJsonBundle(resource: "ValidServerConfig")
+    static let validServerConfig = loadFromJsonBundle(
+        resource: "ValidServerConfig",
+        bundle: BitwardenKitMocksBundleFinder.bundle
+    )
 }
