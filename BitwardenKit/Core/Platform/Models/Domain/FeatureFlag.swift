81eea8c91c029cde9227a9383000d520d796dace
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-05-05 14:23:20 -0500

    Documentation

--- BitwardenKit/Core/Platform/Models/Domain/FeatureFlag.swift
+++ BitwardenKit/Core/Platform/Models/Domain/FeatureFlag.swift
@@ -1,13 +1,30 @@
-public struct FeatureFlag: Codable, Equatable, Hashable, Sendable {
+
+/// A struct that encapsulates information about a feature flag. Importing applications
+/// should extend this struct to add static members representing each flag.
+public struct FeatureFlag: Codable, Equatable, Sendable {
+    /// The initial value of the feature flag.
+    /// If `isRemotelyConfigured` is true for the flag, then this will get overridden by the server;
+    /// but if `isRemotelyConfigured` is false for the flag, then the value here will be used.
+    /// This is a helpful way to manage local feature flags.
     public let initialValue: AnyCodable?
+
+    /// Whether this feature can be enabled remotely.
     public let isRemotelyConfigured: Bool
 
+    /// The string name of the flag as sent by the server.
     public let rawValue: String
 
+    /// The display name of the feature flag.
     public var name: String {
         rawValue.split(separator: "-").map(\.localizedCapitalized).joined(separator: " ")
     }
 
+    /// Initializer for a feature flag.
+    ///
+    /// - Parameters:
+    ///   - rawValue: The string name of the flag as sent by the server.
+    ///   - initialValue: The initial value of the feature flag (if any).
+    ///   - isRemotelyConfigured: Whether this feature can be enabled remotely.
     public init(
         rawValue: String,
         initialValue: AnyCodable? = nil,
@@ -18,6 +35,11 @@ public struct FeatureFlag: Codable, Equatable, Hashable, Sendable {
         self.rawValue = rawValue
     }
 
+}
+
+// MARK: - FeatureFlag + Hashable
+
+extension FeatureFlag: Hashable {
     public func hash(into hasher: inout Hasher) {
         rawValue.hash(into: &hasher)
     }
