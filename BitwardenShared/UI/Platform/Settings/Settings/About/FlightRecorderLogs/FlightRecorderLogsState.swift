4bf604ba920ec6508e5a0cabb6fdadc6f44d6236
Matt Czech <matt@livefront.com>
2025-05-06 13:46:04 -0500

    [PM-19621] Disable flight recorder delete and share all buttons if there are no logs (#1552)

--- BitwardenShared/UI/Platform/Settings/Settings/About/FlightRecorderLogs/FlightRecorderLogsState.swift
+++ BitwardenShared/UI/Platform/Settings/Settings/About/FlightRecorderLogs/FlightRecorderLogsState.swift
@@ -7,4 +7,16 @@ struct FlightRecorderLogsState: Equatable {
 
     /// The list of flight recorder logs on the device.
     var logs = [FlightRecorderLogMetadata]()
+
+    // MARK: Computed Properties
+
+    /// Whether the delete all option is enabled.
+    var isDeleteAllEnabled: Bool {
+        logs.contains { !$0.isActiveLog }
+    }
+
+    /// Whether the share all option is enabled.
+    var isShareAllEnabled: Bool {
+        !logs.isEmpty
+    }
 }
