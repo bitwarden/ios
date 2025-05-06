0148b3c89bacfd326385c13ae52ed7f643f1a3e8
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-05-06 15:09:14 -0500

    Consolidate MetaServerConfig

--- AuthenticatorShared/Core/Platform/Services/ConfigService.swift
+++ AuthenticatorShared/Core/Platform/Services/ConfigService.swift
@@ -330,15 +330,3 @@ class DefaultConfigService: ConfigService {
         }
     }
 }
-
-/// Helper object to send updated server config object with extra metadata
-/// like whether it comes from pre-auth and the user ID it belongs to.
-/// This is useful for getting the config on background and establishing which was the original context.
-struct MetaServerConfig {
-    /// If true, the call is coming before the user is authenticated or when adding a new account
-    let isPreAuth: Bool
-    /// The user ID the requested the server config.
-    let userId: String?
-    /// The server config.
-    let serverConfig: ServerConfig?
-}
