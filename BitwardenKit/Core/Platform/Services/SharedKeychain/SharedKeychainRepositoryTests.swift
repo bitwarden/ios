c231fde76071240941f05c8ebbad7a0ce62e4687
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-06-03 09:36:16 -0500

    Build out repository

--- BitwardenKit/Core/Platform/Services/SharedKeychain/SharedKeychainRepositoryTests.swift
+++ BitwardenKit/Core/Platform/Services/SharedKeychain/SharedKeychainRepositoryTests.swift
@@ -4,123 +4,80 @@ import CryptoKit
 import Foundation
 import XCTest
 
-//final class SharedKeychainRepositoryTests: BitwardenTestCase {
-//    // MARK: Properties
-//
-//    let accessGroup = "group.com.example.bitwarden"
-//    var keychainService: MockAuthenticatorKeychainService!
-//    var subject: DefaultSharedKeychainRepository!
-//
-//    // MARK: Setup & Teardown
-//
-//    override func setUp() {
-//        keychainService = MockAuthenticatorKeychainService()
-////        subject = DefaultSharedKeychainRepository(
-////            sharedAppGroupIdentifier: accessGroup,
-////            keychainService: keychainService
-////        )
-//    }
-//
-//    override func tearDown() {
-//        keychainService = nil
-//        subject = nil
-//    }
-//
-//    // MARK: Tests
-//
-//    /// Verify that `deleteAuthenticatorKey()` issues a delete with the correct search attributes specified.
-//    ///
-//    func test_deleteAuthenticatorKey_success() async throws {
-//        try await subject.deleteAuthenticatorKey()
-//
-//        let queries = try XCTUnwrap(keychainService.deleteQueries as? [[CFString: Any]])
-//        XCTAssertEqual(queries.count, 1)
-//
-//        let query = try XCTUnwrap(queries.first)
-//        try XCTAssertEqual(XCTUnwrap(query[kSecAttrAccessGroup] as? String), accessGroup)
-//        try XCTAssertEqual(XCTUnwrap(query[kSecAttrAccessible] as? String),
-//                           String(kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly))
-//        try XCTAssertEqual(XCTUnwrap(query[kSecAttrAccount] as? String),
-//                           SharedKeychainItem.authenticatorKey.unformattedKey)
-//        try XCTAssertEqual(XCTUnwrap(query[kSecClass] as? String),
-//                           String(kSecClassGenericPassword))
-//    }
-//
-//    /// Verify that `getAuthenticatorKey()` returns a value successfully when one is set. Additionally, verify the
-//    /// search attributes are specified correctly.
-//    ///
-//    func test_getAuthenticatorKey_success() async throws {
-//        let key = SymmetricKey(size: .bits256)
-//        let data = key.withUnsafeBytes { Data(Array($0)) }
-//
-//        keychainService.setSearchResultData(data)
-//
-//        let returnData = try await subject.getAuthenticatorKey()
-//        XCTAssertEqual(returnData, data)
-//
-//        let query = try XCTUnwrap(keychainService.searchQuery as? [CFString: Any])
-//        try XCTAssertEqual(XCTUnwrap(query[kSecAttrAccessGroup] as? String), accessGroup)
-//        try XCTAssertEqual(XCTUnwrap(query[kSecAttrAccessible] as? String),
-//                           String(kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly))
-//        try XCTAssertEqual(XCTUnwrap(query[kSecAttrAccount] as? String),
-//                           SharedKeychainItem.authenticatorKey.unformattedKey)
-//        try XCTAssertEqual(XCTUnwrap(query[kSecClass] as? String), String(kSecClassGenericPassword))
-//        try XCTAssertEqual(XCTUnwrap(query[kSecMatchLimit] as? String), String(kSecMatchLimitOne))
-//        try XCTAssertTrue(XCTUnwrap(query[kSecReturnAttributes] as? Bool))
-//        try XCTAssertTrue(XCTUnwrap(query[kSecReturnData] as? Bool))
-//    }
-//
-//    /// Verify that `getAuthenticatorKey()` fails with a `keyNotFound` error when an unexpected
-//    /// result is returned instead of the key data from the keychain
-//    ///
-//    func test_getAuthenticatorKey_badResult() async throws {
-//        let error = AuthenticatorKeychainServiceError.keyNotFound(SharedKeychainItem.authenticatorKey)
-//        keychainService.searchResult = .success([kSecValueData as String: NSObject()] as AnyObject)
-//
-//        await assertAsyncThrows(error: error) {
-//            _ = try await subject.getAuthenticatorKey()
-//        }
-//    }
-//
-//    /// Verify that `getAuthenticatorKey()` fails with a `keyNotFound` error when a nil
-//    /// result is returned instead of the key data from the keychain
-//    ///
-//    func test_getAuthenticatorKey_nilResult() async throws {
-//        let error = AuthenticatorKeychainServiceError.keyNotFound(SharedKeychainItem.authenticatorKey)
-//        keychainService.searchResult = .success(nil)
-//
-//        await assertAsyncThrows(error: error) {
-//            _ = try await subject.getAuthenticatorKey()
-//        }
-//    }
-//
-//    /// Verify that `getAuthenticatorKey()` fails with an error when the Authenticator key is not
-//    /// present in the keychain
-//    ///
-//    func test_getAuthenticatorKey_keyNotFound() async throws {
-//        let error = AuthenticatorKeychainServiceError.keyNotFound(SharedKeychainItem.authenticatorKey)
-//        keychainService.searchResult = .failure(error)
-//
-//        await assertAsyncThrows(error: error) {
-//            _ = try await subject.getAuthenticatorKey()
-//        }
-//    }
-//
-//    /// Verify that `setAuthenticatorKey(_:)` sets a value with the correct search attributes specified.
-//    ///
-//    func test_setAuthenticatorKey_success() async throws {
-//        let key = SymmetricKey(size: .bits256)
-//        let data = key.withUnsafeBytes { Data(Array($0)) }
-//        try await subject.setAuthenticatorKey(data)
-//
-//        let attributes = try XCTUnwrap(keychainService.addAttributes as? [CFString: Any])
-//        try XCTAssertEqual(XCTUnwrap(attributes[kSecAttrAccessGroup] as? String), accessGroup)
-//        try XCTAssertEqual(XCTUnwrap(attributes[kSecAttrAccessible] as? String),
-//                           String(kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly))
-//        try XCTAssertEqual(XCTUnwrap(attributes[kSecAttrAccount] as? String),
-//                           SharedKeychainItem.authenticatorKey.unformattedKey)
-//        try XCTAssertEqual(XCTUnwrap(attributes[kSecClass] as? String),
-//                           String(kSecClassGenericPassword))
-//        try XCTAssertEqual(XCTUnwrap(attributes[kSecValueData] as? Data), data)
-//    }
-//}
+final class SharedKeychainRepositoryTests: BitwardenTestCase {
+    // MARK: Properties
+
+    var storage: MockSharedKeychainStorage!
+    var subject: DefaultSharedKeychainRepository!
+
+    // MARK: Setup & Teardown
+
+    override func setUp() {
+        storage = MockSharedKeychainStorage()
+        subject = DefaultSharedKeychainRepository(
+            storage: storage
+        )
+    }
+
+    override func tearDown() {
+        storage = nil
+        subject = nil
+    }
+
+    // MARK: Tests
+
+    /// `deleteAuthenticatorKey()` deletes the authenticator key from storage.
+    func test_deleteAuthenticatorKey_success() async throws {
+        storage.storage[.authenticatorKey] = Data()
+        try await subject.deleteAuthenticatorKey()
+        XCTAssertNil(storage.storage[.authenticatorKey])
+    }
+
+    /// `getAuthenticatorKey()` retrieves the authenticator key from storage.
+    func test_getAuthenticatorKey_success() async throws {
+        let key = SymmetricKey(size: .bits256)
+        let data = key.withUnsafeBytes { Data(Array($0)) }
+        storage.storage[.authenticatorKey] = data
+        let authenticatorKey = try await subject.getAuthenticatorKey()
+        XCTAssertEqual(authenticatorKey, data)
+    }
+
+    /// `setAuthenticatorKey()` sets the authenticator key in storage.
+    func test_setAuthenticatorKey_success() async throws {
+        let key = SymmetricKey(size: .bits256)
+        let data = key.withUnsafeBytes { Data(Array($0)) }
+        try await subject.setAuthenticatorKey(data)
+        XCTAssertEqual(storage.storage[.authenticatorKey] as? Data, data)
+    }
+
+    /// `getLastActiveTime()` retrieves the last active time from storage.
+    func test_getLastActiveTime_success() async throws {
+        let date = Date(timeIntervalSince1970: 12345)
+        storage.storage[.lastActiveTime(application: .passwordManager, userId: "1")] = date
+        let lastActiveTime = try await subject.getLastActiveTime(application: .passwordManager, userId: "1")
+        XCTAssertEqual(lastActiveTime, date)
+    }
+
+    /// `setLastActiveTime()` sets the last active time in storage.
+    func test_setLastActiveTime_success() async throws {
+        let date = Date(timeIntervalSince1970: 12345)
+        try await subject.setLastActiveTime(date, application: .passwordManager, userId: "1")
+        XCTAssertEqual(storage.storage[.lastActiveTime(application: .passwordManager, userId: "1")] as? Date, date)
+    }
+
+    /// `getVaultTimeout()` retrieves the vault timeout from storage.
+    func test_getVaultTimeout_success() async throws {
+        storage.storage[.vaultTimeout(application: .authenticator, userId: "1")] = SessionTimeoutValue.oneHour
+        let vaultTimeout: SessionTimeoutValue = try await subject.getVaultTimeout(application: .authenticator, userId: "1") ?? .fifteenMinutes
+        XCTAssertEqual(vaultTimeout, .oneHour)
+    }
+
+    /// `setVaultTimeout()` sets the vault timeout in storage.
+    func test_setVaultTimeout_success() async throws {
+        try await subject.setVaultTimeout(SessionTimeoutValue.oneHour, application: .authenticator, userId: "1")
+        XCTAssertEqual(
+            storage.storage[.vaultTimeout(application: .authenticator, userId: "1")] as? SessionTimeoutValue,
+            .oneHour
+        )
+    }
+}
