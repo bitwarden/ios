78a7d3aa61dcc4f592919207a9b0699d76860eda
Matt Czech <matt@livefront.com>
2025-06-05 15:58:57 -0500

    [PM-22176] Remove Edit FAB and action for deleted items (#1642)

--- BitwardenShared/UI/Vault/VaultItem/ViewItem/ViewItemViewTests.swift
+++ BitwardenShared/UI/Vault/VaultItem/ViewItem/ViewItemViewTests.swift
@@ -164,6 +164,16 @@ class ViewItemViewTests: BitwardenTestCase { // swiftlint:disable:this type_body
         XCTAssertEqual(processor.dispatchedActions.last, .editPressed)
     }
 
+    /// The edit item FAB is hidden if the item has been deleted.
+    @MainActor
+    func test_editItemFloatingActionButton_hidden_cipherDeleted() async throws {
+        processor.state.loadingState = .data(CipherItemState(existing: .fixture(deletedDate: .now), hasPremium: true)!)
+        let fab = try subject.inspect().find(
+            floatingActionButtonWithAccessibilityIdentifier: "EditItemFloatingActionButton"
+        )
+        XCTAssertTrue(fab.isHidden())
+    }
+
     /// Tapping the password history button dispatches the `passwordHistoryPressed` action.
     @MainActor
     func test_passwordHistoryButton_tap() throws {
