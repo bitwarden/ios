182d7ae150a4a087744a39d0d84df14c06e7292f
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-06-10 12:33:27 -0500

    Create mock

---
+++ AuthenticatorBridgeKit/Mocks/MockSharedTimeoutService.swift
@@ -0,0 +1,17 @@
+import AuthenticatorBridgeKit
+import BitwardenKit
+import Foundation
+
+public final class MockSharedTimeoutService: SharedTimeoutService {
+    public func clearTimeout(forUserId userId: String) {
+
+    }
+    
+    public func hasPassedTimeout(userId: String) -> Bool {
+        false
+    }
+    
+    public func updateTimeout(forUserId userId: String, lastActiveDate: Date?, timeoutLength: SessionTimeoutValue) {
+
+    }
+}
