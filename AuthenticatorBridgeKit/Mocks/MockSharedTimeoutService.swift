c75e86bb80998d32bf3b9e9afeea77af4e9b4ba2
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-06-11 13:56:36 -0500

    Build out mock shared timeout service

--- AuthenticatorBridgeKit/Mocks/MockSharedTimeoutService.swift
+++ AuthenticatorBridgeKit/Mocks/MockSharedTimeoutService.swift
@@ -3,19 +3,34 @@ import BitwardenKit
 import Foundation
 
 public final class MockSharedTimeoutService: SharedTimeoutService {
-    public func clearTimeout(forUserId userId: String) {
+    var clearTimeoutUserIds = [String]()
+    var clearTimeoutError: Error?
+    var hasPassedTimeoutResult: Result<[String: Bool], Error> = .success([:])
+    // swiftlint:disable:next large_tuple
+    var updateTimeoutCalls: [(forUserId: String, lastActiveDate: Date?, timeoutLength: SessionTimeoutValue)] = []
+    var updateTimeoutError: Error?
 
+    public init() {}
+
+    public func clearTimeout(forUserId userId: String) async throws {
+        if let clearTimeoutError {
+            throw clearTimeoutError
+        }
+        clearTimeoutUserIds.append(userId)
     }
-    
-    public func hasPassedTimeout(userId: String) -> Bool {
-        false
-    }
-    
-    public func updateTimeout(forUserId userId: String, lastActiveDate: Date?, timeoutLength: SessionTimeoutValue) {
 
+    public func hasPassedTimeout(userId: String) async throws -> Bool {
+        try hasPassedTimeoutResult.get()[userId] ?? false
     }
 
-    public init () {
-
+    public func updateTimeout(
+        forUserId userId: String,
+        lastActiveDate: Date?,
+        timeoutLength: SessionTimeoutValue
+    ) async throws {
+        if let updateTimeoutError {
+            throw updateTimeoutError
+        }
+        updateTimeoutCalls.append((forUserId: userId, lastActiveDate: lastActiveDate, timeoutLength: timeoutLength))
     }
 }
