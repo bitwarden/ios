771e951b3369f6658652583cacc50bd59c7833ca
Matt Czech <matt@livefront.com>
2025-06-13 08:51:31 -0500

    [PM-10285] Update master password reprompt to protect item view (#1660)

--- BitwardenShared/UI/Vault/VaultItem/ViewItem/ViewItemStateTests.swift
+++ BitwardenShared/UI/Vault/VaultItem/ViewItem/ViewItemStateTests.swift
@@ -18,8 +18,7 @@ class ViewItemStateTests: BitwardenTestCase {
                     ),
                     hasPremium: true
                 )!
-            ),
-            hasVerifiedMasterPassword: false
+            )
         )
         XCTAssertTrue(subject.canClone)
     }
@@ -36,8 +35,7 @@ class ViewItemStateTests: BitwardenTestCase {
                     ),
                     hasPremium: true
                 )!
-            ),
-            hasVerifiedMasterPassword: false
+            )
         )
         XCTAssertFalse(subject.canClone)
     }
@@ -56,96 +54,6 @@ class ViewItemStateTests: BitwardenTestCase {
             XCTUnwrap(CipherItemState(existing: .fixture(deletedDate: .now), hasPremium: false))
         ))
         XCTAssertFalse(subject.canEdit)
-    }
-
-    /// `isMasterPasswordRequired` is false when the user has no password.
-    func test_isMasterPasswordRequired_repromptOff_noPassword() {
-        let subject = ViewItemState(
-            loadingState: .data(
-                CipherItemState(
-                    existing: .fixture(
-                        id: "id",
-                        reprompt: .password
-                    ),
-                    hasPremium: true
-                )!
-            ),
-            hasMasterPassword: false,
-            hasVerifiedMasterPassword: false
-        )
-        XCTAssertFalse(subject.isMasterPasswordRequired)
-    }
-
-    /// `isMasterPasswordRequired` is true when the reprompt is on and the master password has not
-    /// been verified yet.
-    func test_isMasterPasswordRequired_repromptOn_unverifiedPassword() {
-        let subject = ViewItemState(
-            loadingState: .data(
-                CipherItemState(
-                    existing: .fixture(
-                        id: "id",
-                        reprompt: .password
-                    ),
-                    hasPremium: true
-                )!
-            ),
-            hasVerifiedMasterPassword: false
-        )
-        XCTAssertTrue(subject.isMasterPasswordRequired)
-    }
-
-    /// `isMasterPasswordRequired` is false when the reprompt is on and the master password has been
-    /// verified.
-    func test_isMasterPasswordRequired_repromptOn_verifiedPassword() {
-        let subject = ViewItemState(
-            loadingState: .data(
-                CipherItemState(
-                    existing: .fixture(
-                        id: "id",
-                        reprompt: .password
-                    ),
-                    hasPremium: true
-                )!
-            ),
-            hasVerifiedMasterPassword: true
-        )
-        XCTAssertFalse(subject.isMasterPasswordRequired)
-    }
-
-    /// `isMasterPasswordRequired` is false when the reprompt is off and the master password has not
-    /// been verified yet.
-    func test_isMasterPasswordRequired_repromptOff_unverifiedPassword() {
-        let subject = ViewItemState(
-            loadingState: .data(
-                CipherItemState(
-                    existing: .fixture(
-                        id: "id",
-                        reprompt: .none
-                    ),
-                    hasPremium: true
-                )!
-            ),
-            hasVerifiedMasterPassword: false
-        )
-        XCTAssertFalse(subject.isMasterPasswordRequired)
-    }
-
-    /// `isMasterPasswordRequired` is false when the reprompt is off and the master password has
-    /// been verified.
-    func test_isMasterPasswordRequired_repromptOff_verifiedPassword() {
-        let subject = ViewItemState(
-            loadingState: .data(
-                CipherItemState(
-                    existing: .fixture(
-                        id: "id",
-                        reprompt: .none
-                    ),
-                    hasPremium: true
-                )!
-            ),
-            hasVerifiedMasterPassword: true
-        )
-        XCTAssertFalse(subject.isMasterPasswordRequired)
     }
 
     /// `navigationTitle` returns the navigation title for the view based on the cipher type.
@@ -193,8 +101,7 @@ class ViewItemStateTests: BitwardenTestCase {
                     ),
                     hasPremium: true
                 )!
-            ),
-            hasVerifiedMasterPassword: false
+            )
         )
         XCTAssertFalse(subject.restrictCipherItemDeletionFlagEnabled)
     }
@@ -211,7 +118,6 @@ class ViewItemStateTests: BitwardenTestCase {
                     hasPremium: true
                 )!
             ),
-            hasVerifiedMasterPassword: false,
             restrictCipherItemDeletionFlagEnabled: true
         )
         XCTAssertTrue(subject.restrictCipherItemDeletionFlagEnabled)
