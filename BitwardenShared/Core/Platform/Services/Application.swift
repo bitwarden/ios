2d0d4633a234689f5e1d69636568a9f4c6c411c0
Matt Czech <matt@livefront.com>
2025-07-03 08:19:25 -0500

    [PM-23304] Fix biometric unlock errors on background launch (#1715)

--- BitwardenShared/Core/Platform/Services/Application.swift
+++ BitwardenShared/Core/Platform/Services/Application.swift
@@ -3,6 +3,9 @@ import UIKit
 /// A protocol for the application instance (i.e. `UIApplication`).
 ///
 public protocol Application {
+    /// The app’s current state, or that of its most active scene.
+    @MainActor var applicationState: UIApplication.State { get }
+
     /// Marks the start of a task with a custom name that should continue if the app enters the background.
     /// See note in `UIApplication+Application.swift`
     /// TODO: PM-11189
