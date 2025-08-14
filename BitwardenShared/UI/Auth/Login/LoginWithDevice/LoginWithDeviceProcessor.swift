8788b22ec3ea6be36c50218799e8b79643b0962b
Andre Rosado <arosado@bitwarden.com>
2025-08-14 15:16:00 +0100

    removed captcha connector code

--- BitwardenShared/UI/Auth/Login/LoginWithDevice/LoginWithDeviceProcessor.swift
+++ BitwardenShared/UI/Auth/Login/LoginWithDevice/LoginWithDeviceProcessor.swift
@@ -16,7 +16,6 @@ final class LoginWithDeviceProcessor: StateProcessor<
 
     typealias Services = HasAuthRepository
         & HasAuthService
-        & HasCaptchaService
         & HasConfigService
         & HasErrorReporter
 
@@ -105,7 +104,7 @@ final class LoginWithDeviceProcessor: StateProcessor<
     }
 
     /// Attempt to login.
-    private func attemptLogin(with request: LoginRequest?, captchaToken: String? = nil) async {
+    private func attemptLogin(with request: LoginRequest?) async {
         do {
             coordinator.showLoadingOverlay(title: Localizations.loggingIn)
             // Used the cached request if it's nil (for example, if the captcha flow just completed).
@@ -116,8 +115,7 @@ final class LoginWithDeviceProcessor: StateProcessor<
             let (privateKey, key) = try await services.authService.loginWithDevice(
                 request,
                 email: state.email,
-                isAuthenticated: state.isAuthenticated,
-                captchaToken: captchaToken
+                isAuthenticated: state.isAuthenticated
             )
 
             // Attempt to unlock the vault.
@@ -177,12 +175,10 @@ final class LoginWithDeviceProcessor: StateProcessor<
     private func handleError(_ error: Error, _ tryAgain: (() async -> Void)? = nil) async {
         coordinator.hideLoadingOverlay()
 
-        // Handle a captcha or two-factor error.
+        // Handle a two-factor error.
         if let identityTokenError = error as? IdentityTokenRequestError {
             switch identityTokenError {
-            case let .captchaRequired(token):
-                await launchCaptchaFlow(with: token)
-            case let .twoFactorRequired(authMethodsData, _, _, _):
+            case let .twoFactorRequired(authMethodsData, _, _):
                 let unlockMethod: TwoFactorUnlockMethod? = if let key = approvedRequest?.key, let authRequestResponse {
                     TwoFactorUnlockMethod.loginWithDevice(
                         key: key,
@@ -207,27 +203,6 @@ final class LoginWithDeviceProcessor: StateProcessor<
         services.errorReporter.log(error: error)
     }
 
-    /// Generates the items needed and authenticates with the captcha flow.
-    ///
-    /// - Parameter siteKey: The site key that was returned with a captcha error. The token used to authenticate
-    ///   with hCaptcha.
-    ///
-    private func launchCaptchaFlow(with siteKey: String) async {
-        do {
-            let url = try services.captchaService.generateCaptchaUrl(with: siteKey)
-            coordinator.navigate(
-                to: .captcha(
-                    url: url,
-                    callbackUrlScheme: services.captchaService.callbackUrlScheme
-                ),
-                context: self
-            )
-        } catch {
-            await coordinator.showErrorAlert(error: error)
-            services.errorReporter.log(error: error)
-        }
-    }
-
     /// Set or reset the auto-check timer.
     private func setCheckTimer() {
         checkTimer?.invalidate()
@@ -237,29 +212,6 @@ final class LoginWithDeviceProcessor: StateProcessor<
             Task { @MainActor [weak self] in
                 await self?.checkForResponse()
             }
-        }
-    }
-}
-
-// MARK: CaptchaFlowDelegate
-
-extension LoginWithDeviceProcessor: CaptchaFlowDelegate {
-    func captchaCompleted(token: String) {
-        Task {
-            await attemptLogin(with: nil, captchaToken: token)
-        }
-    }
-
-    func captchaErrored(error: Error) {
-        guard (error as NSError).code != ASWebAuthenticationSessionError.canceledLogin.rawValue else { return }
-
-        services.errorReporter.log(error: error)
-
-        // Show the alert after a delay to ensure it doesn't try to display over the
-        // closing captcha view.
-        Task { @MainActor in
-            try await Task.sleep(forSeconds: UI.duration(0.6))
-            await coordinator.showErrorAlert(error: error)
         }
     }
 }
