c9c5c116c52f4cda01c749e3382eb8538a9e4412
Carlos Gonçalves <cgoncalves@bitwarden.com>
2025-08-22 15:43:22 +0100

    Add script to update bitwarden sdk revision value

---
+++ Scripts/update-sdk-version.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Script to update SDK version in project-bwa.yml, project-bwk.yml and project-pm.ym
+# Usage: ./Scripts/update-sdk-version.sh <sdk-package> <sdk-version>
+# ./Scripts/update-sdk-version.sh BitwardenSdk 2a6609428275c758fcda5383bfb6b3166ec29eda
+
+set -euo pipefail
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <sdk-package> <sdk-version>"
+    echo "Example: $0 BitwardenSdk 2a6609428275c758fcda5383bfb6b3166ec29eda"
+    exit 1
+fi
+
+SDK_PACKAGE="$1"
+SDK_VERSION="$2"
+FILES=(
+  "project-bwa.yml"
+  "project-bwk.yml"
+  "project-pm.yml"
+)
+
+for file in "${FILES[@]}"; do
+    if [[ -f "$file" ]]; then
+        echo "🔧 Updating revision in $file..."
+        sed -i.bak -E "/^packages:/,/^[^[:space:]]/ {
+          /$SDK_PACKAGE:/,/^[[:space:]]{2}[[:alnum:]]/ {
+            s/^([[:space:]]{4}revision: ).*/\1$SDK_VERSION/
+          }
+        }" "$file"
+        echo "✅ Updated revision line:"
+        grep "revision:" "$file"
+    else
+        echo "⚠️  Skipping missing file: $file"
+    fi
+done
