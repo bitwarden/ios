606d16f2181781187fa65c4b54f14487fca07f62
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-08-28 11:03:51 -0500

    Cleanup

--- BitwardenShared/UI/Auth/ProfileSwitcher/ProfileSwitcherCoordinator.swift
+++ BitwardenShared/UI/Auth/ProfileSwitcher/ProfileSwitcherCoordinator.swift
@@ -1,64 +1,5 @@
 import Foundation
 
-// MARK: - TODO
-
-/// An object that is signaled when specific circumstances in the application flow have been encountered.
-///
-@MainActor
-public protocol ProfileSwitcherCoordinatorDelegate: AnyObject {
-    /// Called when the user locks their vault.
-    ///
-    /// - Parameters:
-    ///   - userId: The user Id of the selected account. Defaults to the active user id if nil.
-    ///   - isManuallyLocking: Whether the user is manually locking the account.
-    ///
-//    func lockVault(userId: String?, isManuallyLocking: Bool)
-
-    /// Called when the user has been logged out.
-    ///
-    /// - Parameters:
-    ///   - userId: The id of the account to log out.
-    ///   - userInitiated: Did a user action initiate this logout?
-    ///
-//    func logout(userId: String?, userInitiated: Bool)
-
-    /// Called when the user taps add account.
-    ///
-//    func didTapAddAccount()
-
-    /// Called when the user taps selects alternate account.
-    ///
-    ///  - Parameter userId: The userId of the selected account.
-    ///
-//    func didTapAccount(userId: String)
-
-    /// Present the login request view.
-    ///
-    /// - Parameter loginRequest: The login request.
-    ///
-//    func presentLoginRequest(_ loginRequest: LoginRequest)
-
-    /// When the user requests an account switch.
-    ///
-    /// - Parameters:
-    ///   - userId: The user Id of the account.
-    ///   - isAutomatic: Did the system trigger the account switch?
-    ///   - authCompletionRoute: An optional route that should be navigated to after switching
-    ///     accounts and vault unlock
-    ///
-//    func switchAccount(userId: String, isAutomatic: Bool, authCompletionRoute: AppRoute?)
-
-    /// Called when the user needs to switch to the settings tab and navigate to a `SettingsRoute`.
-    ///
-    /// - Parameter route: The route to navigate to in the settings tab.
-    ///
-//    func switchToSettingsTab(route: SettingsRoute)
-
-    /// The `State` for a toast view.
-//    var toast: Toast? { get set }
-}
-
-
 // MARK: - ProfileSwitcherCoordinator
 
 /// A coordinator that manages navigation in the profile switcher.
@@ -70,9 +11,6 @@ class ProfileSwitcherCoordinator: NSObject, Coordinator, HasStackNavigator {
     typealias Service = HasAuthRepository
 
     // MARK: Private Properties
-
-    /// The delegate for this coordinator, used to notify when profile switching actions occur.
-    private weak var delegate: ProfileSwitcherCoordinatorDelegate?
 
     private var handler: ProfileSwitcherHandler
 
