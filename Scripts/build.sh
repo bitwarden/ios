107b932482510ea4282f4b63c522e6ffee8083fa
Federico Maccaroni <fedemkr@gmail.com>
2025-08-29 10:11:22 -0300

    Revert "PM-23543 Added Swift active compilation condition flag DISABLE_ASSERTION_FAILURE_ON_ERROR that can be used to disable assertion failure on DEBUG error reporter logging. This has been set in place in the Simulator builds on CI so QA Automation doesn't crash when an error is thrown to the log reporter."
    This reverts commit b37438f8618883aabfb2e5be3dcd870803ef7d72.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -61,7 +61,7 @@ case "$MODE" in
     xcrun xcodebuild \
       -workspace Bitwarden.xcworkspace \
       -scheme "${BUILD_SCHEME}" \
-      -configuration Debug SWIFT_ACTIVE_COMPILATION_CONDITIONS="$(inherited) DISABLE_ASSERTION_FAILURE_ON_ERROR"\
+      -configuration Debug \
       -destination "generic/platform=iOS Simulator" \
       -derivedDataPath "${DERIVED_DATA_PATH}" \
       | xcbeautify --renderer github-actions
