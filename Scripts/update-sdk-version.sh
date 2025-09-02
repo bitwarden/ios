e7ef69ba08041522b76d4016e1f9b678fd6808a2
Álison Fernandes <vvolkgang@users.noreply.github.com>
2025-09-02 21:19:33 +0100

    update sdk script sets sdk version as a comment now

--- Scripts/update-sdk-version.sh
+++ Scripts/update-sdk-version.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 
-# Script to update SDK version in project-common.yml
-# Usage: ./Scripts/update-sdk-version.sh <sdk-package> <sdk-version>
-# ./Scripts/update-sdk-version.sh BitwardenSdk 2a6609428275c758fcda5383bfb6b3166ec29eda
+# Update SDK revision in project-common.yml
 
 set -euo pipefail
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 <sdk-package> <sdk-version>"
-    echo "Example: $0 BitwardenSdk 2a6609428275c758fcda5383bfb6b3166ec29eda"
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <sdk-package> <sdk-swift-ref> <sdk-version>"
+    echo "Example: $0 BitwardenSdk 2a6609428275c758fcda5383bfb6b3166ec29eda 1.0.0-281-a1611ee"
     exit 1
 fi
 
 SDK_PACKAGE="$1"
-SDK_VERSION="$2"
-FILES=(
-  "project-common.yml"
-)
+SDK_SWIFT_REF="$2"
+SDK_VERSION="$3"
+FILE="project-common.yml"
 
-for file in "${FILES[@]}"; do
-    if [[ -f "$file" ]]; then
-        echo "🔧 Updating revision in $file..."
-        yq -i ".packages[\"$SDK_PACKAGE\"].revision = \"$SDK_VERSION\"" "$file"
-        echo "✅ Updated revision line:"
-        grep "revision:" "$file"
-    else
-        echo "⚠️  Skipping missing file: $file"
-    fi
-done
+echo "🔧 Updating revision in $FILE..."
+yq -i ".packages[\"$SDK_PACKAGE\"].revision = \"$SDK_SWIFT_REF\" | .packages[\"$SDK_PACKAGE\"].revision line_comment = \"$SDK_VERSION\"" "$FILE"
+echo "✅ Updated revision line:"
+grep -A 3 "$SDK_PACKAGE:" "$FILE"
