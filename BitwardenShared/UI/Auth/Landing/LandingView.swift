49bd9dbe2b4e864acb423adaedd1c8f24863ed1b
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-09-08 11:52:09 -0500

    Remove bottom

--- BitwardenShared/UI/Auth/Landing/LandingView.swift
+++ BitwardenShared/UI/Auth/Landing/LandingView.swift
@@ -14,7 +14,7 @@ struct LandingView: View {
     @ObservedObject public var store: Store<LandingState, LandingAction, LandingEffect>
 
     var body: some View {
-        ZStack(alignment: .bottom) {
+        ZStack {
             mainContent
             profileSwitcher
         }
