8c2df2c098cee216946875c285de59652d40d84c
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-09-11 10:18:13 -0500

    Refactor

--- BitwardenShared/UI/Auth/ProfileSwitcher/ProfileSwitcherSheet.swift
+++ BitwardenShared/UI/Auth/ProfileSwitcher/ProfileSwitcherSheet.swift
@@ -11,7 +11,7 @@ public struct ProfileSwitcherSheet: View {
             SharedAsset.Colors.backgroundPrimary.swiftUIColor.ignoresSafeArea()
 
             SectionView(Localizations.selectAccount) {
-                accounts
+                ProfileSwitcherAccountsView(store: store)
                 if store.state.showsAddAccount {
                     addAccountRow
                 }
@@ -44,90 +44,6 @@ public struct ProfileSwitcherSheet: View {
         .buttonStyle(.bitwardenBorderless)
         .frame(maxWidth: .infinity, alignment: .center)
         .accessibilityIdentifier("AddAccountButton")
-    }
-
-    /// A group of account views
-    private var accounts: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            ForEachIndexed(store.state.alternateAccounts, id: \.self) { _, account in
-                profileSwitcherRow(accountProfile: account)
-            }
-            selectedProfileSwitcherRow
-        }
-        .clipShape(RoundedRectangle(cornerRadius: 8))
-    }
-
-    /// A row to display the active account profile
-    ///
-    /// - Parameter accountProfile: A `ProfileSwitcherItem` to display in row format
-    ///
-    @ViewBuilder private var selectedProfileSwitcherRow: some View {
-        if let profile = store.state.activeAccountProfile {
-            profileSwitcherRow(
-                accountProfile: profile,
-                showDivider: false
-            )
-        }
-    }
-
-    // MARK: Private Methods
-
-    /// A row to display an account profile
-    ///
-    /// - Parameters
-    ///     - accountProfile: A `ProfileSwitcherItem` to display in row format
-    ///     - showDivider: Should the cell show a divider at the bottom.
-    ///
-    @ViewBuilder
-    private func profileSwitcherRow(
-        accountProfile: ProfileSwitcherItem,
-        showDivider: Bool = true
-    ) -> some View {
-        let isActive = (accountProfile.userId == store.state.activeAccountId)
-        ProfileSwitcherRow(
-            store: store.child(
-                state: { _ in
-                    ProfileSwitcherRowState(
-                        allowLockAndLogout: store.state.allowLockAndLogout,
-                        shouldTakeAccessibilityFocus: store.state.isVisible
-                            && isActive,
-                        showDivider: showDivider,
-                        rowType: isActive
-                            ? .active(accountProfile)
-                            : .alternate(accountProfile),
-                        trailingIconAccessibilityID: isActive
-                            ? "ActiveVaultIcon"
-                            : "InactiveVaultIcon"
-                    )
-                },
-                mapAction: { action in
-                    switch action {
-                    case let .accessibility(accessibilityAction):
-                        switch accessibilityAction {
-                        case .logout:
-                            .accessibility(.logout(accountProfile))
-                        case .remove:
-                            .accessibility(.remove(accountProfile))
-                        }
-                    }
-                },
-                mapEffect: { effect in
-                    switch effect {
-                    case let .accessibility(accessibility):
-                        switch accessibility {
-                        case .lock:
-                            .accessibility(.lock(accountProfile))
-                        case .select:
-                            .accessibility(.select(accountProfile))
-                        }
-                    case .longPressed:
-                        .accountLongPressed(accountProfile)
-                    case .pressed:
-                        .accountPressed(accountProfile)
-                    }
-                }
-            )
-        )
     }
 }
 
