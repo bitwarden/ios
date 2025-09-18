866f36057da6e2709b7485b5c5403c5b81091d1e
Matt Czech <matt@livefront.com>
2025-09-18 12:00:24 -0500

    Add mock methods to MockCryptoClient

--- AuthenticatorShared/Core/Auth/Services/TestHelpers/MockCryptoClient.swift
+++ AuthenticatorShared/Core/Auth/Services/TestHelpers/MockCryptoClient.swift
@@ -44,6 +44,28 @@ class MockCryptoClient: CryptoClientProtocol {
     var initializeUserCryptoRequest: InitUserCryptoRequest?
     var initializeUserCryptoResult: Result<Void, Error> = .success(())
 
+    var makeUpdateKdfKdf: Kdf?
+    var makeUpdateKdfPassword: String?
+    var makeUpdateKdfResult: Result<UpdateKdfResponse, Error> = .success(
+        UpdateKdfResponse(
+            masterPasswordAuthenticationData: MasterPasswordAuthenticationData(
+                kdf: .pbkdf2(iterations: NonZeroU32(600_000)),
+                salt: "AUTHENTICATION_SALT",
+                masterPasswordAuthenticationHash: "MASTER_PASSWORD_AUTHENTICATION_HASH"
+            ),
+            masterPasswordUnlockData: MasterPasswordUnlockData(
+                kdf: .pbkdf2(iterations: NonZeroU32(600_000)),
+                masterKeyWrappedUserKey: "MASTER_KEY_WRAPPED_USER_KEY",
+                salt: "UNLOCK_SALT"
+            ),
+            oldMasterPasswordAuthenticationData: MasterPasswordAuthenticationData(
+                kdf: .pbkdf2(iterations: NonZeroU32(600_000)),
+                salt: "OLD_AUTHENTICATION_SALT",
+                masterPasswordAuthenticationHash: "MASTER_PASSWORD_AUTHENTICATION_HASH"
+            )
+        )
+    )
+
     var updatePasswordNewPassword: String?
     var updatePasswordResult: Result<UpdatePasswordResponse, Error> = .success(
         UpdatePasswordResponse(
@@ -93,6 +115,17 @@ class MockCryptoClient: CryptoClientProtocol {
     func initializeUserCrypto(req: InitUserCryptoRequest) async throws {
         initializeUserCryptoRequest = req
         return try initializeUserCryptoResult.get()
+    }
+
+    func makeUpdateKdf(password: String, kdf: Kdf) throws -> UpdateKdfResponse {
+        makeUpdateKdfPassword = password
+        makeUpdateKdfKdf = kdf
+        return try makeUpdateKdfResult.get()
+    }
+
+    func makeUpdatePassword(newPassword: String) throws -> UpdatePasswordResponse {
+        updatePasswordNewPassword = newPassword
+        return try updatePasswordResult.get()
     }
 
     func updatePassword(newPassword: String) throws -> BitwardenSdk.UpdatePasswordResponse {
