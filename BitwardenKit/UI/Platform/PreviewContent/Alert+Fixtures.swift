cdc0fa03702a2ff20e7c8aee2956632d6f29dfc5
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-10-28 08:22:41 -0500

    Public

--- BitwardenKit/UI/Platform/PreviewContent/Alert+Fixtures.swift
+++ BitwardenKit/UI/Platform/PreviewContent/Alert+Fixtures.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 #if DEBUG
-extension Alert {
+public extension Alert {
     static func fixture(
         title: String = "🍎",
         message: String? = "🥝",
@@ -19,7 +19,7 @@ extension Alert {
     }
 }
 
-extension AlertAction {
+public extension AlertAction {
     static func ok(
         title: String = "OK",
         style: UIAlertAction.Style = .default,
@@ -49,7 +49,7 @@ extension AlertAction {
     }
 }
 
-extension AlertTextField {
+public extension AlertTextField {
     static func fixture(
         id: String = "field",
         autocapitalizationType: UITextAutocapitalizationType = .allCharacters,
