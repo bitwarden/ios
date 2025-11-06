3149dee2bcc37f45166d824ba2b29d5e15e1e792
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-11-06 12:44:18 -0600

    Decouple test

--- BitwardenKit/UI/Platform/Application/Utilities/AnyRouterTests.swift
+++ BitwardenKit/UI/Platform/Application/Utilities/AnyRouterTests.swift
@@ -4,40 +4,51 @@ import XCTest
 
 // MARK: - AnyRouterTests
 
-//class AnyRouterTests: BitwardenTestCase {
-//    // MARK: Properties
-//
-//    var router: MockRouter<AuthEvent, AuthRoute>!
-//    var subject: AnyRouter<AuthEvent, AuthRoute>!
-//
-//    // MARK: Setup & Teardown
-//
-//    override func setUp() {
-//        super.setUp()
-//        router = MockRouter(routeForEvent: { _ in .landing })
-//        subject = router.asAnyRouter()
-//    }
-//
-//    override func tearDown() {
-//        super.tearDown()
-//        router = nil
-//        subject = nil
-//    }
-//
-//    // MARK: Tests
-//
-//    /// `handleAndRoute()` calls the `handleAndRoute()` method on the wrapped router.
-//    @MainActor
-//    func test_handleAndRoute() async {
-//        var didStart = false
-//        router.routeForEvent = { event in
-//            guard case .didStart = event else { return .landing }
-//            didStart = true
-//            return .complete
-//        }
-//        let route = await subject.handleAndRoute(.didStart)
-//        XCTAssertEqual(router.events, [.didStart])
-//        XCTAssertEqual(route, .complete)
-//        XCTAssertTrue(didStart)
-//    }
-//}
+class AnyRouterTests: BitwardenTestCase {
+    // MARK: Types
+
+    enum TestEvent: Equatable {
+        case didStart
+    }
+
+    enum TestRoute: Equatable {
+        case complete
+        case landing
+    }
+
+    // MARK: Properties
+
+    var router: MockRouter<TestEvent, TestRoute>!
+    var subject: AnyRouter<TestEvent, TestRoute>!
+
+    // MARK: Setup & Teardown
+
+    override func setUp() {
+        super.setUp()
+        router = MockRouter(routeForEvent: { _ in .landing })
+        subject = router.asAnyRouter()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        router = nil
+        subject = nil
+    }
+
+    // MARK: Tests
+
+    /// `handleAndRoute()` calls the `handleAndRoute()` method on the wrapped router.
+    @MainActor
+    func test_handleAndRoute() async {
+        var didStart = false
+        router.routeForEvent = { event in
+            guard case .didStart = event else { return .landing }
+            didStart = true
+            return .complete
+        }
+        let route = await subject.handleAndRoute(.didStart)
+        XCTAssertEqual(router.events, [.didStart])
+        XCTAssertEqual(route, .complete)
+        XCTAssertTrue(didStart)
+    }
+}
