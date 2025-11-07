4cf22d32481c43835958c83ee879404c1c553540
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-11-07 12:23:00 -0600

    Automock

--- BitwardenKit/Core/Platform/Services/ActiveAccountStateProvider.swift
+++ BitwardenKit/Core/Platform/Services/ActiveAccountStateProvider.swift
@@ -1,6 +1,6 @@
 /// Protocol wrapping information about the currently active account.
 /// In practice, this is the `StateService` in each app.
-public protocol ActiveAccountStateProvider: AnyObject {
+public protocol ActiveAccountStateProvider: AnyObject { // sourcery: AutoMockable
     /// Gets the active account id.
     ///
     /// - Returns: The active user account id.
