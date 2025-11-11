d4e75a1a9fb282596bdb3edc090dce878fd0e10b
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-11-11 11:51:00 -0600

    PM to test

--- BitwardenKit/Application/TestHelpers/ServiceContainer.swift
+++ BitwardenKit/Application/TestHelpers/ServiceContainer.swift
@@ -5,6 +5,7 @@ import BitwardenKitMocks
 ///
 typealias Services = HasConfigService
     & HasEnvironmentService
+    & HasErrorReportBuilder
     & HasErrorReporter
     & HasTimeProvider
 
@@ -12,17 +13,20 @@ typealias Services = HasConfigService
 ///
 class ServiceContainer: Services {
     let configService: ConfigService
+    let errorReportBuilder: any ErrorReportBuilder
     let environmentService: EnvironmentService
     let errorReporter: ErrorReporter
     let timeProvider: TimeProvider
 
     required init(
         configService: ConfigService,
+        errorReportBuilder: ErrorReportBuilder,
         environmentService: EnvironmentService,
         errorReporter: ErrorReporter,
         timeProvider: TimeProvider,
     ) {
         self.configService = configService
+        self.errorReportBuilder = errorReportBuilder
         self.environmentService = environmentService
         self.errorReporter = errorReporter
         self.timeProvider = timeProvider
@@ -32,12 +36,14 @@ class ServiceContainer: Services {
 extension ServiceContainer {
     static func withMocks(
         configService: ConfigService = MockConfigService(),
+        errorReportBuilder: ErrorReportBuilder = MockErrorReportBuilder(),
         environmentService: EnvironmentService = MockEnvironmentService(),
         errorReporter: ErrorReporter = MockErrorReporter(),
         timeProvider: TimeProvider = MockTimeProvider(.currentTime),
     ) -> ServiceContainer {
         self.init(
             configService: configService,
+            errorReportBuilder: errorReportBuilder,
             environmentService: environmentService,
             errorReporter: errorReporter,
             timeProvider: timeProvider,
