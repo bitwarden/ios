b8161ee72c897d352760fbcc65485de034dd8d89
Matt Czech <matt@livefront.com>
2025-11-17 11:45:54 -0600

    [PM-26063] Add reusable component for the Flight Recorder settings view (#2141)

--- BitwardenKit/UI/Platform/Settings/FlightRecorder/FlightRecorderSettingsSection/FlightRecorderSettingsSectionStateTests.swift
+++ BitwardenKit/UI/Platform/Settings/FlightRecorder/FlightRecorderSettingsSection/FlightRecorderSettingsSectionStateTests.swift
@@ -1,22 +1,21 @@
-import BitwardenKit
 import BitwardenResources
 import XCTest
 
-@testable import BitwardenShared
+@testable import BitwardenKit
 
-class AboutStateTests: BitwardenTestCase {
+class FlightRecorderSettingsSectionStateTests: BitwardenTestCase {
     /// `flightRecorderToggleAccessibilityLabel` returns the flight recorder toggle's accessibility
     /// label when the flight recorder is off.
     func test_flightRecorderToggleAccessibilityLabel_flightRecorderOff() {
-        let subject = AboutState(flightRecorderActiveLog: nil)
+        let subject = FlightRecorderSettingsSectionState()
         XCTAssertEqual(subject.flightRecorderToggleAccessibilityLabel, Localizations.flightRecorder)
     }
 
     /// `flightRecorderToggleAccessibilityLabel` returns the flight recorder toggle's accessibility
     /// label when the flight recorder is on.
     func test_flightRecorderToggleAccessibilityLabel_flightRecorderOn() {
-        let subject = AboutState(
-            flightRecorderActiveLog: FlightRecorderData.LogMetadata(
+        let subject = FlightRecorderSettingsSectionState(
+            activeLog: FlightRecorderData.LogMetadata(
                 duration: .eightHours,
                 startDate: Date(year: 2025, month: 5, day: 1),
             ),
