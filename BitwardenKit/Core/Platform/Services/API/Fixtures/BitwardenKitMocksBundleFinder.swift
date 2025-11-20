1ad6b750674317f40893cca50107e08d3dabb1e9
Matt Czech <matt@livefront.com>
2025-11-20 16:55:35 -0600

    [PM-27143] Add default bundle for APITestData/TestDataHelpers in each framework

--- BitwardenKit/Core/Platform/Services/API/Fixtures/BitwardenKitMocksBundleFinder.swift
+++ BitwardenKit/Core/Platform/Services/API/Fixtures/BitwardenKitMocksBundleFinder.swift
@@ -3,3 +3,8 @@ import Foundation
 class BitwardenKitMocksBundleFinder {
     static let bundle = Bundle(for: BitwardenKitMocksBundleFinder.self)
 }
+
+public extension Bundle {
+    /// The bundle for the `BitwardenKitMocks` target.
+    static let bitwardenKitMocks = BitwardenKitMocksBundleFinder.bundle
+}
