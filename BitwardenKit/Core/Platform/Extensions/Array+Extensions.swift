f70c40b41a115f67ec38631ee6093059c7caf4ba
Katherine Bertelsen <kbertelsen@bitwarden.com>
2025-12-03 12:20:32 -0600

    Update comment

--- BitwardenKit/Core/Platform/Extensions/Array+Extensions.swift
+++ BitwardenKit/Core/Platform/Extensions/Array+Extensions.swift
@@ -1,6 +1,13 @@
 public extension Array {
-    /// Gets the element on the index when inside the bounds
-    /// of the array, otherwise returns `nil`.
+    /// Safely access elements in an array by index without running into an out-of-bounds error.
+    /// This works like normal array subscript access, but if the index is out of bounds, then
+    /// returns nil instead of throwing an error. This can be useful in cases, particularly in tests,
+    /// where we want to access array elements by index number, and not have additional error handling
+    /// if the index in question does not exist in the array.
+    ///
+    /// - Parameters:
+    ///   - index: The position of the element to access.
+    /// - Returns: The element at the specified index if it is within bounds, otherwise `nil`.
     subscript(safeIndex index: Int) -> Element? {
         guard index >= 0, index < endIndex else {
             return nil
