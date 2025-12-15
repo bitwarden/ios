c89daf4b6b576dd7d8c05d036320b1f5841a0cfc
André Bispo <abispo@bitwarden.com>
2025-12-15 15:42:13 +0000

    [PM-28997] Add ViewInspector and Snapshot testing

---
+++ BitwardenShared/UI/Vault/MigrateToMyItems/MigrateToMyItemsView+SnapshotTests.swift
@@ -0,0 +1,53 @@
+// swiftlint:disable:this file_name
+import BitwardenKit
+import BitwardenKitMocks
+import SnapshotTesting
+import XCTest
+
+@testable import BitwardenShared
+
+class MigrateToMyItemsViewSnapshotTests: BitwardenTestCase {
+    // MARK: Properties
+
+    var processor: MockProcessor<MigrateToMyItemsState, MigrateToMyItemsAction, MigrateToMyItemsEffect>!
+    var subject: MigrateToMyItemsView!
+
+    // MARK: Setup & Teardown
+
+    override func setUp() {
+        super.setUp()
+
+        processor = MockProcessor(state: MigrateToMyItemsState(organizationName: "Acme Corporation"))
+        let store = Store(processor: processor)
+
+        subject = MigrateToMyItemsView(store: store)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+
+        processor = nil
+        subject = nil
+    }
+
+    // MARK: Snapshots
+
+    /// The transfer page renders correctly.
+    @MainActor
+    func test_snapshot_transferPage() {
+        assertSnapshots(
+            of: subject.navStackWrapped,
+            as: [.defaultPortrait, .defaultPortraitDark, .defaultPortraitAX5],
+        )
+    }
+
+    /// The decline confirmation page renders correctly.
+    @MainActor
+    func test_snapshot_declineConfirmationPage() {
+        processor.state.page = .declineConfirmation
+        assertSnapshots(
+            of: subject.navStackWrapped,
+            as: [.defaultPortrait, .defaultPortraitDark, .defaultPortraitAX5],
+        )
+    }
+}
