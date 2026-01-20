6ce22a1f91518bdb709c7d466358b87c383384de
Federico Maccaroni <fedemkr@gmail.com>
2026-01-20 13:30:15 -0300

    [PM-29636] Implemented vault archive group list empty state (#2264)

--- BitwardenShared/UI/Vault/Vault/VaultGroup/VaultGroupStateTests.swift
+++ BitwardenShared/UI/Vault/Vault/VaultGroup/VaultGroupStateTests.swift
@@ -1,3 +1,4 @@
+import BitwardenResources
 import XCTest
 
 @testable import BitwardenShared
@@ -44,5 +45,89 @@ class VaultGroupStateTests: BitwardenTestCase {
 
         let subjectTrash = VaultGroupState(group: .trash, vaultFilterType: .myVault)
         XCTAssertNil(subjectTrash.newItemButtonType)
+    }
+
+    /// `noItemsString` returns the appropriate message based on the group.
+    func test_noItemsString() {
+        let subjectArchive = VaultGroupState(group: .archive, vaultFilterType: .myVault)
+        XCTAssertEqual(subjectArchive.noItemsString, Localizations.archiveEmptyDescriptionLong)
+
+        let subjectCard = VaultGroupState(group: .card, vaultFilterType: .myVault)
+        XCTAssertEqual(subjectCard.noItemsString, Localizations.thereAreNoCardsInYourVault)
+
+        let subjectCollection = VaultGroupState(
+            group: .collection(id: "1", name: "Collection", organizationId: ""),
+            vaultFilterType: .myVault,
+        )
+        XCTAssertEqual(subjectCollection.noItemsString, Localizations.noItemsCollection)
+
+        let subjectFolder = VaultGroupState(
+            group: .folder(id: "1", name: "Folder"),
+            vaultFilterType: .myVault,
+        )
+        XCTAssertEqual(subjectFolder.noItemsString, Localizations.noItemsFolder)
+
+        let subjectIdentity = VaultGroupState(group: .identity, vaultFilterType: .myVault)
+        XCTAssertEqual(subjectIdentity.noItemsString, Localizations.thereAreNoIdentitiesInYourVault)
+
+        let subjectLogin = VaultGroupState(group: .login, vaultFilterType: .myVault)
+        XCTAssertEqual(subjectLogin.noItemsString, Localizations.thereAreNoLoginsInYourVault)
+
+        let subjectSecureNote = VaultGroupState(group: .secureNote, vaultFilterType: .myVault)
+        XCTAssertEqual(subjectSecureNote.noItemsString, Localizations.thereAreNoNotesInYourVault)
+
+        let subjectSSHKey = VaultGroupState(group: .sshKey, vaultFilterType: .myVault)
+        XCTAssertEqual(subjectSSHKey.noItemsString, Localizations.thereAreNoSSHKeysInYourVault)
+
+        let subjectTrash = VaultGroupState(group: .trash, vaultFilterType: .myVault)
+        XCTAssertEqual(subjectTrash.noItemsString, Localizations.noItemsTrash)
+
+        let subjectNoFolder = VaultGroupState(group: .noFolder, vaultFilterType: .myVault)
+        XCTAssertEqual(subjectNoFolder.noItemsString, Localizations.noItems)
+
+        let subjectTotp = VaultGroupState(group: .totp, vaultFilterType: .myVault)
+        XCTAssertEqual(subjectTotp.noItemsString, Localizations.noItems)
+    }
+
+    /// `noItemsTitle` returns the appropriate title based on the group.
+    func test_noItemsTitle() {
+        let subjectArchive = VaultGroupState(group: .archive, vaultFilterType: .myVault)
+        XCTAssertEqual(subjectArchive.noItemsTitle, Localizations.archiveIsEmpty)
+
+        let subjectCard = VaultGroupState(group: .card, vaultFilterType: .myVault)
+        XCTAssertNil(subjectCard.noItemsTitle)
+
+        let subjectCollection = VaultGroupState(
+            group: .collection(id: "1", name: "Collection", organizationId: ""),
+            vaultFilterType: .myVault,
+        )
+        XCTAssertNil(subjectCollection.noItemsTitle)
+
+        let subjectFolder = VaultGroupState(
+            group: .folder(id: "1", name: "Folder"),
+            vaultFilterType: .myVault,
+        )
+        XCTAssertNil(subjectFolder.noItemsTitle)
+
+        let subjectIdentity = VaultGroupState(group: .identity, vaultFilterType: .myVault)
+        XCTAssertNil(subjectIdentity.noItemsTitle)
+
+        let subjectLogin = VaultGroupState(group: .login, vaultFilterType: .myVault)
+        XCTAssertNil(subjectLogin.noItemsTitle)
+
+        let subjectSecureNote = VaultGroupState(group: .secureNote, vaultFilterType: .myVault)
+        XCTAssertNil(subjectSecureNote.noItemsTitle)
+
+        let subjectSSHKey = VaultGroupState(group: .sshKey, vaultFilterType: .myVault)
+        XCTAssertNil(subjectSSHKey.noItemsTitle)
+
+        let subjectTrash = VaultGroupState(group: .trash, vaultFilterType: .myVault)
+        XCTAssertNil(subjectTrash.noItemsTitle)
+
+        let subjectNoFolder = VaultGroupState(group: .noFolder, vaultFilterType: .myVault)
+        XCTAssertNil(subjectNoFolder.noItemsTitle)
+
+        let subjectTotp = VaultGroupState(group: .totp, vaultFilterType: .myVault)
+        XCTAssertNil(subjectTotp.noItemsTitle)
     }
 }
