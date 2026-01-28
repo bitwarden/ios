28bcefe05e498ba8f369e3089a241771fbd825e8
Matt Czech <matt@livefront.com>
2026-01-28 13:02:27 -0600

    [PM-24007] Update KeychainRepository to update existing items instead of delete/add

--- BitwardenShared/Core/Auth/Services/KeychainService.swift
+++ BitwardenShared/Core/Auth/Services/KeychainService.swift
@@ -38,6 +38,14 @@ protocol KeychainService: AnyObject {
     /// - Returns: The search results.
     ///
     func search(query: CFDictionary) throws -> AnyObject?
+
+    /// Updates an existing keychain item.
+    ///
+    /// - Parameters:
+    ///   - query: Query to identify the item to update.
+    ///   - attributes: New attributes to set.
+    ///
+    func update(query: CFDictionary, attributes: CFDictionary) throws
 }
 
 // MARK: - DefaultKeychainService
