494bb2d46dc1bc00ed61d25ab29756cc6f18b16f
Katherine Bertelsen <kbertelsen@bitwarden.com>
2026-02-06 14:51:40 -0600

    Create test

--- BitwardenShared/Core/Auth/Models/Request/WebAuthnLoginSaveCredentialRequestModel.swift
+++ BitwardenShared/Core/Auth/Models/Request/WebAuthnLoginSaveCredentialRequestModel.swift
@@ -3,29 +3,59 @@ import Foundation
 import Networking
 
 /// The request body for a request to save a WebAuthn credential for authenticating or decrypting the Bitwarden account.
-struct WebAuthnLoginSaveCredentialRequestModel: JSONRequestBody, Equatable {
+public struct WebAuthnLoginSaveCredentialRequestModel: JSONRequestBody, Equatable {
     // MARK: Properties
 
     /// The response received from the authenticator.
     /// This contains all information needed for future authentication flows.
-    let deviceResponse: WebAuthnPublicKeyCredentialWithAttestationResponse
+    public let deviceResponse: WebAuthnPublicKeyCredentialWithAttestationResponse
 
     /// Encrypted private key in rotatable key set.
-    let encryptedPrivateKey: EncString?
+    public let encryptedPrivateKey: EncString?
 
     /// Encrypted public key in rotatable key set.
-    let encryptedPublicKey: EncString?
+    public let encryptedPublicKey: EncString?
 
     /// Encapsulated user key in rotatable key set.
-    let encryptedUserKey: EncString?
+    public let encryptedUserKey: EncString?
 
     /// Nickname chosen by the user to identify this credential
-    let name: String
+    public let name: String
 
     /// `true` if the credential was created with PRF support.
-    let supportsPrf: Bool
+    public let supportsPrf: Bool
 
     /// Token required by the server to complete the creation.
     /// It contains encrypted information that the server needs to verify the credential.
-    let token: String
+    public let token: String
+
+    // MARK: Initialization
+
+    /// Creates a new `WebAuthnLoginSaveCredentialRequestModel`.
+    ///
+    /// - Parameters:
+    ///   - deviceResponse: The response received from the authenticator.
+    ///   - encryptedPrivateKey: Encrypted private key in rotatable key set.
+    ///   - encryptedPublicKey: Encrypted public key in rotatable key set.
+    ///   - encryptedUserKey: Encapsulated user key in rotatable key set.
+    ///   - name: Nickname chosen by the user to identify this credential.
+    ///   - supportsPrf: `true` if the credential was created with PRF support.
+    ///   - token: Token required by the server to complete the creation.
+    public init(
+        deviceResponse: WebAuthnPublicKeyCredentialWithAttestationResponse,
+        encryptedPrivateKey: EncString?,
+        encryptedPublicKey: EncString?,
+        encryptedUserKey: EncString?,
+        name: String,
+        supportsPrf: Bool,
+        token: String
+    ) {
+        self.deviceResponse = deviceResponse
+        self.encryptedPrivateKey = encryptedPrivateKey
+        self.encryptedPublicKey = encryptedPublicKey
+        self.encryptedUserKey = encryptedUserKey
+        self.name = name
+        self.supportsPrf = supportsPrf
+        self.token = token
+    }
 }
