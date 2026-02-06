a0a1d717292c852da7c635b2a24b603a858731b2
Federico Maccaroni <fedemkr@gmail.com>
2026-02-06 15:22:49 -0300

    [PM-31654] fix: Handle premium subscription required on Archive group in the vault list (#2327)

--- BitwardenShared/UI/Vault/Views/VaultListItemRow/VaultListItemRowView+SnapshotTests.swift
+++ BitwardenShared/UI/Vault/Views/VaultListItemRow/VaultListItemRowView+SnapshotTests.swift
@@ -37,6 +37,18 @@ class VaultListItemRowViewTests: BitwardenTestCase {
 
     // MARK: Snapshots
 
+    /// Test that the archive with premium subscription expired renders correctly.
+    @MainActor
+    func disabletest_snapshot_archivePremiumRequired() {
+        processor.state.iconBaseURL = .example
+        processor.state.item = VaultListItem(
+            id: "ID",
+            hasPremium: false,
+            itemType: .group(.archive, 0)
+        )
+        assertSnapshot(of: subject, as: .fixedSize())
+    }
+
     /// Test that the default view renders correctly.
     @MainActor
     func disabletest_snapshot_default() {
