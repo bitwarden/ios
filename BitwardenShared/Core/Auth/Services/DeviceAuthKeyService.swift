8216da29bca22db76e82e9f6f89df84668de16f3
Katherine Bertelsen <kbertelsen@bitwarden.com>
2026-02-11 14:04:17 -0600

    Adjust comments

--- BitwardenShared/Core/Auth/Services/DeviceAuthKeyService.swift
+++ BitwardenShared/Core/Auth/Services/DeviceAuthKeyService.swift
@@ -9,11 +9,12 @@ import os.log
 protocol DeviceAuthKeyService {
     /// Create device passkey with PRF encryption key.
     ///
-    /// Before calling, vault must be unlocked to wrap user encryption key.
-    ///  - Parameters:
-    ///      - masterPasswordHash: Master password hash suitable for server authentication.
-    ///      - overwrite: Whether to overwrite an existing value if a previous one is already found.
-    ///      - userId: Currently active user ID for the account.
+    /// Before calling, the vault must be unlocked to wrap user encryption key.
+    ///
+    /// - Parameters:
+    ///   - masterPasswordHash: Master password hash suitable for server authentication.
+    ///   - overwrite: Whether to overwrite an existing value if a previous one is already found.
+    ///   - userId: Currently active user ID for the account.
     func createDeviceAuthKey(
         masterPasswordHash: String,
         overwrite: Bool,
@@ -23,12 +24,12 @@ protocol DeviceAuthKeyService {
     /// Signs a passkey assertion request with the device auth key, if it exists and matches the given
     /// ``recordIdentifier``.
     ///
-    ///  - Parameters:
-    ///      - request: The passkey assertion request.
-    ///      - recordIdentifier: The record identifier for the ``ASPasskeyCredentialIdentity``  related to the passkey
-    ///                    assertion request,  which should be equal to the cipher ID of the device auth key record.
-    ///      - userId: Currently active user ID for the account.
-    /// - Returns: A ``GetAssertionResult``, or ``nil`` if the device auth key does not exist.
+    /// - Parameters:
+    ///   - request: The passkey assertion request.
+    ///   - recordIdentifier: The record identifier for the ``ASPasskeyCredentialIdentity`` related to the passkey
+    ///     assertion request, which should be equal to the cipher ID of the device auth key record.
+    ///   - userId: Currently active user ID for the account.
+    /// - Returns: A ``GetAssertionResult``, or `nil` if the device auth key does not exist.
     func assertDeviceAuthKey(
         for request: GetAssertionRequest,
         recordIdentifier: String,
@@ -37,8 +38,8 @@ protocol DeviceAuthKeyService {
 
     /// Retrieve the metadata for the device passkey, if it exists.
     ///
-    ///  - Parameters:
-    ///      - userId: Currently active user ID for the account.
+    /// - Parameters:
+    ///   - userId: Currently active user ID for the account.
     func getDeviceAuthKeyMetadata(userId: String) async throws -> DeviceAuthKeyMetadata?
 }
 
@@ -85,9 +86,10 @@ struct DefaultDeviceAuthKeyService: DeviceAuthKeyService {
     /// Retrieve the device auth key secrets, if the record exists. This is private because no other class should need
     /// to have access to the private key; all of the auth is done here.
     ///
-    /// Before calling, vault must be unlocked to wrap user encryption key.
-    ///  - Parameters:
-    ///      - userId: User ID for the account to fetch.
+    /// Before calling, the vault must be unlocked to wrap user encryption key.
+    ///
+    /// - Parameters:
+    ///   - userId: User ID for the account to fetch.
     private func getDeviceAuthKeyRecord(userId: String) async throws -> DeviceAuthKeyRecord? {
         try await keychainRepository.getDeviceAuthKey(userId: userId)
     }
@@ -96,6 +98,6 @@ struct DefaultDeviceAuthKeyService: DeviceAuthKeyService {
 // MARK: - DeviceAuthKeyError
 
 enum DeviceAuthKeyError: Error {
-    case notImplemented
     case missingOrInvalidKey
+    case notImplemented
 }
