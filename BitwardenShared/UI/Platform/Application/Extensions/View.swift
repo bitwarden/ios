8a58f83439064143999408e5edd049eaf4ed82bb
Federico Maccaroni <fedemkr@gmail.com>
2026-03-11 11:34:05 -0300

    PM-33231 Improved dispatch async after code.

--- BitwardenShared/UI/Platform/Application/Extensions/View.swift
+++ BitwardenShared/UI/Platform/Application/Extensions/View.swift
@@ -175,12 +175,8 @@ private struct KeyboardDismissOnAppearView: UIViewRepresentable {
     func makeUIView(context: Context) -> UIView {
         let view = UIView(frame: .zero)
         view.backgroundColor = .clear
-        Task {
-            /// Wait a bit so the animation ends and also until the keyboard is actually shown to be dismissed.
-            try? await Task.sleep(nanoseconds: 500_000_000)
-            _ = await MainActor.run {
-                view.window?.endEditing(true)
-            }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+            view.window?.endEditing(true)
         }
         return view
     }
