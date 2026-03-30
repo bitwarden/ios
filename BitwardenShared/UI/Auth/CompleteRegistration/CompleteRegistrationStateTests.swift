ede1a2c3323c57de987c0301903135553dd2b351
Morgan Zellers <mzellers@bitwarden.com>
2026-03-30 11:16:50 -0500

    Adds comments for tests.

--- BitwardenShared/UI/Auth/CompleteRegistration/CompleteRegistrationStateTests.swift
+++ BitwardenShared/UI/Auth/CompleteRegistration/CompleteRegistrationStateTests.swift
@@ -3,6 +3,7 @@ import XCTest
 @testable import BitwardenShared
 
 class CompleteRegistrationStateTests: BitwardenTestCase {
+    /// `doesMasterPasswordMatchHint` returns `true` if the password and hint match.
     func test_doesMasterPasswordMatchHint_whenMatching_returnsTrue() {
         var subject = CompleteRegistrationState(
             emailVerificationToken: "",
@@ -14,7 +15,8 @@ class CompleteRegistrationStateTests: BitwardenTestCase {
 
         XCTAssertTrue(subject.doesMasterPasswordMatchHint)
     }
-
+    
+    /// `doesMasterPasswordMatchHint` returns `true` if the password and hint match after trimming whitespace from the hint.
     func test_doesMasterPasswordMatchHint_whenWhitespaceDifference_returnsTrue() {
         var subject = CompleteRegistrationState(
             emailVerificationToken: "",
@@ -27,6 +29,7 @@ class CompleteRegistrationStateTests: BitwardenTestCase {
         XCTAssertTrue(subject.doesMasterPasswordMatchHint)
     }
 
+    /// `doesMasterPasswordMatchHint` returns `false` if the password and hint do not match.
     func test_doesMasterPasswordMatchHint_whenValidHint_returnsFalse() {
         var subject = CompleteRegistrationState(
             emailVerificationToken: "",
@@ -39,6 +42,7 @@ class CompleteRegistrationStateTests: BitwardenTestCase {
         XCTAssertFalse(subject.doesMasterPasswordMatchHint)
     }
 
+    /// `continueButtonEnabled` returns `false` when password is empty.
     func test_isContinueButtonEnabled_whenPasswordEmpty_returnsFalse() {
         var subject = CompleteRegistrationState(
             emailVerificationToken: "",
@@ -50,7 +54,8 @@ class CompleteRegistrationStateTests: BitwardenTestCase {
 
         XCTAssertFalse(subject.continueButtonEnabled)
     }
-
+    
+    /// `continueButtonEnabled` returns `false` when password is too short.
     func test_isContinueButtonEnabled_whenPasswordShort_returnsFalse() {
         var subject = CompleteRegistrationState(
             emailVerificationToken: "",
@@ -62,7 +67,8 @@ class CompleteRegistrationStateTests: BitwardenTestCase {
 
         XCTAssertFalse(subject.continueButtonEnabled)
     }
-
+    
+    /// `continueButtonEnabled` returns `true` when password is valid .
     func test_isContinueButtonEnabled_validPassword_returnsTrue() {
         var subject = CompleteRegistrationState(
             emailVerificationToken: "",
@@ -75,6 +81,7 @@ class CompleteRegistrationStateTests: BitwardenTestCase {
         XCTAssertTrue(subject.continueButtonEnabled)
     }
 
+    /// `isWeakPassword` returns `false` when strength score is `nil`.
     func test_isWeakPassword_whenScoreIsNil_returnsFalse() {
         var subject = CompleteRegistrationState(
             emailVerificationToken: "",
@@ -86,6 +93,7 @@ class CompleteRegistrationStateTests: BitwardenTestCase {
         XCTAssertFalse(subject.isWeakPassword)
     }
 
+    /// `isWeakPassword` returns `true` when strength score is below threshold.
     func test_isWeakPassword_whenScoreJustBelowThreshold_returnsTrue() {
         var subject = CompleteRegistrationState(
             emailVerificationToken: "",
@@ -97,6 +105,7 @@ class CompleteRegistrationStateTests: BitwardenTestCase {
         XCTAssertTrue(subject.isWeakPassword)
     }
 
+    /// `isWeakPassword` returns `false` when strength score is equal to threshold.
     func test_isWeakPassword_whenScoreAtThreshold_returnsFalse() {
         var subject = CompleteRegistrationState(
             emailVerificationToken: "",
@@ -108,6 +117,7 @@ class CompleteRegistrationStateTests: BitwardenTestCase {
         XCTAssertFalse(subject.isWeakPassword)
     }
 
+    /// `isWeakPassword` returns `false` when strength score is above threshold.
     func test_isWeakPassword_whenScoreAboveThreshold_returnsFalse() {
         var subject = CompleteRegistrationState(
             emailVerificationToken: "",
