6030fca4829a2175f602dad8faa8d837641aba3d
Katherine Bertelsen <kbertelsen@bitwarden.com>
2026-04-02 14:07:39 -0500

    Change shared keychain to automockable

--- AuthenticatorBridgeKit/SharedKeychain/Mocks/MockSharedKeychainRepository.swift
+++ AuthenticatorBridgeKit/SharedKeychain/Mocks/MockSharedKeychainRepository.swift
@@ -3,51 +3,11 @@ import BitwardenKit
 import CryptoKit
 import Foundation
 
-public class MockSharedKeychainRepository: SharedKeychainRepository {
-    public var authenticatorKey: Data?
-    public var errorToThrow: Error?
-    public var accountAutoLogoutTime = [String: Date]()
-
-    public init() {}
-
+public extension MockSharedKeychainRepository {
     /// Generates a `Data` object that looks like the key used for encrypting shared items.
     /// Useful for tests that want reasonably authentic-looking data.
-    public func generateMockKeyData() -> Data {
+    func generateMockKeyData() -> Data {
         let key = SymmetricKey(size: .bits256)
         return key.withUnsafeBytes { Data(Array($0)) }
-    }
-
-    public func deleteAuthenticatorKey() throws {
-        if let errorToThrow { throw errorToThrow }
-
-        authenticatorKey = nil
-    }
-
-    public func getAuthenticatorKey() async throws -> Data {
-        if let errorToThrow { throw errorToThrow }
-
-        if let authenticatorKey {
-            return authenticatorKey
-        } else {
-            throw KeychainServiceError.keyNotFound(SharedKeychainItem.authenticatorKey)
-        }
-    }
-
-    public func setAuthenticatorKey(_ value: Data) async throws {
-        if let errorToThrow { throw errorToThrow }
-
-        authenticatorKey = value
-    }
-
-    public func getAccountAutoLogoutTime(userId: String) async throws -> Date? {
-        if let errorToThrow { throw errorToThrow }
-
-        return accountAutoLogoutTime[userId]
-    }
-
-    public func setAccountAutoLogoutTime(_ value: Date?, userId: String) async throws {
-        if let errorToThrow { throw errorToThrow }
-
-        accountAutoLogoutTime[userId] = value
     }
 }
