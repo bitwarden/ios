1b950e5711e65b204b48574569830d60ceb278b3
André Bispo <abispo@bitwarden.com>
2026-04-15 09:13:51 +0100

    [PM-35126] Fix BitwardenDiscountType doc comment hyphenation

--- BitwardenShared/Core/Billing/Models/Enum/BitwardenDiscountType.swift
+++ BitwardenShared/Core/Billing/Models/Enum/BitwardenDiscountType.swift
@@ -3,7 +3,7 @@
 /// The type of discounts Bitwarden supports.
 ///
 enum BitwardenDiscountType: String, Codable, Equatable, Sendable {
-    /// A fixed amount discount.
+    /// A fixed-amount discount.
     case amountOff = "amount-off"
 
     /// A percentage discount.
