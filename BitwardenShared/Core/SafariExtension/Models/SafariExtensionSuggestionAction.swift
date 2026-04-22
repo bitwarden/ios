a40225d7b51ec264bb89a20a71e81b76de51feb2
2001y <akitani@akitaninoMac-mini.local>
2026-04-22 21:46:14 +0900

    feat: add safari extension suggestion action model

---
+++ BitwardenShared/Core/SafariExtension/Models/SafariExtensionSuggestionAction.swift
@@ -0,0 +1,26 @@
+// MARK: - SafariExtensionSuggestionAction
+
+/// The primary action the Safari extension UI should suggest for a given request.
+enum SafariExtensionSuggestionAction: String, Codable, Equatable {
+    case none
+    case fill
+    case saveLogin
+    case updatePassword
+    case generatePassword
+
+    static func from(_ request: SafariExtensionRequest) -> Self {
+        if request.canAutofill {
+            return .fill
+        }
+        if request.canSaveLogin {
+            return .saveLogin
+        }
+        if request.canChangePassword {
+            return .updatePassword
+        }
+        if request.canGeneratePassword {
+            return .generatePassword
+        }
+        return .none
+    }
+}
