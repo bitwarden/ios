c35f49ebadb1386740ff527bcf07d1c03d85deb7
2001y <akitani@akitaninoMac-mini.local>
2026-04-22 19:11:50 +0900

    feat: add safari extension settings scaffold

---
+++ BitwardenShared/UI/Platform/Settings/Settings/AutoFill/SafariExtension/SafariExtensionProcessorTests.swift
@@ -0,0 +1,58 @@
+import BitwardenKitMocks
+import BitwardenSdk
+import XCTest
+
+@testable import BitwardenShared
+
+class SafariExtensionProcessorTests: BitwardenTestCase {
+    // MARK: Properties
+
+    var coordinator: MockCoordinator<SettingsRoute, SettingsEvent>!
+    var subject: SafariExtensionProcessor!
+
+    // MARK: Setup & Teardown
+
+    override func setUp() {
+        super.setUp()
+
+        coordinator = MockCoordinator()
+
+        subject = SafariExtensionProcessor(
+            coordinator: coordinator.asAnyCoordinator(),
+            state: SafariExtensionState(),
+        )
+    }
+
+    override func tearDown() {
+        super.tearDown()
+
+        coordinator = nil
+        subject = nil
+    }
+
+    // MARK: Tests
+
+    @MainActor
+    func test_didDismissSafariExtensionSetup_enabled() {
+        subject.didDismissSafariExtensionSetup(enabled: true)
+
+        XCTAssertTrue(subject.state.extensionActivated)
+        XCTAssertTrue(subject.state.extensionEnabled)
+    }
+
+    @MainActor
+    func test_didDismissSafariExtensionSetup_notEnabled() {
+        subject.didDismissSafariExtensionSetup(enabled: false)
+
+        XCTAssertTrue(subject.state.extensionActivated)
+        XCTAssertFalse(subject.state.extensionEnabled)
+    }
+
+    @MainActor
+    func test_receive_activateButtonTapped() {
+        subject.receive(.activateButtonTapped)
+
+        XCTAssertEqual(coordinator.routes.last, .safariExtensionSetup)
+        XCTAssertIdentical(coordinator.contexts.last as? SafariExtensionProcessor, subject)
+    }
+}
---
+++ BitwardenShared/UI/Platform/Settings/Settings/AutoFill/SafariExtension/SafariExtensionState.swift
@@ -0,0 +1,10 @@
+// MARK: - SafariExtensionState
+
+/// The state used to present the `SafariExtensionView`.
+struct SafariExtensionState: Equatable {
+    /// Whether the extension was activated.
+    var extensionActivated = false
+
+    /// Whether the extension is enabled.
+    var extensionEnabled = false
+}
