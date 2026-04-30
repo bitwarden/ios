df506fb2fd61ed14b065d9d11dd8316e75895e57
Álison Fernandes <vvolkgang@users.noreply.github.com>
2026-04-30 13:19:42 +0100

    Update jira_release_notes.py script to use jira cloud ID and http stdin lib

--- .github/scripts/jira-get-release-notes/jira_release_notes.py
+++ .github/scripts/jira-get-release-notes/jira_release_notes.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import sys
 import base64
 import json
-import requests
+import sys
+from urllib.error import HTTPError
+from urllib.request import Request, urlopen
 
 SCRIPT_NAME = "jira_release_notes.py"
 
@@ -64,31 +65,34 @@ def parse_release_notes(response_json):
         return ''
 
 def main():
-    if len(sys.argv) != 4:
-        print(f"Usage: {sys.argv[0]} <issue_id> <jira_email> <jira_api_token>")
+    if len(sys.argv) != 5:
+        print(f"Usage: {sys.argv[0]} <issue_id> <jira_cloud_id> <jira_email> <jira_api_token>")
         sys.exit(1)
 
     jira_issue_id = sys.argv[1]
-    jira_email = sys.argv[2]
-    jira_api_token = sys.argv[3]
-    jira_base_url = "https://bitwarden.atlassian.net"
+    jira_cloud_id = sys.argv[2]
+    jira_email = sys.argv[3]
+    jira_api_token = sys.argv[4]
+    jira_base_url = "https://api.atlassian.com/ex/jira"
 
     auth = base64.b64encode(f"{jira_email}:{jira_api_token}".encode()).decode()
-    headers = {
-        "Authorization": f"Basic {auth}",
-        "Content-Type": "application/json"
-    }
-
-    response = requests.get(
-        f"{jira_base_url}/rest/api/3/issue/{jira_issue_id}",
-        headers=headers
+    request = Request(
+        f"{jira_base_url}/{jira_cloud_id}/rest/api/3/issue/{jira_issue_id}",
+        headers={
+            "Authorization": f"Basic {auth}",
+            "Content-Type": "application/json"
+        }
     )
 
-    if response.status_code != 200:
-        print(f"[{SCRIPT_NAME}] Error fetching Jira issue ({jira_issue_id}). Status code: {response.status_code}. Msg: {response.text}", file=sys.stderr)
+    try:
+        with urlopen(request) as response:
+            response_json = json.loads(response.read().decode())
+    except HTTPError as error:
+        error_text = error.read().decode()
+        print(f"[{SCRIPT_NAME}] Error fetching Jira issue ({jira_issue_id}). Status code: {error.code}. Msg: {error_text}", file=sys.stderr)
         sys.exit(1)
 
-    release_notes = parse_release_notes(response.json())
+    release_notes = parse_release_notes(response_json)
     print(release_notes)
 
 if __name__ == "__main__":
