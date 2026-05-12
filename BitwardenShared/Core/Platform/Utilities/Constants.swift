8e761e7b9939a60b17c37cca659a43141075ac04
Katherine Bertelsen <kbertelsen@bitwarden.com>
2026-05-12 10:36:10 -0500

    [PM-36643] refactor: Extract premium checkout debounce interval to Constants (#2632)

--- BitwardenShared/Core/Platform/Utilities/Constants.swift
+++ BitwardenShared/Core/Platform/Utilities/Constants.swift
@@ -87,6 +87,9 @@ extension Constants {
     /// The default number of KDF iterations to perform.
     static let pbkdf2Iterations = 600_000
 
+    /// The premium checkout status publisher debounce interval.
+    static let premiumCheckoutStatusDebounceInterval: DispatchQueue.SchedulerTimeType.Stride = .milliseconds(300)
+
     /// The minimum account age required to show the premium upgrade banner.
     static let premiumUpgradeBannerAccountAge: TimeInterval = 7 * 24 * 60 * 60
 
