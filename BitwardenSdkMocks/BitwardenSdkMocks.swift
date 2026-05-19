9ee43772e77cd893b24348ab37cc211febe6e15d
Matt Czech <matt@livefront.com>
2026-05-19 09:45:17 -0500

    [PM-35680] chore: Convert CryptoClientProtocol to AutoMockable (#2663)

--- BitwardenSdkMocks/BitwardenSdkMocks.swift
+++ BitwardenSdkMocks/BitwardenSdkMocks.swift
@@ -12,6 +12,7 @@ extension AttachmentsClientProtocol {}
 extension CiphersClientProtocol {}
 extension ClientManagedTokens {}
 extension CollectionsClientProtocol {}
+extension CryptoClientProtocol {}
 extension ExporterClientProtocol {}
 extension FoldersClientProtocol {}
 extension GeneratorClientsProtocol {}
