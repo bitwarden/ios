23b9354a732010e84ba0b417de5d63912120e6a2
André Bispo <abispo@bitwarden.com>
2026-05-20 15:56:43 +0100

    [PM-37697] feat: Inline manageSubscriptionURL in showManageSubscriptionAlert

--- BitwardenShared/UI/Billing/PremiumPlan/PremiumPlanProcessor.swift
+++ BitwardenShared/UI/Billing/PremiumPlan/PremiumPlanProcessor.swift
@@ -69,10 +69,9 @@ final class PremiumPlanProcessor: StateProcessor<
     /// Shows the "Continue to web app?" alert for managing the subscription plan.
     ///
     private func showManageSubscriptionAlert() {
-        let manageSubscriptionURL = services.environmentService.manageSubscriptionURL
         coordinator.showAlert(
             .manageSubscriptionPlanAlert { [weak self] in
-                self?.state.urlToOpen = manageSubscriptionURL
+                self?.state.urlToOpen = self?.services.environmentService.manageSubscriptionURL
             },
         )
     }
