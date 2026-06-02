1aa29d89eb50e942fa79b2d08623436b7a4487b5
Morgan Zellers <mzellers@bitwarden.com>
2026-06-02 14:25:02 -0500

    Removing leftover use-passkey changes.

--- TestHarnessShared/UI/Platform/Root/RootRoute.swift
+++ TestHarnessShared/UI/Platform/Root/RootRoute.swift
@@ -9,7 +9,6 @@ public enum RootRoute {
     /// A route to the create account form test screen.
     case createAccountForm
 
-
     /// A route to the date field picker showcase screen.
     case dateFieldPickerShowcase
 
