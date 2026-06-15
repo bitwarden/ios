2312452a1674a1ee134df29cec25069853bab4b0
Patrick Honkonen <1883101+SaintPatrck@users.noreply.github.com>
2026-06-15 09:45:09 -0400

    [PM-38150] feat: Add Driver's License view screen (#2758)

---
+++ BitwardenShared/UI/Vault/VaultItem/ViewItem/ViewDriversLicenseItem/ViewDriversLicenseItemAction.swift
@@ -0,0 +1,8 @@
+// MARK: ViewDriversLicenseItemAction
+
+/// An enum of actions for viewing a driver's license item.
+///
+enum ViewDriversLicenseItemAction: Equatable {
+    /// Toggles the visibility of the license number.
+    case toggleLicenseNumberVisibilityChanged
+}
