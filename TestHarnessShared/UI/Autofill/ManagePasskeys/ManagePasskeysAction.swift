ca218f8cc9a99fe3c2377d1d3a4c3f3a6d51cd6f
Morgan Zellers <mzellers@bitwarden.com>
2026-07-17 15:09:14 -0500

    Removing empty ManagePasskeysAction

--- TestHarnessShared/UI/Autofill/ManagePasskeys/ManagePasskeysAction.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-/// Actions that can be processed by a `ManagePasskeysProcessor`.
-///
-/// This screen has no synchronous user interactions of its own; loading and deleting stored
-/// credentials are handled as `ManagePasskeysEffect`s.
-///
-enum ManagePasskeysAction: Equatable {}
