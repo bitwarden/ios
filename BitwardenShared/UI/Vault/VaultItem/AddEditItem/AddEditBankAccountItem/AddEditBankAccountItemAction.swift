bf82f3ef2dea355727c4714a17e798d371cc2913
Morgan Zellers <mzellers@bitwarden.com>
2026-08-25 16:18:41 -0500

    Handles unset account type correctly.

--- BitwardenShared/UI/Vault/VaultItem/AddEditItem/AddEditBankAccountItem/AddEditBankAccountItemAction.swift
+++ BitwardenShared/UI/Vault/VaultItem/AddEditItem/AddEditBankAccountItem/AddEditBankAccountItemAction.swift
@@ -11,6 +11,9 @@ enum AddEditBankAccountItemAction: Equatable, Sendable {
     /// The type of the bank account changed.
     case accountTypeChanged(DefaultableType<BankAccountType>)
 
+    /// The account type menu was opened.
+    case accountTypeMenuOpened
+
     /// The contact phone number for the bank changed.
     case bankContactPhoneChanged(String)
 
